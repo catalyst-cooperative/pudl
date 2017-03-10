7c76f2dbd11582d18433ea25530d8a9686eb6243
Zane Selvans <zane.selvans@catalyst.coop>
2017-03-10 13:15:33 -0700

    Removed f1_purchased_pwr from ferc1_working_tables. Because it's not.

--- pudl/constants.py
+++ pudl/constants.py
@@ -458,8 +458,7 @@ ferc1_pudl_tables = ['f1_fuel',
                      'f1_steam',
                      'f1_hydro',
                      'f1_plant_in_srvce',
-                     'f1_accumdepr_prvsn',
-                     'f1_purchased_pwr']
+                     'f1_accumdepr_prvsn']
 
 # This is the full set of tables that currently ingestible by the ferc1 DB:
 ferc1_working_tables = ['f1_respondent_id',
