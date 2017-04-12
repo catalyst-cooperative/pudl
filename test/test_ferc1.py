302e9e58b0b2f2fbc411865baa4322cae530d244
Zane Selvans <zane.selvans@catalyst.coop>
2017-04-12 10:14:55 -0600

    Update module imports and standardize test args.
    Realized that the test modules need to import all of the PUDL modules
    (including those which define the DB tables) so that they have access to the
    MetaData objects required to drop the FERC & PUDL DB tables, once they're done
    running the tests.
    
    Updated some of the arguments to init_db() functions to be canonical values set
    in the constants module, rather than hard coded or quiet default keyword values.

--- test/test_ferc1.py
+++ test/test_ferc1.py
@@ -2,12 +2,17 @@
 
 import pytest
 import pudl.ferc1
-import pudl.constants as pc
+from pudl import ferc1, settings, constants
 
 
 def test_init_db():
     """Create a fresh FERC Form 1 DB and attempt to access it."""
-    pudl.ferc1.init_db(refyear=2015, years=pc.ferc1_working_years,
-                       def_db=True, verbose=True, testing=True)
-    ferc1_engine = pudl.ferc1.db_connect_ferc1(testing=True)
+    ferc1.init_db(ferc1_tables=constants.ferc1_default_tables,
+                  refyear=2015,
+                  years=constants.ferc1_working_years,
+                  def_db=True,
+                  verbose=True,
+                  testing=True)
+
+    ferc1_engine = ferc1.db_connect_ferc1(testing=True)
     pudl.ferc1.drop_tables_ferc1(ferc1_engine)
