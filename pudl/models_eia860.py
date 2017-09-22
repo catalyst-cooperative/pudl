9d463a95decf0bf4797e9ecb283b563b5c6edcf1
Zane Selvans <zane.selvans@catalyst.coop>
2017-09-22 10:52:03 -0600

    Removed erroneous primary key designation for plant_id

--- pudl/models_eia860.py
+++ pudl/models_eia860.py
@@ -112,8 +112,7 @@ class GeneratorsEIA860(pudl.models.PUDLBase):
     id = Column(Integer, autoincrement=True, primary_key=True)
     operator_id = Column(Integer)
     operator_name = Column(String)
-    plant_id = Column(Integer, ForeignKey('plants_eia.plant_id'),
-                      primary_key=True)
+    plant_id = Column(Integer, ForeignKey('plants_eia.plant_id'))
     plant_name = Column(String)
     state = Column(String)
     county = Column(String)
