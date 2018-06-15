e7e2b286aa8e3ad89c3579193acb3ae3752b7032
karldw <karldw@users.noreply.github.com>
2018-06-15 09:50:48 -0700

    Add Travis files, borrowing heavily from pandas
    - Also edit environment.yml
        - matplotlib is used in analysis.py
        - dbfread is in conda now

---
+++ ci/setup_postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "installing dbs"
+
+psql -c 'create database pandas_nosetest;'
+psql -U postgres -c "CREATE ROLE catalyst WITH LOGIN;"
+psql -U postgres -c "CREATE DATABASE pudl_test WITH OWNER=catalyst ENCODING='UTF8';"
+psql -U postgres -c "CREATE DATABASE ferc1_test WITH OWNER=catalyst ENCODING='UTF8';"
+
+echo "done"
+exit 0
