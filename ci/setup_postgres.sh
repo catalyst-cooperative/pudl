70482a00b4d8f26382eebe8a43658cd31d37426a
Zane Selvans <zane.selvans@catalyst.coop>
2018-06-17 22:30:07 -0600

    Add GitHub access token for Travis CI
    Also removed the pandas postgresql DB creation line.

--- ci/setup_postgres.sh
+++ ci/setup_postgres.sh
@@ -2,7 +2,6 @@
 
 echo "installing dbs"
 
-psql -c 'create database pandas_nosetest;'
 psql -U postgres -c "CREATE ROLE catalyst WITH LOGIN;"
 psql -U postgres -c "CREATE DATABASE pudl_test WITH OWNER=catalyst ENCODING='UTF8';"
 psql -U postgres -c "CREATE DATABASE ferc1_test WITH OWNER=catalyst ENCODING='UTF8';"
