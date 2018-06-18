7a399a29ef4f5d5339e46fd0c0f4561d30e05093
karldw <karldw@users.noreply.github.com>
2018-06-17 23:38:57 -0700

    Add op_datetime, remove op_date, op_hour, and heatrate

--- pudl/models/epacems.py
+++ pudl/models/epacems.py
@@ -1,18 +1,6 @@
 """Database models for PUDL tables derived from EPA CEMS Data."""
 
-from sqlalchemy import (
-    Boolean,
-    Integer,
-    String,
-    Float,
-    Numeric,
-    Date,
-    Column,
-    ForeignKey,
-    ForeignKeyConstraint,
-    UniqueConstraint,
-    CheckConstraint,
-)
+from sqlalchemy import Integer, String, Float, DateTime, Column
 import pudl.models.entities
 
 
@@ -20,7 +8,11 @@ class HourlyEmissions(pudl.models.entities.PUDLBase):
     """Hourly emissions data by month as reported to EPA CEMS."""
     # TODO: Add these constraints and indexes *after* loading all the data in.
     # UniqueConstraint("orispl_code", "unitid", "op_date", "op_hour")
-    # indexes on: orispl_code, op_date, orispl_code (each individually)
+    # indexes on: orispl_code, op_datetime, orispl_code, and the date part of
+    # op_datetime (each individually)
+    # Command to create an index for the date part of op_datetime
+    # CREATE INDEX op_date_idx (op_datetime::date)
+
     __tablename__ = "hourly_emissions_epacems"
     __table_args__ = {'prefixes': ['UNLOGGED']}
     id = Column(Integer, autoincrement=True, primary_key=True)  # surrogate key
@@ -29,9 +21,8 @@ class HourlyEmissions(pudl.models.entities.PUDLBase):
     # TODO: Link to EIA plant ID
     orispl_code = Column(Integer, nullable=False)
     unitid = Column(String, nullable=False)
-    # TODO: parse these as better dates or intervals
-    op_date = Column(String, nullable=False)
-    op_hour = Column(Integer, nullable=False)
+    op_datetime = Column(DateTime, nullable=False)
+    # TODO: Make a view that multiplies op_time and gload_mw to get gload_mwh
     op_time = Column(Float)
     gload_mw = Column(Float)
     sload_1000_lbs = Column(Float)
@@ -48,9 +39,11 @@ class HourlyEmissions(pudl.models.entities.PUDLBase):
     co2_rate_tons_mmbtu = Column(Float)
     co2_rate_measure_flg = Column(String)
     heat_input_mmbtu = Column(Float)
+    # TODO: Make a view that divides heat_input_mmbtu / gload_mwh to get heatrate
+    #   And also has a bad_heatrate flag.
+    # heatrate = Column(Float)
+    # bad_heatrate = Column(Boolean, nullable=False)
     # TODO: I think these are the same as orispl_code and unitid, but check.
     # These are currently being dropped in extract/epacems.py
     # fac_id = Column(Integer)
     # unit_id = Column(Integer)
-    heatrate = Column(Float)
-    bad_heatrate = Column(Boolean, nullable=False)
--- pudl/transform/epacems.py
+++ pudl/transform/epacems.py
@@ -13,19 +13,35 @@ import pudl.constants as pc
 ###############################################################################
 
 
-def calc_heatrate(df):
-    """
-    Calculate heatrates and flag implausible values -- NOT YET IMPLEMENTED
+def fix_up_dates(df):
+    """Fix the dates for the CEMS data
+
+    Three date/datetime changes (not all implemented)
+    - Make op_date a DATE type
+    - Make an appropriate INTERVAL type (not implemented)
+    - Add a UTC timestamp (not implemented)
 
     Args:
         df(pandas.DataFrame): A CEMS hourly dataframe for one year-month-state
     Output:
-        pandas.DataFrame: The same data, with additional columns
-        heatrate (float) and bad_heatrate (bool)
+        pandas.DataFrame: The same data, with an op_datetime column added and
+        the op_date and op_hour columns removed
     """
-    # TODO: actually implement this!
-    df["heatrate"] = np.NaN
-    df["bad_heatrate"] = True
+    # Convert to interval:
+    # df = convert_time_to_interval(df)
+
+    # Convert op_date and op_hour from string and integer to datetime:
+    # Note that doing this conversion, rather than reading the CSV with
+    # `parse_dates=True` is >10x faster.
+    df["op_datetime"] = pd.to_datetime(
+        df['op_date'].str.cat(df['op_hour'].astype(str), sep='-'),
+        format="%m-%d-%Y-%H",
+        exact=True,
+        cache=True)
+    del df['op_date']
+    del df['op_hour']
+
+    # Add UTC timestamp ... not done.
     return df
 
 
@@ -33,13 +49,23 @@ def convert_time_to_interval(df):
     """
     Reframe the CEMS hourly time as an interval -- NOT YET IMPLEMENTED
 
+    NOTE: this function is currently written to be called *before* converting
+    op_date to a Date.
+
     Args:
         df(pandas.DataFrame): A CEMS hourly dataframe for one year-month-state
     Output:
-        pandas.DataFrame: The same data, with the hour timestamp converted to
-        interval.
+        pandas.DataFrame: The same data, with an op_interval column added.
     """
-    # TODO: implement this.
+    raise NotImplementedError("This op_interval isn't included in the " +
+        "SQLAlchemy model yet. Figure out what you want to do with interval " +
+        "data first.")
+    df['op_interval'] = pd.to_datetime(
+        df['op_date'].str.cat(df['op_hour'].astype(str), sep='-'),
+        format="%m-%d-%Y-%H",
+        box=False,
+        exact=True,
+        cache=True).dt.to_period("H")
     return df
 
 
@@ -72,8 +98,7 @@ def transform(epacems_raw_dfs, verbose=True):
         # that could be changed if you want.
         for yr_mo_st, raw_df in raw_df_dict.items():
             df = (
-                raw_df.pipe(convert_time_to_interval)
-                .pipe(calc_heatrate)
+                raw_df.pipe(fix_up_dates)
                 .pipe(harmonize_eia_epa_orispl)
             )
             yield {yr_mo_st: df}
