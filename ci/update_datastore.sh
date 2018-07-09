224e9ad54b4acdf8abaa7e2e1be66f60cdef0f52
Zane Selvans <zane.selvans@catalyst.coop>
2018-07-09 12:42:52 -0600

    Make update_datastore.sh executable

--- ci/update_datastore.sh
+++ ci/update_datastore.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Download some federal utility data to test with.
 
+echo
+echo "[updating PUDL datastore]"
 cd scripts
 python update_datastore.py --source ferc1 eia860 --years 2012 2016
 python update_datastore.py --source eia923 --years 2016
