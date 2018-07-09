9ae2fb7dfff65507a1e7bcbc6a77ba88ab04497c
Zane Selvans <zane.selvans@catalyst.coop>
2018-07-09 13:18:30 -0600

    Debugging conda environment on Travis CI

--- ci/update_datastore.sh
+++ ci/update_datastore.sh
@@ -5,6 +5,8 @@ echo
 echo "[updating PUDL datastore]"
 cd scripts
 which python
+source activate pudl
+which python
 python update_datastore.py --source ferc1 eia860 --years 2012 2016
 python update_datastore.py --source eia923 --years 2016
 python update_datastore.py --source epacems --states CO --years 2016
