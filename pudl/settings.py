66985847f18a450272e2646ebd639cb5866e031b
Christina Gosnell <cgosnell@catalyst.coop>
2018-09-24 15:39:33 -0400

    Separate settings as init_db inputs and global variables

--- pudl/settings.py
+++ pudl/settings.py
@@ -10,10 +10,21 @@ from pudl import __file__ as pudl_pkg_file
 import os.path
 
 PUDL_DIR = os.path.dirname(os.path.dirname(pudl_pkg_file))
-with open(os.path.join(PUDL_DIR, "settings.yml"), "r") as f:
-    SETTINGS = yaml.load(f)
 
 
+def settings_init(settings_file="settings.yml"):
+    with open(os.path.join(PUDL_DIR, 'scripts', settings_file), "r") as f:
+        settings_init = yaml.load(f)
+
+    # if the refyear (particular if it came directly from settings.yml) is set
+    # to none, but there
+    if not settings_init['ferc1_ref_year']:
+        settings_init['ferc1_ref_year'] = max(settings_init['ferc1_years'])
+
+    return(settings_init)
+
+
+SETTINGS = {}
 # ALL_CAPS indicates global variables that pertain to the entire project.
 SETTINGS['pudl_dir'] = PUDL_DIR
 SETTINGS['data_dir'] = os.path.join(SETTINGS['pudl_dir'], 'data')
@@ -27,6 +38,38 @@ SETTINGS['epacems_data_dir'] = os.path.join(
     SETTINGS['data_dir'], 'epa', 'cems')
 SETTINGS['test_dir'] = os.path.join(SETTINGS['pudl_dir'], 'test')
 SETTINGS['docs_dir'] = os.path.join(SETTINGS['pudl_dir'], 'docs')
+SETTINGS['csvdir'] = os.path.join(SETTINGS['pudl_dir'], 'results', 'csvdump')
 
-SETTINGS['csvdir'] = os.path.join(SETTINGS['pudl_dir'],
-                                  'results', SETTINGS['csvdir'])
+
+# These DB connection dictionaries are used by sqlalchemy.URL()
+# (Using 127.0.0.1, the numeric equivalent of localhost, to make postgres use
+# the `.pgpass` file without fussing around in the config.)
+# sqlalchemy.URL will make a URL missing post (therefore using the default),
+# and missing a password (which will make the system look for .pgpass)
+SETTINGS['db_ferc1'] = {
+    'drivername': 'postgresql',
+    'host': '127.0.0.1',
+    'username': 'catalyst',
+    'database': 'ferc1'
+}
+
+SETTINGS['db_pudl'] = {
+    'drivername': 'postgresql',
+    'host': '127.0.0.1',
+    'username': 'catalyst',
+    'database': 'pudl'
+}
+
+SETTINGS['db_ferc1_test'] = {
+    'drivername': 'postgresql',
+    'host': '127.0.0.1',
+    'username': 'catalyst',
+    'database': 'ferc1_test'
+}
+
+SETTINGS['db_pudl_test'] = {
+    'drivername': 'postgresql',
+    'host': '127.0.0.1',
+    'username': 'catalyst',
+    'database': 'pudl_test'
+}
