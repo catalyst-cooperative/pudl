d5d3c7deee8584700ee27e62b7a3b43a2ebba9f0
Zane Selvans <zane.selvans@catalyst.coop>
2019-06-14 13:51:34 -0600

    Fixing path to scripts and python modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../pudl'))
+sys.path.insert(0, os.path.abspath('../../scripts'))
 
 
 # -- Project information -----------------------------------------------------
