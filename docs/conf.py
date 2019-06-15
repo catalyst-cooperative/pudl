8fce3c7ff07ddb760bb1e02817d2a1f0390e2215
Zane Selvans <zane.selvans@catalyst.coop>
2019-06-15 08:00:02 -0600

    Fix syntax error.

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,10 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    sphinx.ext.napoleon,
-    sphinx.ext.autodoc,
-    sphinx.ext.doctest,
-    sphinx.ext.viewcode,
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
