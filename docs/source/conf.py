082b9c9049888df27f899b7c680742e6fd340be3
Zane Selvans <zane.selvans@catalyst.coop>
2019-06-15 19:20:54 -0600

    Enable github pages Sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
