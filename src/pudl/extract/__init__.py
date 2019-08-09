00e6a5cd1d9b8faf4df6fa709cced81e5866193d
Zane Selvans <zane.selvans@catalyst.coop>
2019-08-09 17:24:27 -0600

    Fix docstring refs, limit doc build to docs testenv

--- src/pudl/extract/__init__.py
+++ src/pudl/extract/__init__.py
@@ -2,11 +2,11 @@
 Modules implementing the "Extract" step of the PUDL ETL pipeline.
 
 Each module in this subpackage implements data extraction for a single data
-source from the PUDL :doc:`data_catalog`. This process begins with the original
-data as retrieved by the :mod:`pudl.datastore` subpackage, and ends with a
-dictionary of "raw" :class:`pandas.DataFrame`s, that have been minimally
-altered from the original data, and are ready for normalization and data
-cleaning by the data source specific modules in the :mod:`pudl.transform`
+source from the PUDL :ref:`data-catalog`. This process begins with
+the original data as retrieved by the :mod:`pudl.datastore` subpackage, and
+ends with a dictionary of "raw" :class:`pandas.DataFrame`s, that have been
+minimally altered from the original data, and are ready for normalization and
+data cleaning by the data source specific modules in the :mod:`pudl.transform`
 subpackage.
 
 """
--- src/pudl/transform/__init__.py
+++ src/pudl/transform/__init__.py
@@ -2,7 +2,7 @@
 Modules implementing the "Transform" step of the PUDL ETL pipeline.
 
 Each module in this subpackage transforms the tabular data associated with a
-single data source from the PUDL :doc:`data_catalog`. This process begins with
+single data source from the PUDL :ref:`data-catalog`. This process begins with
 a dictionary of "raw" :class:`pandas.DataFrame`s produced by the corresponding
 data source specific routines from the :mod:`pudl.extract` subpackage, and
 ends with a dictionary of :class:`pandas.DataFrame`s that are fully normalized,
