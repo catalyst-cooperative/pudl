aa784931b89877024c871459abc6c55f36b907df
karldw <karldw@users.noreply.github.com>
2019-10-21 17:39:16 -0700

    Adjust types and years in epacems_to_parquet.
    - Set types as pandas reads the CSV
    - Use CEMS original years for partitioning (as datapackages do), not UTC years
      (these differ because they're off by a few hours Dec 31 and Jan 1)
    - pyarrow.parquet.write_to_dataset skips the empty year-state combinations

--- src/pudl/convert/epacems_to_parquet.py
+++ src/pudl/convert/epacems_to_parquet.py
@@ -65,12 +65,22 @@ def create_in_dtypes():
     )
     state_cats = pd.CategoricalDtype(categories=pc.cems_states.keys(), ordered=False)
     in_dtypes = {
-        "co2_mass_measurement_code": co2_so2_cats,
-        "nox_mass_measurement_code": nox_cats,
-        "nox_rate_measurement_code": nox_cats,
-        "so2_mass_measurement_code": co2_so2_cats,
         "state": state_cats,
+        "plant_id_eia": "int32",
         "unitid": "str",
+        # "operating_datetime_utc": "datetime",
+        "operating_time_hours": "float32",
+        "gross_load_mw": "float32",
+        "steam_load_1000_lbs": "float32",
+        "so2_mass_lbs": "float32",
+        "so2_mass_measurement_code": co2_so2_cats,
+        "nox_rate_lbs_mmbtu": "float32",
+        "nox_rate_measurement_code": nox_cats,
+        "nox_mass_lbs": "float32",
+        "nox_mass_measurement_code": nox_cats,
+        "co2_mass_tons": "float32",
+        "co2_mass_measurement_code": co2_so2_cats,
+        "heat_content_mmbtu": "float32",
         # Note: it'd be better to use pandas' nullable integers once this issue
         # is resolved: https://issues.apache.org/jira/browse/ARROW-5379
         # "facility_id": "Int32",
@@ -133,21 +143,6 @@ def create_cems_schema():
     ])
 
 
-def year_from_operating_datetime(df):
-    """Add a 'year' column based on the year in the operating_datetime.
-
-    Args:
-        df (pandas.DataFrame): A DataFrame containing EPA CEMS data.
-
-    Returns:
-        pandas.DataFrame: A DataFrame containing EPA CEMS data with a 'year'
-        column.
-
-    """
-    df['year'] = df.operating_datetime_utc.dt.year
-    return df
-
-
 def _verify_cems_args(data_path, epacems_years, epacems_states):
     """Check that the data packaage has all years and states you want."""
     years = set()
@@ -185,6 +180,9 @@ def epacems_to_parquet(pkg_dir,
     across all of the dataframes, and downcasting to the most efficient data
     type possible for each of them. We also add a 'year' column so that we can
     partition the datset on disk by year as well as state.
+    (Year partitions follow the CEMS input data, based on local plant time.
+    The operating_datetime_utc identifies time in UTC, so there's a mismatch
+    of a few hours on December 31 / January 1.)
 
     Args:
         pkg_dir (path-like): Path to the directory of the data package which
@@ -211,7 +209,7 @@ def epacems_to_parquet(pkg_dir,
     if not out_dir:
         raise AssertionError("Required output directory not specified.")
     out_dir = pudl.load.metadata.prep_directory(out_dir, clobber=clobber)
-    in_dtypes = create_in_dtypes()
+    in_types = create_in_dtypes()
     schema = create_cems_schema()
     data_path = pathlib.Path(pkg_dir, 'data')
     # double check that all of the years you are asking for are actually in
@@ -219,15 +217,16 @@ def epacems_to_parquet(pkg_dir,
     for file in data_path.iterdir():
         if "epacems" in file.name:
             df_name = file.name[:file.name.find(".")]
-            year = df_name[25:29]
-            state = df_name[30:]
+            year = int(df_name[25:29])
+            state = df_name[30:].upper()
             # only convert the years and states that you actually want
-            if int(year) in epacems_years and state.upper() in epacems_states:
-                df = pd.read_csv(file, parse_dates=['operating_datetime_utc'])
+            if year in epacems_years and state in epacems_states:
+                df = pd.read_csv(
+                    file, dtype=in_types, parse_dates=["operating_datetime_utc"]
+                ).assign(year=year)
                 logger.info(
                     f"Converted {len(df)} records for {year} and {state}."
                 )
-                df = year_from_operating_datetime(df).astype(in_dtypes)
                 pq.write_to_dataset(
                     pa.Table.from_pandas(
                         df, preserve_index=False, schema=schema),
