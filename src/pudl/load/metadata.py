22879b633880dce30ac774f00181c7c77b294b48
Zane Selvans <zane.selvans@catalyst.coop>
2020-01-29 18:01:12 -0600

    Added some bars to highlight contact info.

--- src/pudl/load/metadata.py
+++ src/pudl/load/metadata.py
@@ -569,10 +569,12 @@ def validate_save_datapkg(datapkg_descriptor, datapkg_dir,
             f"Data package data validation failed with goodtables. "
             f"Errors: {goodtables_errors}"
         )
-    logger.info('Congrats! You made a valid data package!')
-    logger.info('If you like PUDL (or not!), we would love to hear from you..')
-    logger.info('    Let us know you are using PUDL at: hello@catalyst.coop')
-    logger.info('    Or sign up for our newsletter at: catalyst.coop/updates/')
+    logger.info("Congrats! You made a valid data package!")
+    logger.info("============================================================")
+    logger.info("  If you like PUDL (or not!) we'd love to hear from you...")
+    logger.info("  Let us know you are using PUDL at: hello@catalyst.coop")
+    logger.info("  Sign up for our newsletter: https://catalyst.coop/updates/")
+    logger.info("============================================================")
 
     return report
 
