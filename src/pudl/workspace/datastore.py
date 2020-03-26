e2d12327f87f5b2e55e77c479266a2ed9fc64e62
Pablo Virgo <mailbox@pablovirgo.com>
2020-03-26 18:33:36 +0000

    pudl 568: use pathlib instead of os.path, separate data retrieval and saving, integration test ensures remote datastore.json available for hard-coded stores

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Datastore manages file retrieval for PUDL datasets."""
+
 import os
+from pathlib import Path
 import logging
 import re
 import requests
 
-import sys
 import yaml
 
 
@@ -30,24 +32,24 @@ DOI = {
     "production": {}
 }
 
+PUDL_YML = Path.home() / ".pudl.yml"
+
 
 class Datastore:
-    """
-    Handle connections and downloading of Zenodo Source archives
-    """
+    """Handle connections and downloading of Zenodo Source archives."""
 
     def __init__(self, loglevel="WARNING", verbose=False, sandbox=False):
         """
-        Create a ZenDownloader instance
+        Datastore manages file retrieval for PUDL datasets.
+
+        It pulls files from Zenodo archives as needed, and caches them
+        locally.
 
         Args:
             loglevel: str, logging level
             verbose: boolean. If true, logs printed to stdout
             sandbox: boolean. If true, use the sandbox server instead of
                      production
-
-        Returns:
-            ZenDownloader instance
         """
         logger = logging.Logger(__name__)
         logger.setLevel(loglevel)
@@ -68,13 +70,13 @@ class Datastore:
             self.token = TOKEN["production"]
             self.api_root = "https://zenodo.org/api"
 
-        with open(os.path.expanduser("~/.pudl.yml"), "r") as f:
+        with PUDL_YML.open() as f:
             cfg = yaml.load(f.read(), Loader=yaml.FullLoader)
-            self.pudl_in = os.environ.get("PUDL_IN", cfg["pudl_in"])
+            self.pudl_in = Path(os.environ.get("PUDL_IN", cfg["pudl_in"]))
 
     def doi(self, dataset):
         """
-        Produce the DOI for a given dataset, or log & raise error
+        Produce the DOI for a given dataset, or log & raise error.
 
         Args:
             datapackage: the name of the datapackage
@@ -90,6 +92,27 @@ class Datastore:
             self.logger.error(msg)
             raise ValueError(msg)
 
+    def doi_to_url(self, doi):
+        """
+        Given a DOI, produce the API url to retrieve it.
+
+        Args:
+            doi: str, the doi (concept doi) to retrieve, per
+                 https://help.zenodo.org/
+
+        Returns:
+            url to get the deposition from the api
+        """
+        match = re.search(r"zenodo.([\d]+)", doi)
+
+        if match is None:
+            msg = "Invalid doi %s" % doi
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        zen_id = int(match.groups()[0])
+        return "%s/deposit/depositions/%d" % (self.api_root, zen_id)
+
     def local_path(self, dataset, filename=None):
         """
         Produce the local path for a given dataset.
@@ -102,18 +125,77 @@ class Datastore:
         Return:
             str: a path
         """
-        doi_path = re.sub("/", "-", self.doi(dataset))
-        directory = os.path.join(self.pudl_in, dataset, "data", doi_path)
+        doi_dirname = re.sub("/", "-", self.doi(dataset))
+        directory = self.pudl_in / dataset / "data" / doi_dirname
 
         if filename is None:
             return directory
 
-        return os.path.join(directory, filename)
+        return directory / filename
+
+    def remote_datapackage_json_text(self, doi):
+        """
+        Produce the contents of a remote datapackage.json.
+
+        Args:
+            doi: the DOI
+
+        Returns:
+            string contents of the datapackage.json file as available on
+            Zenodo, or raises an error
+        """
+        dpkg_url = self.doi_to_url(doi)
+        response = requests.get(
+            dpkg_url, params={"access_token": self.token})
+
+        if response.status_code > 299:
+            msg = "Failed to retrieve %s" % dpkg_url
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        jsr = response.json()
+        files = {x["filename"]: x for x in jsr["files"]}
+
+        response = requests.get(
+            files["datapackage.json"]["links"]["download"],
+            params={"access_token": self.token})
+
+        if response.status_code > 299:
+            msg = "Failed to retrieve datapackage for %s: %s" % (
+                doi, response.text)
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        return response.text
+
+    def datapackage_json(self, dataset, force_download=False):
+        """
+        Produce the contents of datapackage.json, cache as necessary.
+
+        Args:
+            dataset (str): name of the dataset, must be available in the DOIS
+
+        Return:
+            dict representation of the datapackage.json
+        """
+        path = self.local_path(dataset, filename="datapackage.json")
+
+        if force_download or not path.exists():
+            doi = self.doi(dataset)
+            dpkg = self.remote_datapackage_json_text(doi)
+            path.parent.mkdir(parents=True, exist_ok=True)
+
+            with path.open("w") as f:
+                f.write(dpkg)
+
+            self.logger.debug("Cached %s", path)
+
+        with path.open("r") as f:
+            return yaml.load(f.read(), Loader=yaml.FullLoader)
 
     def collect(self, dataset, filters=None):
         """
-        Download the files from the provided dataset to the appropriate
-        PUDL_IN directory, coordinating other methods as needed
+        Download dataset files to PUDL_IN.
 
         Args:
             dataset (str): name of the dataset, must be available in the DOIS
@@ -141,81 +223,9 @@ class Datastore:
                 local_path = self.download_resource(r, output_dir)
                 self.logger.info("Downloaded %s" % local_path)
 
-    def datapackage_contents(self, dataset, force_download=False):
-        """
-        Produce the contents of the datapackage.json file, caching locally as
-        necessary
-
-        Args:
-            dataset (str): name of the dataset, must be available in the DOIS
-
-        Return:
-            dict representation of the datapackage.json
-        """
-        def download_datapackage_json(doi, path):
-            dpkg_url = self.doi_to_url(doi)
-            response = requests.get(
-                dpkg_url, params={"access_token": self.token})
-
-            if response.status_code > 299:
-                msg = "Failed to retrieve %s" % dpkg_url
-                self.logger.error(msg)
-                raise ValueError(msg)
-
-            jsr = response.json()
-            files = {x["filename"]: x for x in jsr["files"]}
-
-            response = requests.get(
-                files["datapackage.json"]["links"]["download"],
-                params={"access_token": self.token})
-
-            if response.status_code > 299:
-                msg = "Failed to retrieve datapackage for %s: %s" % (
-                    doi, response.text)
-                self.logger.error(msg)
-                raise ValueError(msg)
-
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-
-            with open(path, "w") as f:
-                f.write(response.text)
-
-            self.logger.debug("Datapackage cached: %s" % path)
-            return path
-            # ------------------------------------------------------
-
-        path = self.local_path(dataset, filename="datapackage.json")
-
-        if force_download or not os.path.exists(path):
-            download_datapackage_json(self.doi(dataset), path)
-
-        with open(path, "r") as f:
-            return yaml.load(f.read(), Loader=yaml.FullLoader)
-
-    def doi_to_url(self, doi):
-        """
-        Given a DOI, produce the API url to retrieve it
-
-        Args:
-            doi: str, the doi (concept doi) to retrieve, per
-                 https://help.zenodo.org/
-
-        Returns:
-            url to get the deposition from the api
-        """
-        match = re.search(r"zenodo.([\d]+)", doi)
-
-        if match is None:
-            msg = "Invalid doi %s" % doi
-            self.logger.error(msg)
-            raise ValueError(msg)
-
-        zen_id = int(match.groups()[0])
-        return "%s/deposit/depositions/%d" % (self.api_root, zen_id)
-
     def passes_filters(self, resource, filters):
         """
-        Test whether file metadata passes given filters
+        Test whether file metadata passes given filters.
 
         Args:
             resource: dict, a "resource" descriptior from a frictionless
@@ -239,7 +249,7 @@ class Datastore:
 
     def download_resource(self, resource, output_dir):
         """
-        Download a frictionless datapackage resource
+        Download a frictionless datapackage resource.
 
         Args:
             resource: dict, a "resource" descriptior from a frictionless
@@ -249,6 +259,7 @@ class Datastore:
         Returns:
             str, path to the locally saved resource, or None on failure
         """
+        raise NotImplementedError("REDO")
         local_path = os.path.join(output_dir, resource["name"])
         response = requests.get(
             resource["path"], params={"access_token": self.token})
@@ -263,92 +274,3 @@ class Datastore:
             self.logger.debug("Downloaded %s", local_path)
 
         return local_path
-
-
-def main_arguments():
-    """
-    Parse the command line arguments.
-
-    Args: None
-    Returns: args object
-    """
-
-    parser = argparse.ArgumentParser(
-        description="Download PUDL source data from Zenodo archives",
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    parser.add_argument("--year", type=int, help="Limit results by (4 digit) year")
-    parser.add_argument("--month", type=int,
-                        help="Limit results by (2 digit) month")
-    parser.add_argument("--state", type=str,
-                        help="Limit results by (2 char) US state")
-
-    parser.add_argument("--sandbox", action="store_const", const=True,
-                        default=False,
-                        help="Use sandbox instead of production sources")
-    parser.add_argument("--verbose", action="store_const",
-                        const=True, default=False, help="Log to stdout")
-    parser.add_argument("--loglevel", type=str, default="WARNING",
-                        help="Set log level")
-    parser.add_argument(
-        "archive", type=str,
-        help="Dataset to download, or 'list' to see what's available.")
-
-    return parser.parse_args()
-
-
-def available_archives():
-    """
-    List available sources, as found in the ~/.pudl.yml file
-
-    Args: None
-    Returns: str, describe available sources
-    """
-    cfg = ZenodoDownload.load_config()
-
-    try:
-        sandboxes = ", ".join(cfg["zenodo_download"]["sandbox"]["dois"].keys())
-    except:
-        sandboxes = "None"
-
-    try:
-        production = ", ".join(cfg["zenodo_download"]["production"]["dois"].keys())
-    except:
-        production = "None"
-
-    return """Available Archives
-------------------
-
- - Production: %s
- - Sandbox: %s
-""" % (production, sandboxes)
-
-
-def main():
-    args = main_arguments()
-
-    if args.archive == "list":
-        print(available_archives())
-        return 0
-
-    filters = {}
-
-    if getattr(args, "year", None) is not None:
-        filters["year"] = args.year
-
-    if getattr(args, "month", None) is not None:
-        filters["month"] = args.month
-
-    if getattr(args, "state", None) is not None:
-        filters["state"] = args.state
-
-    zen_dl = ZenodoDownload(loglevel=args.loglevel, verbose=args.verbose,
-                            sandbox=args.sandbox)
-
-    zen_dl.collect(args.archive, filters=filters)
-    zen_dl.logger.debug("Done")
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())
--- test/datastore_test.py
+++ test/datastore_test.py
@@ -1,17 +1,43 @@
-"""
-Exercise the functionality in the datastore management module.
-"""
+"""Exercise the functionality in the datastore management module."""
 
+import json
 import random
 import pudl.workspace.datastore as datastore
 
+
 class TestDatastore:
+    """Test the Datastore functions."""
+
     ds = datastore.Datastore(sandbox=True)
 
     def test_doi_to_url(self):
-        """Get the DOI url right"""
+        """Get the DOI url right."""
         number = random.randint(100000, 999999)
         fake_doi = "10.5072/zenodo.%d" % number
 
         assert self.ds.doi_to_url(fake_doi) == \
             "https://sandbox.zenodo.org/api/deposit/depositions/%d" % number
+
+    def test_all_datapackage_json_available(self):
+        """
+        Ensure that every recorded DOI has a datapackage.json file.
+
+        Integration test!
+        """
+
+        def test_it(client, doi):
+            text = client.remote_datapackage_json_text(doi)
+            jsr = json.loads(text)
+
+            assert "title" in jsr, "No title for %s" % doi
+            assert "resources" in jsr, "No resources for %s" % doi
+            assert jsr["profile"] == "data-package", \
+                "Incorrect profile for %s" % doi
+
+        # Yes, you could nest these too, but it's too clever for testing
+        for _, doi in datastore.DOI["sandbox"].items():
+            test_it(self.ds, doi)
+
+        prod = datastore.Datastore()
+        for _, doi in datastore.DOI["production"].items():
+            test_it(prod, doi)
