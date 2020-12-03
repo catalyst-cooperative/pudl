d84d919617a8df9e2e854783856f1f5a27ea2495
Christina Gosnell <cgosnell@catalyst.coop>
2020-12-03 17:36:41 -0500

    clean some datastore tests....

--- test/etl_test.py
+++ test/etl_test.py
@@ -184,31 +184,68 @@ class TestFerc1Datastore:
 class TestExcelExtractor:
     """Verify that we can lead excel files as provided via the datastore."""
 
+    @staticmethod
+    def test_expected_file_name(extractor, page, year, expected_name):
+        """Check if extractor can access files with expected file names."""
+        if extractor.excel_filename(page, year=year) != expected_name:
+            raise AssertionError(
+                f"file name for {page} in {year} doesn't match datastore."
+            )
+
     def test_excel_filename_eia860(self, pudl_datastore_fixture):
         """Spot check eia860 extractor gets the correct excel sheet names."""
         extractor = pudl.extract.eia860.Extractor(pudl_datastore_fixture)
-        assert extractor.excel_filename(
-            "boiler_generator_assn", year=2011) == "EnviroAssocY2011.xlsx"
-        assert extractor.excel_filename(
-            "generator_retired", year=2016) == "3_1_Generator_Y2016.xlsx"
-        assert extractor.excel_filename(
-            "utility", year=2018) == "1___Utility_Y2018.xlsx"
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='boiler_generator_assn',
+            year=2011,
+            expected_name="EnviroAssocY2011.xlsx"
+        )
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='generator_retired',
+            year=2016,
+            expected_name="3_1_Generator_Y2016.xlsx"
+        )
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='utility',
+            year=2018,
+            expected_name="1___Utility_Y2018.xlsx"
+        )
 
     def test_excel_filename_eia923(self, pudl_datastore_fixture):
         """Spot check eia923 extractor gets the correct excel sheet names."""
         extractor = pudl.extract.eia923.Extractor(pudl_datastore_fixture)
-        assert extractor.excel_filename("plant_frame", year=2009) == \
-            "EIA923 SCHEDULES 2_3_4_5 M Final 2009 REVISED 05252011.XLS"
-        assert extractor.excel_filename("energy_storage", year=2019) == \
-            "EIA923_Schedules_2_3_4_5_M_12_2019_Final.xlsx"
-        assert extractor.excel_filename("puerto_rico", year=2012) == \
-            "EIA923_Schedules_2_3_4_5_M_12_2012_Final_Revision.xlsx"
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='plant_frame',
+            year=2009,
+            expected_name="EIA923 SCHEDULES 2_3_4_5 M Final 2009 REVISED 05252011.XLS"
+        )
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='energy_storage',
+            year=2019,
+            expected_name="EIA923_Schedules_2_3_4_5_M_12_2019_Final.xlsx"
+        )
+        self.test_expected_file_name(
+            extractor=extractor,
+            page='puerto_rico',
+            year=2012,
+            expected_name="EIA923_Schedules_2_3_4_5_M_12_2012_Final_Revision.xlsx"
+        )
 
     def test_extract_eia860(self, pudl_datastore_fixture):
         """Spot check extraction of eia860 excel files."""
         extractor = pudl.extract.eia860.Extractor(pudl_datastore_fixture)
-        assert "Ownership" in extractor.load_excel_file(
-            page="ownership", year=2018).sheet_names
+        page = 'ownership'
+        year = 2018
+        if "Ownership" not in extractor.load_excel_file(
+                page="ownership", year=2018).sheet_names:
+            raise AssertionError(
+                f"page {page} not found in datastore for {year}"
+            )
 
     def test_extract_eia923(self, pudl_datastore_fixture):
         """Spot check extraction eia923 excel files."""
