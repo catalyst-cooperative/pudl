f814d398199f755bcb1213528a58a14e8eff3132
Zane Selvans <zane.selvans@catalyst.coop>
2021-01-04 20:14:02 -0600

    Use compatible version notation for dependencies.

--- setup.py
+++ setup.py
@@ -7,28 +7,28 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 install_requires = [
-    "addfips>=0.3,<0.4",
-    "coloredlogs>=15.0,<16.0",
-    "contextily>=1.0,<2.0",
-    "datapackage>=1.11,<2.0",
-    "catalystcoop.dbfread>=3.0,<4.0",
-    "geopandas>=0.8.1,<0.9",
-    "goodtables-pandas-py>=0.2,<0.3",
-    "matplotlib>=3.0,<4.0",
-    "networkx>=2.2,<3.0",
-    "numpy>=1.19,<2.0",
-    "pandas>=1.2,<2.0",
-    "pyarrow>=2.0,<3.0",
-    "pyyaml>=5.0,<6.0",
-    "scikit-learn>=0.20,<1.0",
-    "scipy>=1.6,<2.0",
-    "seaborn>=0.11,<0.12",
-    "sqlalchemy>=1.3,<2.0",
-    "tableschema>=1.12.3,<2.0",
-    "tableschema-sql>=1.3.1,<2.0",
-    "timezonefinder>=5.0,<6.0",
-    "tqdm>=4.0,<5.0",
-    "xlsxwriter>=1.3,<2.0",
+    "addfips~=0.3.0",
+    "coloredlogs~=15.0",
+    "contextily~=1.0",
+    "datapackage~=1.11",
+    "catalystcoop.dbfread~=3.0",
+    "geopandas~=0.8.1",
+    "goodtables-pandas-py~=0.2.0",
+    "matplotlib~=3.0",
+    "networkx~=2.2",
+    "numpy~=1.19",
+    "pandas~=1.2",
+    "pyarrow~=2.0",
+    "pyyaml~=5.0",
+    "scikit-learn~=0.24",
+    "scipy~=1.6",
+    "seaborn~=0.11.1",
+    "sqlalchemy~=1.3",
+    "tableschema~=1.12",
+    "tableschema-sql~=1.3",
+    "timezonefinder~=5.0",
+    "tqdm~=4.0",
+    "xlsxwriter~=1.3",
 ]
 
 # We are installing the PUDL module to build the docs, but the C libraries
@@ -36,32 +36,32 @@ install_requires = [
 # from the installed dependencies here, and mock it for import in docs/conf.py
 # using the autodoc_mock_imports parameter:
 if not os.getenv("READTHEDOCS"):
-    install_requires.append("python-snappy>=0.5.4,<0.6")
+    install_requires.append("python-snappy~=0.5.4")
 
 doc_requires = [
-    "doc8>=0.8,<0.9",
-    "sphinx>=3.0,<4.0",
-    "sphinx-issues>=1.2,<2.0",
-    "sphinx_rtd_theme>=0.5,<0.6",
+    "doc8~=0.8.0",
+    "sphinx~=3.0",
+    "sphinx-issues~=1.2",
+    "sphinx_rtd_theme~=0.5.0",
 ]
 
 test_requires = [
-    "bandit>=1.6,<2.0",
-    "coverage>=5.3,<6.0",
-    "doc8>=0.8,<0.9",
-    "flake8>=3.8,<4.0",
-    "flake8-builtins>=1.5,<2.0",
-    "flake8-colors>=0.1,<0.2",
-    "flake8-docstrings>=1.5,<2.0",
+    "bandit~=1.6",
+    "coverage~=5.3",
+    "doc8~=0.8.0",
+    "flake8~=3.8",
+    "flake8-builtins~=1.5",
+    "flake8-colors~=0.1.0",
+    "flake8-docstrings~=1.5",
     "flake8-rst-docstrings",
-    "flake8-use-fstring>=1.0,<2.0",
-    "mccabe>=0.6,<0.7",
-    "nbval>=0.9,<1.0",
-    "pep8-naming>=0.11,<0.12",
-    "pre-commit>=2.9,<3.0",
-    "pydocstyle>=5.1,<6.0",
-    "pytest>=6.0,<7.0",
-    "pytest-cov>=2.10,<3.0",
+    "flake8-use-fstring~=1.0",
+    "mccabe~=0.6.0",
+    "nbval~=0.9",
+    "pep8-naming~=0.11.0",
+    "pre-commit~=2.9",
+    "pydocstyle~=5.1",
+    "pytest~=6.2",
+    "pytest-cov~=2.10",
 ]
 
 
