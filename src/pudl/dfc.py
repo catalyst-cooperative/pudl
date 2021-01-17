b42a3ae2e3a6f097d918eb17ea19282d2c931937
Jan Rous <rousik@gmail.com>
2021-01-17 10:49:48 -0800

    Add DataFrameCollection implementation.

---
+++ src/pudl/dfc.py
@@ -0,0 +1,154 @@
+"""Implemenation of DataFrameCollection.
+
+Pudl ETL needs to exchange collections of named tables (pandas.DataFrame)
+between ETL tasks and the volume of data contained in these tables can
+far exceed the memory of a single machine.
+
+Prefect framework currently caches task results in-memory and this can
+lead to out of memory problem, especially when dealing with large datasets
+(e.g. during the full data release). To alleviate this problem, prefect
+team recommends passing "references" to actual data that is stored separately.
+
+DataFrameCollection does just this. It keeps lightweight references to named
+data frames and stores the data either locally or on cloud storage (we use
+pandas.to_pickle method which supports these various storage backends out of
+the box).
+
+Think of DataFrameCollection as a dict-like structure backed by a disk.
+"""
+
+import logging
+import os
+import uuid
+from pathlib import Path
+from typing import Dict, Iterator, List, Optional, Tuple
+
+import pandas as pd
+import prefect
+from prefect import task
+
+logger = logging.getLogger(__name__)
+
+
+class DataFrameCollection:
+    """This class can hold named pandas.DataFrame that are stored on disk or GCS.
+
+    This should be used whenever dictionaries of named pandas.DataFrames are passed
+    between prefect tasks. Due to the implicit in-memory caching of task results it
+    is important to keep the in-memory footprint of the exchanged data small.
+
+    This wrapper achieves this by maintaining references to tables that themselves
+    are stored on a persistent medium such as local disk of GCS bucket.
+
+    This is intended to be used from within prefect flows and new instances
+    can be configured by setting relevant prefect.context variables.
+    """
+
+    def __init__(
+            self,
+            storage_path: Optional[str] = None,
+            **data_frames: Dict[str, pd.DataFrame]):
+        """Initializes DataFrameCollection with the given set of DataFrames."""
+        storage_path = storage_path or prefect.context.get(
+            "data_frame_storage_path", None)
+        if not storage_path:
+            raise AssertionError(
+                "data_frame_storage_path needs to be set in prefect.context "
+                "in order to instantiate DataFrameCollection.")
+
+        self._storage_path = storage_path.rstrip("/")
+        self._instance_id = uuid.uuid1()
+        self._table_ids = {}  # type: Dict[str, uuid.UUID]
+
+        for name, data in data_frames.items():
+            if not isinstance(data, pd.DataFrame):
+                raise ValueError(f"Value of data frame {name} is not pandas.DataFrame.")
+        for name, data in data_frames.items():
+            self.store(name, data)
+
+    def _get_filename(self, name: str, table_id: uuid.UUID) -> str:
+        """Returns filename where the given dataframe shoudl be stored."""
+        return f"{self._storage_path}/{table_id.hex}/{name}"
+
+    def get(self, name: str) -> pd.DataFrame:
+        """Returns the content of the named dataframe."""
+        try:
+            return pd.read_pickle(self._get_filename(name, self._table_ids[name]))
+        except KeyError:
+            raise KeyError(f"Table {name} not found in the collection.")
+
+    def store(self, name: str, data: pd.DataFrame):
+        """Adds named dataframe to collection and stores its contents on disk."""
+        filename = self._get_filename(name, self._instance_id)
+        if not filename.startswith("gs://"):
+            # Do not make directories when dealing with remote storage.
+            # TODO(rousik): this is fairly crude solution and won't work
+            # for non gcs remote storage.
+            Path(os.path.dirname(filename)).mkdir(exist_ok=True, parents=True)
+        data.to_pickle(filename)
+        self._table_ids[name] = self._instance_id
+
+    def add_reference(self, name: str, table_id: uuid.UUID):
+        """Adds reference to a named dataframe to this collection.
+
+        This assumes that the data is already present on disk.
+        """
+        self._table_ids[name] = table_id
+
+    def __getitem__(self, name: str) -> pd.DataFrame:
+        """Allows accessing dataframes via self[name]."""
+        return self.get(name)
+
+    def __setitem__(self, name: str, data: pd.DataFrame):
+        """Allows adding dataframes via self[name] = value."""
+        return self.store(name, data)
+
+    def items(self) -> Iterator[Tuple[str, pd.DataFrame]]:
+        """Iterates over table names and the corresponding pd.DataFrame objects."""
+        for name in self.get_table_names():
+            yield (name, self.get(name))
+
+    def get_table_names(self) -> List[str]:
+        """Returns sorted list of dataframes that are contained in this collection."""
+        return sorted(set(self._table_ids))
+
+    def get_table_ids(self) -> Dict[str, uuid.UUID]:
+        """Returns dict mapping dataframe names to their uuid identifiers."""
+        return dict(self._table_ids)
+
+    @staticmethod
+    def make_from_dict(d: Dict[str, pd.DataFrame]):
+        """Constructs new DataFrameCollection from dataframe dictionary."""
+        return DataFrameCollection(**d)
+
+    def to_dict(self) -> Dict[str, pd.DataFrame]:
+        """Loads the entire collection to memory as a dictionary."""
+        return dict(self.items())
+
+    def update(self, other):
+        """Adds references to tables from the other DataFrameCollection."""
+        # TODO(rousik): typecheck other?
+        for name, table_id in other.get_table_ids().items():
+            self.add_reference(name, table_id)
+
+    def union(self, *others):
+        """Returns new DataFrameCollection that is union of self and others."""
+        # TODO(rousik): annotation for others: List[DataFrameCollection] does not work due
+        # to python idiosyncracies. There is a way but it is a weird one using generics.
+        dfc = DataFrameCollection()
+        dfc.update(self)
+        for col in others:
+            dfc.update(col)
+        return dfc
+
+
+@task
+def merge(left: DataFrameCollection, right: DataFrameCollection):
+    """Merges two DataFrameCollection instances."""
+    return left.union(right)
+
+
+@task
+def merge_list(list_of_dfc: List[DataFrameCollection]):
+    """Merges list of DataFrameCollection instancs."""
+    return DataFrameCollection().union(*list_of_dfc)
---
+++ src/pudl/dfc_test.py
@@ -0,0 +1,154 @@
+"""Unit tests for pudl.dfc module."""
+import shutil
+import tempfile
+import unittest
+
+import pandas as pd
+import prefect
+
+from pudl import dfc
+from pudl.dfc import DataFrameCollection
+
+
+class TestDataFrameCollection(unittest.TestCase):
+    """Unit tests for pudl.dfc.DataFrameCollection class."""
+
+    def setUp(self):
+        """Creates temporary directory for dataframe storage."""
+        self.temp_dir = tempfile.mkdtemp()
+        self.dfc = DataFrameCollection(storage_path=self.temp_dir)
+        prefect.context.data_frame_storage_path = self.temp_dir
+
+    def tearDown(self):
+        """Removes temporary directory."""
+        shutil.rmtree(self.temp_dir)
+
+    def test_empty_dfc(self):
+        """Tests that new DataFrameCollection is devoid of content."""
+        self.assertEqual([], self.dfc.get_table_names())
+        self.assertEqual([], list(self.dfc.get_table_ids()))
+        self.assertEqual([], list(self.dfc.items()))
+
+    def test_get_throws_key_error(self):
+        """Getting frame that does not exist results in KeyError."""
+        self.assertRaises(KeyError, self.dfc.get, "unknown_df")
+
+    def test_simple_df_retrieval(self):
+        """Adds single dataframe to DFC and retrieves it."""
+        df_in = pd.DataFrame({'x': [1, 2], 'y': [3, 4]})
+        self.dfc.store('test_table', df_in)
+        self.assertEqual(['test_table'], self.dfc.get_table_names())
+
+        df_out = self.dfc.get('test_table')
+        pd.testing.assert_frame_equal(df_in, df_out)
+
+        items = list(self.dfc.items())
+        self.assertEqual(1, len(items))
+
+        table_name, df_out = items[0]
+        self.assertEqual("test_table", table_name)
+        pd.testing.assert_frame_equal(df_in, df_out)
+
+    def test_add_reference(self):
+        """Tests that dataframes can be added to collection by reference."""
+        extra_dfc = DataFrameCollection(storage_path=self.temp_dir)
+
+        test_df = pd.DataFrame({"x": [1, 2, 3]})
+        extra_dfc.store("test", test_df)
+        self.assertEqual([], self.dfc.get_table_names())
+
+        self.dfc.add_reference("test", extra_dfc.get_table_ids()["test"])
+        self.assertEqual(["test"], self.dfc.get_table_names())
+        pd.testing.assert_frame_equal(test_df, self.dfc.get("test"))
+
+    def test_two_dfc_do_not_collide(self):
+        """Test that two dfc storing the same df will not collide."""
+        dfc1 = DataFrameCollection(storage_path=self.temp_dir)
+        dfc2 = DataFrameCollection(storage_path=self.temp_dir)
+
+        dfc1.store("test", pd.DataFrame({"t1": [1]}))
+        dfc2.store("test", pd.DataFrame({"t2": [1]}))
+
+        t1_out = dfc1.get("test")
+        t2_out = dfc2.get("test")
+        self.assertEqual(["t1"], t1_out.columns)
+        self.assertEqual(["t2"], t2_out.columns)
+
+    def test_union_of_two(self):
+        """Union of two DFCs contains DataFrames from both."""
+        dfc1 = DataFrameCollection(storage_path=self.temp_dir)
+        dfc2 = DataFrameCollection(storage_path=self.temp_dir)
+
+        first = pd.DataFrame({'x': [1]})
+        second = pd.DataFrame({'y': [2]})
+
+        dfc1.store("first", first)
+        dfc2.store("second", second)
+
+        final_dfc = dfc1.union(dfc2)
+        self.assertEqual(["first"], dfc1.get_table_names())
+        self.assertEqual(["second"], dfc2.get_table_names())
+        self.assertEqual(["first", "second"], final_dfc.get_table_names())
+        pd.testing.assert_frame_equal(first, final_dfc.get("first"))
+        pd.testing.assert_frame_equal(second, final_dfc.get("second"))
+
+    def test_to_dict(self):
+        """Tests that dictionaries containing dataframes are correctly built from DFCs."""
+        first = pd.DataFrame({'x': [1]})
+        second = pd.DataFrame({'y': [2]})
+
+        self.dfc.store("first", first)
+        self.dfc.store("second", second)
+
+        d = self.dfc.to_dict()
+        self.assertEqual(["first", "second"], sorted(d))
+        pd.testing.assert_frame_equal(first, d["first"])
+        pd.testing.assert_frame_equal(second, d["second"])
+
+    def test_make_from_dict(self):
+        """Tests that DFCs can be built from dictionaries."""
+        first = pd.DataFrame({'x': [1]})
+        second = pd.DataFrame({'y': [2]})
+
+        test_dfc = DataFrameCollection.make_from_dict(
+            {"first": first, "second": second})
+        self.assertEqual(["first", "second"], test_dfc.get_table_names())
+        pd.testing.assert_frame_equal(first, test_dfc.get("first"))
+        pd.testing.assert_frame_equal(second, test_dfc.get("second"))
+
+    def test_make_from_dict_fails_with_invalid_types(self):
+        """If something other than pd.DataFrame is passed DFC should not be created."""
+        self.assertRaises(
+            ValueError,
+            DataFrameCollection.make_from_dict,
+            {"first": pd.DataFrame(), "second": "just a string"})
+
+    def test_merge_task(self):
+        """Test that dfc.merge prefect task works as expected."""
+        @prefect.task
+        def dfc_from_dict(df_name, d):
+            return DataFrameCollection(**{df_name: pd.DataFrame(d)})
+
+        l_df_dict = {"x": [1]}
+        r_df_dict = {"y": [2]}
+        with prefect.Flow("test") as f:
+            l_result = dfc_from_dict("left", l_df_dict)
+            r_result = dfc_from_dict('right', r_df_dict)
+            out_dfc_result = dfc.merge(l_result, r_result)
+
+        final = f.run().result[out_dfc_result].result
+        self.assertEqual(["left", "right"], final.get_table_names())
+        pd.testing.assert_frame_equal(pd.DataFrame(l_df_dict), final.get("left"))
+        pd.testing.assert_frame_equal(pd.DataFrame(r_df_dict), final.get("right"))
+
+    def test_merge_list_task(self):
+        """Test that dfc.merge_list task works as expected."""
+        @prefect.task
+        def named_empty_df(df_name):
+            return DataFrameCollection(**{df_name: pd.DataFrame()})
+
+        with prefect.Flow("test") as f:
+            names = ["a", "b", "c", "d"]
+            res = dfc.merge_list(named_empty_df.map(names))
+        final = f.run().result[res].result
+        self.assertEqual(["a", "b", "c", "d"], final.get_table_names())
