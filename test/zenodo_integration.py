e25d1bb4fce0d72cca011053d0c506d2694b9f71
Zane Selvans <zane.selvans@catalyst.coop>
2021-01-22 20:27:51 -0600

    Debug Docker build, ignore Zenodo network exceptions

--- test/zenodo_integration.py
+++ test/zenodo_integration.py
@@ -1,6 +1,7 @@
 """Integration tests that verify that zenodo datapackages are correct."""
 
 import pytest
+from requests.exceptions import ConnectionError
 from urllib3.exceptions import MaxRetryError
 
 from pudl.workspace.datastore import Datastore
@@ -9,7 +10,7 @@ from pudl.workspace.datastore import Datastore
 class TestZenodoDatapackages:
     """Ensure both prod & sandbox Datastores point to valid datapackage descriptors."""
 
-    @pytest.mark.xfail(raises=MaxRetryError)
+    @pytest.mark.xfail(raises=(MaxRetryError, ConnectionError))
     def test_sandbox_datapackages(self):
         """All datasets point to valid descriptors and each specifies non-zero resources."""
         ds = Datastore(sandbox=True)
@@ -17,7 +18,7 @@ class TestZenodoDatapackages:
             desc = ds.get_datapackage_descriptor(dataset)
             assert list(desc.get_resources())
 
-    @pytest.mark.xfail(raises=MaxRetryError)
+    @pytest.mark.xfail(raises=(MaxRetryError, ConnectionError))
     def test_prod_datapackages(self):
         """All datasets point to valid descriptors and each specifies non-zero resources."""
         ds = Datastore(sandbox=False)
