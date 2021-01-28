4e4a72db275d12175478054ae58dce19cd64beea
rousik <rousik@gmail.com>
2021-01-28 16:30:00 +0000

    Add bokeh as an install dependency (needed by dask)

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from setuptools import find_packages, setup
 
 install_requires = [
     "addfips~=0.3.0",
+    "bokeh>=0.13.0",
     "catalystcoop.dbfread~=3.0",
     "coloredlogs~=15.0",
     "contextily~=1.0",
