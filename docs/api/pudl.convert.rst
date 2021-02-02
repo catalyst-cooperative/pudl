7d0574c3e39afa9b60ef9efdecb577966761950c
Austen Sharpe <>
2021-02-02 12:20:57 -0700

    add function to put all description tables into one rst file. Fix write function so that it successfully overrides old versions of the build.

--- docs/api/pudl.convert.rst
+++ docs/api/pudl.convert.rst
@@ -7,7 +7,6 @@ Submodules
 .. toctree::
    :maxdepth: 4
 
-   pudl.convert.datapkg_to_rst
    pudl.convert.datapkg_to_sqlite
    pudl.convert.epacems_to_parquet
    pudl.convert.ferc1_to_sqlite
--- src/pudl/convert/datapkg_to_rst.py
+++ src/pudl/convert/datapkg_to_rst.py
@@ -9,6 +9,10 @@ serve to extract the field names, field data types, and field descriptions of
 each pudl table and outputs them in a manner that automatically updates the
 read-the-docs.
 
+datapky2rst_many() reads the metadata json file, extracts the afformentioned
+data, and puts it into an rst file with tags and titles for each of the unique
+tables. This way, these tags can be referenced and appear in other rst files.
+
 """
 
 import json
@@ -21,22 +25,62 @@ metadata = open(path)
 metadata_json = json.load(metadata)
 
 
-def datapkg2rst(table):
-    """Convert json metadata to rst files."""
-    template = Template(rst_template)
-    rendered = template.render(
-        metadata_json, table=table).strip('\n') + ('\n')
+def datapkg2rst_many():
+    """Convert json metadata to a single rst file."""
+    template = Template(rst_template_many)
+    rendered = template.render(metadata_json)
+    print(rendered)
+    rendered
     # Create or overwrite an rst file containing the field descriptions of the input table
-    with open('docs/data_sources/file.rst', 'w') as f:
+    with open('docs/data_sources/metadata.rst', 'w') as f:
+        f.seek(0)  # Used to overwrite exisiting content
         f.write(rendered)
-    f.close()
+        f.truncate()  # Used to overwrite exisiting content
 
 
-rst_template = '''
+def datapkg2rst_one(table):
+    """Convert json metadata to multiple rst files."""
+    template = Template(rst_template_one)
+    rendered = template.render(metadata_json, table=table).strip('\n') + ('\n')
+    # Create or overwrite an rst file containing the field descriptions of the input table
+    with open(f'docs/data_sources/{table}.rst', 'w') as f:
+        f.seek(0)  # Used to overwrite exisiting content
+        f.write(rendered)
+        f.truncate()  # Used to overwrite exisiting content
+
+# ----------------------------------------------------------------------------
+# T E M P L A T E S
+# ----------------------------------------------------------------------------
+
+
+# Template for all tables in one rst file
+rst_template_many = '''
+{% for resource in resources %}
+.. {{ resource.name }}:
+
+Contents of {{ resource.name }} table
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. list-table::
+
+  * - **Field**
+    - **Data Type**
+    - **Description**
+{% for field in resource.schema.fields %}
+  * - {{ field.name }}
+    - {{ field.type }}{% if field.description %}
+    - {{ field.description }}{% else %}
+    - N/A{% endif %}
+{% endfor %}
+{% endfor %}
+'''
+
+# Template for one table per rst file
+rst_template_one = '''
 {% for resource in resources %}
 {% if resource.name == table %}
 Contents of {{ resource.name }}
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 .. list-table::
    :widths: 15 10 30
    :header-rows: 1
