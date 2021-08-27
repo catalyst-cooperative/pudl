19b1888c0b5bb37b95c3b5f4831e54506a9750da
Zane Selvans <zane.selvans@catalyst.coop>
2021-08-27 11:47:11 -0500

    Add real docstrings to globals set in helpers

--- src/pudl/helpers.py
+++ src/pudl/helpers.py
@@ -27,15 +27,20 @@ from pudl import constants as pc
 
 logger = logging.getLogger(__name__)
 
-# This is a little abbreviated function that allows us to propagate the NA
-# values through groupby aggregations, rather than using inefficient lambda
-# functions in each one.
 sum_na = partial(pd.Series.sum, skipna=False)
+"""
+A sum function that returns NA if the Series includes any NA values.
 
-# Initializing this TimezoneFinder opens a bunch of geography files and holds
-# them open for efficiency. I want to avoid doing that for every call to find
-# the timezone, so this is global.
-tz_finder = timezonefinder.TimezoneFinder()
+In many of our aggregations we need to override the default behavior of treating
+NA values as if they were zero. E.g. when calculating the heat rates of
+generation units, if there are some months where fuel consumption is reported
+as NA, but electricity generation is reported normally, then the fuel
+consumption for the year needs to be NA, otherwise we'll get unrealistic heat
+rates.
+"""
+
+TZ_FINDER = timezonefinder.TimezoneFinder()
+"""A global TimezoneFinder to cache geographies in memory for faster access."""
 
 
 def download_zip_url(url, save_path, chunk_size=128):
@@ -777,10 +782,10 @@ def find_timezone(*, lng=None, lat=None, state=None, strict=True):
 
     """
     try:
-        tz = tz_finder.timezone_at(lng=lng, lat=lat)
+        tz = TZ_FINDER.timezone_at(lng=lng, lat=lat)
         if tz is None:  # Try harder
             # Could change the search radius as well
-            tz = tz_finder.closest_timezone_at(lng=lng, lat=lat)
+            tz = TZ_FINDER.closest_timezone_at(lng=lng, lat=lat)
     # For some reason w/ Python 3.6 we get a ValueError here, but with
     # Python 3.7 we get an OverflowError...
     except (OverflowError, ValueError):
