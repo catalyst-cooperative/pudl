eb2089fe48feee93c59642c7d39c4d0d166decf8
Austen Sharpe <austensharpe@gmail.com>
2021-09-08 15:57:49 -0600

    add output table that combines all ferc tables: steam, small_gens, hydro, pumped_storage

--- src/pudl/output/ferc1.py
+++ src/pudl/output/ferc1.py
@@ -1,8 +1,12 @@
 """Functions for pulling FERC Form 1 data out of the PUDL DB."""
+import logging
+
 import numpy as np
 import pandas as pd
 
 import pudl
+
+logger = logging.getLogger(__name__)
 
 
 def plants_utils_ferc1(pudl_engine):
@@ -148,8 +152,12 @@ def plants_small_ferc1(pudl_engine):
     plants_small_df = (
         pd.read_sql_table("plants_small_ferc1", pudl_engine)
         .drop(['id'], axis="columns")
-        .merge(pd.read_sql_table("utilities_ferc1", pudl_engine),
-               on="utility_id_ferc1")
+        .merge(plants_utils_ferc1(pudl_engine),
+               on=['utility_id_ferc1', 'plant_name_ferc1'])
+        .assign(
+            capacity_factor=lambda x:
+                x.net_generation_mwh / (8760 * x.capacity_mw),
+            opex_fuel_per_mwh=lambda x: x.opex_fuel / x.net_generation_mwh)
         .pipe(pudl.helpers.organize_cols, ['report_year',
                                            'utility_id_ferc1',
                                            'utility_id_pudl',
@@ -168,6 +176,8 @@ def plants_hydro_ferc1(pudl_engine):
         .drop(['id'], axis="columns")
         .merge(plants_utils_ferc1(pudl_engine),
                on=["utility_id_ferc1", "plant_name_ferc1"])
+        .assign(capacity_factor=lambda x: (
+            x.net_generation_mwh / (8760 * x.capacity_mw)))
         .pipe(pudl.helpers.organize_cols, ["report_year",
                                            "utility_id_ferc1",
                                            "utility_id_pudl",
@@ -185,6 +195,8 @@ def plants_pumped_storage_ferc1(pudl_engine):
         .drop(['id'], axis="columns")
         .merge(pudl.output.ferc1.plants_utils_ferc1(pudl_engine),
                on=["utility_id_ferc1", "plant_name_ferc1"])
+        .assign(capacity_factor=lambda x:
+                x.net_generation_mwh / (8760 * x.capacity_mw))
         .pipe(pudl.helpers.organize_cols, ["report_year",
                                            "utility_id_ferc1",
                                            "utility_id_pudl",
@@ -226,3 +238,77 @@ def plant_in_service_ferc1(pudl_engine):
                                            "amount_type"])
     )
     return pis_df
+
+
+def all_plants_ferc1(pudl_engine):
+    """Combine the steam, small generators, hydro, and pumped storage tables.
+
+    While this table may have many purposes, the main one is to prepare it for
+    integration with the EIA Master Unit List (MUL). All subtables included in this
+    output table must have pudl ids. Table prepping involves ensuring that
+    the individual tables can merge correctly (like columns have the same name)
+    both with each other and the EIA MUL.
+
+    """
+    logger.info("loading steam table")
+    steam_df = plants_steam_ferc1(pudl_engine)
+    logger.info("loading small gens table")
+    small_df = plants_small_ferc1(pudl_engine)
+    logger.info("loading hydro table")
+    hydro_df = plants_hydro_ferc1(pudl_engine)
+    logger.info("loading pumped storage table")
+    pump_df = plants_pumped_storage_ferc1(pudl_engine)
+    logger.info("loading fbp table")
+    fbp_df = fuel_by_plant_ferc1(pudl_engine)
+
+    # Prep steam table (merge with fbp)
+    logger.info("prepping steam table")
+    fbp_cols_to_use = [
+        'report_year', 'utility_id_ferc1', 'plant_name_ferc1',
+        'utility_id_pudl', 'fuel_cost', 'fuel_mmbtu',
+        'primary_fuel_by_mmbtu']
+    steam_fbp_df = (
+        pd.merge(
+            steam_df,
+            fbp_df[fbp_cols_to_use],
+            on=['report_year',
+                'utility_id_ferc1',
+                'utility_id_pudl',
+                'plant_name_ferc1'],
+            how='left')
+        .rename(columns={'opex_plants': 'opex_plant'})
+        .pipe(pudl.helpers.convert_cols_dtypes,
+              'ferc1', 'ferc1 plant records'))
+
+    if 0.9 > (len(steam_fbp_df) /
+              len(steam_fbp_df.drop_duplicates(
+                  subset=['report_year',
+                          'utility_id_pudl',
+                          'plant_id_ferc1'])) < 1.1):
+        raise AssertionError(
+            'Merge issue w/ pudl_out.plants_steam_ferc1 and fbp_ferc1')
+
+    # Prep hydro tables (Add this to the meta data later)
+    logger.info("prepping hydro tables")
+    hydro_df = hydro_df.rename(columns={'project_num': 'ferc_license_id'})
+    pump_df = pump_df.rename(columns={'project_num': 'ferc_license_id'})
+
+    # Combine all the tables together
+    logger.info("combining all tables")
+    all_plants_df = (
+        pd.concat([steam_fbp_df, small_df, hydro_df, pump_df])
+        .rename(columns={
+            'fuel_cost': 'total_fuel_cost',
+            'fuel_mmbtu': 'total_mmbtu',
+            'opex_fuel_per_mwh': 'fuel_cost_per_mwh',
+            'primary_fuel_by_mmbtu': 'fuel_type_code_pudl',
+            'record_id': 'record_id_ferc1'})
+        .assign(
+            fuel_cost_per_mmbtu=lambda x: (
+                x.total_fuel_cost / x.total_mmbtu),
+            heat_rate_mmbtu_mwh=lambda x: (
+                x.total_mmbtu / x.net_generation_mwh))
+        .replace({'': np.nan})
+    )
+
+    return all_plants_df
