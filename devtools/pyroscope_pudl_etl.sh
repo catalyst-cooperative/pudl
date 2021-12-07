b6829523e7645cf7aefe24c9d38aa803e1b49f2f
Zane Selvans <zane.selvans@catalyst.coop>
2021-12-07 15:41:28 -0600

    Make pyspy profile output location more robust

--- devtools/pyroscope_pudl_etl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-pyroscope exec --log-level info --spy-name pyspy \
-    --application-name pudl ferc1_to_sqlite --clobber $1 && \
-pyroscope exec --log-level info --spy-name pyspy \
-    --application-name pudl pudl_etl --clobber $1
--- devtools/pyspy_pudl_etl.sh
+++ devtools/pyspy_pudl_etl.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
-mkdir -p devtools/profiles/
 
-py-spy record -r 10 -f speedscope -o "devtools/profiles/$2" -- pudl_etl --clobber $1
+# This script takes two positional arguments:
+# * The first one is the path to the PUDL settings file
+
+PROFILE_DIR=`git rev-parse --show-toplevel`/devtools/profiles
+OUTFILE=pyspy_pudl_etl_`date +%Y%m%d%H%M%S`.json
+mkdir -p $PROFILE_DIR
+
+py-spy record -r 10 -f speedscope -o "$PROFILE_DIR/$OUTFILE" -- pudl_etl --clobber $1
