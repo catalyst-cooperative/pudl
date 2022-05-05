89faca53bfd3a47bb5648f1d9e47735e13aacdd7
bendnorman <bdn29@cornell.edu>
2022-05-05 14:32:13 -0800

    Add gcs cache args to pudl_etl script

--- docker/pudl_etl.sh
+++ docker/pudl_etl.sh
@@ -5,9 +5,13 @@ pudl_setup \
     --pudl_out $CONTAINER_PUDL_OUT
 ferc1_to_sqlite \
     --clobber \
+    --gcs-cache-path gs://zenodo-cache.catalyst.coop \
+    --bypass-local-cache \
     --logfile $LOGFILE \
     $PUDL_SETTINGS_YML
 pudl_etl \
     --clobber \
+    --gcs-cache-path gs://zenodo-cache.catalyst.coop \
+    --bypass-local-cache \
     --logfile $LOGFILE \
     $PUDL_SETTINGS_YML
