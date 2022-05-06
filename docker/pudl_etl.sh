d9684f0154f5d43499e3b5fce5bb4221b26695bc
bendnorman <bdn29@cornell.edu>
2022-05-06 14:09:07 -0800

    Add vm shutdown code

--- docker/pudl_etl.sh
+++ docker/pudl_etl.sh
@@ -15,3 +15,10 @@ pudl_etl \
     --bypass-local-cache \
     --logfile $LOGFILE \
     $PUDL_SETTINGS_YML
+
+# Shut down the deploy-pudl-vm instance when the etl is done.
+ACCESS_TOKEN=`curl \
+    "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
+    -H "Metadata-Flavor: Google" | jq -r '.access_token'`
+
+curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://compute.googleapis.com/compute/v1/projects/catalyst-cooperative-pudl/zones/us-central1-a/instances/deploy-pudl-vm/stop
