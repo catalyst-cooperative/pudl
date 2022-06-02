0851ec1ae2c9df49562c217c8b130b59f8ccbc48
bendnorman <bdn29@cornell.edu>
2022-06-02 11:59:51 -0800

    Create separate action for dev and tags

--- docker/pudl_etl.sh
+++ docker/pudl_etl.sh
@@ -28,7 +28,7 @@ function run_pudl_etl() {
     && pytest \
         --gcs-cache-path gs://zenodo-cache.catalyst.coop \
         --bypass-local-cache \
-        --etl-settings src/pudl/package_data/settings/etl_full.yml \
+        --etl-settings $PUDL_SETTINGS_YML \
         --live-dbs test
 }
 
@@ -36,12 +36,12 @@ function shutdown_vm() {
     gsutil -m cp -r $CONTAINER_PUDL_OUT "gs://pudl-etl-logs/$GITHUB_SHA-$GITHUB_REF"
 
     echo "Shutting down VM."
-    # # Shut down the deploy-pudl-vm instance when the etl is done.
-    # ACCESS_TOKEN=`curl \
-    #     "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
-    #     -H "Metadata-Flavor: Google" | jq -r '.access_token'`
+    # # Shut down the vm instance when the etl is done.
+    ACCESS_TOKEN=`curl \
+        "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
+        -H "Metadata-Flavor: Google" | jq -r '.access_token'`
 
-    # curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://compute.googleapis.com/compute/v1/projects/catalyst-cooperative-pudl/zones/us-central1-a/instances/deploy-pudl-vm/stop
+    curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://compute.googleapis.com/compute/v1/projects/catalyst-cooperative-pudl/zones/us-central1-a/instances/$GCE_INSTANCE/stop
 }
 
 function notify_slack() {
