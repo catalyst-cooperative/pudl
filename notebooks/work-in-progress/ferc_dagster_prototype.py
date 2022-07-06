52370c2b329acd06c6f74ca04ec8f35c970ccf13
bendnorman <bdn29@cornell.edu>
2022-07-06 10:51:12 -0800

    Add dependency between fuel and steam

--- notebooks/work-in-progress/ferc_dagster_prototype.py
+++ notebooks/work-in-progress/ferc_dagster_prototype.py
@@ -3,18 +3,44 @@ from dagster import GraphOut, graph, in_process_executor, job, op
 
 
 @op
-def extract():
-    return {"steam": pd.DataFrame(), "hydro": pd.DataFrame()}
+def extract_dbf():
+    return {
+        "steam": pd.DataFrame(),
+        "hydro": pd.DataFrame(),
+        "small_plants": pd.DataFrame(),
+        "fuel": pd.DataFrame(),
+    }
 
 
 @op
-def transform_steam(df):
-    return df["steam"]
+def extract_xbrl():
+    return {
+        "steam": pd.DataFrame(),
+        "hydro": pd.DataFrame(),
+        "small_plants": pd.DataFrame(),
+        "fuel": pd.DataFrame(),
+    }
 
 
 @op
-def transform_hydro(df):
-    return df["hydro"]
+def transform_steam(dbf_raw_dfs, xbrl_raw_dfs, transformed_fuel_table):
+    print(f"Do something with transformed_fuel_table: {transformed_fuel_table.shape}")
+    return pd.concat([dbf_raw_dfs["steam"], xbrl_raw_dfs["steam"]])
+
+
+@op
+def transform_fuel(dbf_raw_dfs, xbrl_raw_dfs):
+    return pd.concat([dbf_raw_dfs["fuel"], xbrl_raw_dfs["fuel"]])
+
+
+@op
+def transform_hydro(dbf_raw_dfs, xbrl_raw_dfs):
+    return pd.concat([dbf_raw_dfs["hydro"], xbrl_raw_dfs["hydro"]])
+
+
+@op
+def transform_small_plants(dbf_raw_dfs, xbrl_raw_dfs):
+    return pd.concat([dbf_raw_dfs["small_plants"], xbrl_raw_dfs["small_plants"]])
 
 
 @op
@@ -22,21 +48,39 @@ def load(name):
     print(f"Loading {name}")
 
 
-ferc1_tfr_funcs = {"steam": transform_steam, "hydro": transform_hydro}
+ferc1_tfr_funcs = {
+    "steam": transform_steam,
+    "fuel": transform_fuel,
+    "hydro": transform_hydro,
+    "small_plants": transform_small_plants,
+}
 
 
 @graph(out={name: GraphOut() for name in ferc1_tfr_funcs})
-def transform(raw_dfs):
+def transform(dbf_raw_dfs, xbrl_raw_dfs):
     transformed_dfs = {}
-    for table in ferc1_tfr_funcs:
-        transformed_dfs[table] = ferc1_tfr_funcs[table](raw_dfs)
+
+    # Define dependencies between tables. We don't have to pass around the
+    # transform_dfs dictionary if we specify the dependencies using dagster.
+    transformed_dfs["fuel"] = transform_fuel(dbf_raw_dfs, xbrl_raw_dfs)
+    transformed_dfs["steam"] = transform_steam(
+        dbf_raw_dfs, xbrl_raw_dfs, transformed_dfs["fuel"]
+    )
+
+    # Transform tables that don't have dependencies
+    for table_name, transform_func in ferc1_tfr_funcs.items():
+        # If the table hasn't been transformed, transform it!
+        if table_name not in transformed_dfs:
+            transformed_dfs[table_name] = transform_func(dbf_raw_dfs, xbrl_raw_dfs)
+
     return transformed_dfs
 
 
 @graph
 def etl_ferc1():
-    raw_dfs = extract()
-    transformed_dfs = transform(raw_dfs)
+    dbf_raw_dfs = extract_dbf()
+    xbrl_raw_dfs = extract_xbrl()
+    transformed_dfs = transform(dbf_raw_dfs, xbrl_raw_dfs)
     for table in transformed_dfs:
         load(table)
 
