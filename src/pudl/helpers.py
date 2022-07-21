74877b17e2bca6b9bd3994d45a6ab13cf7411f2d
Austen Sharpe <austensharpe@gmail.com>
2022-07-21 12:38:59 -0600

    Make remove_leading_zeros_from_numeric_strings() helper function slightly better

--- src/pudl/helpers.py
+++ src/pudl/helpers.py
@@ -894,12 +894,12 @@ def remove_leading_zeros_from_numeric_strings(
 
     """
     if col_name in df.columns:
-        leading_zeros = df[col_name].str.contains(r"^0+\d+$")
+        leading_zeros = df[col_name].str.contains(r"^0+\d+$").fillna(False)
         if leading_zeros.any():
             logger.debug(f"Fixing leading zeros in {col_name} column")
-            df.loc[df[col_name].notna() & leading_zeros, col_name] = df[
-                col_name
-            ].str.replace(r"^0+", "", regex=True)
+            df.loc[leading_zeros, col_name] = df[col_name].str.replace(
+                r"^0+", "", regex=True
+            )
         else:
             logger.debug(f"Found no numeric leading zeros in {col_name}")
     return df
