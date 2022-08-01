4b703589847186983d06aa7eda3f812fff6cfe38
Zane Selvans <zane.selvans@catalyst.coop>
2022-08-01 09:28:30 -0500

    Update eia860m Zenodo DOI. Merge in dev.

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -158,7 +158,7 @@ class ZenodoFetcher:
         "production": {
             "censusdp1tract": "10.5281/zenodo.4127049",
             "eia860": "10.5281/zenodo.5534934",
-            "eia860m": "10.5281/zenodo.6910008",
+            "eia860m": "10.5281/zenodo.6929086",
             "eia861": "10.5281/zenodo.5602102",
             "eia923": "10.5281/zenodo.5596977",
             "epacems": "10.5281/zenodo.4660268",
