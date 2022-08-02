bea0481ab73e09675302021b171330bb81a28272
Zane Selvans <zane.selvans@catalyst.coop>
2022-08-02 08:40:11 -0500

    Require doc8 1.0 or greater to avoid Windows install issue.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
             "twine>=3.3,<4.1",
         ],
         "doc": [
-            "doc8>=0.9,<1.1",
+            "doc8>=1.0,<1.1",
             "furo>=2022.4.7",
             "sphinx>=4,!=5.1.0,<5.1.2",
             "sphinx-autoapi>=1.8,<1.10",
@@ -95,7 +95,7 @@ setup(
         "test": [
             "bandit>=1.6,<1.8",
             "coverage>=5.3,<6.5",
-            "doc8>=0.9,<1.1",
+            "doc8>=1.0,<1.1",
             "flake8>=4.0,<5.1",
             "flake8-builtins>=1.5,<1.6",
             "flake8-colors>=0.1,<0.2",
