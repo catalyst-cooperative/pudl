10878cabbf248ed4de328baa372362ca968441ff
zschira <zach.schira@catalyst.coop>
2022-08-11 10:18:40 -0400

    Update xbrl-extractor dependency to install from git (development only)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=[
         "addfips>=0.3.1,<0.4.0",
         "catalystcoop.dbfread>=3.0,<3.1",
-        "catalystcoop.ferc_xbrl_extractor==0.2.1",
+        "catalystcoop.ferc_xbrl_extractor @ git+https://github.com/catalyst-cooperative/ferc-xbrl-extractor.git@dev",
         "coloredlogs>=15.0,<15.1",
         "dask>=2021.8,<2022.7.2",
         "datapackage>=1.11,<1.16",  # Transition datastore to use frictionless.
@@ -143,7 +143,7 @@ setup(
             "censusdp1tract_to_sqlite = pudl.convert.censusdp1tract_to_sqlite:main",
             "metadata_to_rst = pudl.convert.metadata_to_rst:main",
             "epacems_to_parquet = pudl.convert.epacems_to_parquet:main",
-            "ferc1_to_sqlite = pudl.convert.ferc1_to_sqlite:main",
+            "ferc_to_sqlite = pudl.convert.ferc_to_sqlite:main",
             "datasette_metadata_to_yml = pudl.convert.datasette_metadata_to_yml:main",
             "pudl_datastore = pudl.workspace.datastore:main",
             "pudl_etl = pudl.cli:main",
