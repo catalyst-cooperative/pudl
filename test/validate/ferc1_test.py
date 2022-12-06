acf2923e6352330ba04229dde5a430967b639c69
cbz <cgosnell@catalyst.coop>
2022-12-06 13:55:45 -0500

    remove table from record_id valition test

--- test/validate/ferc1_test.py
+++ test/validate/ferc1_test.py
@@ -21,6 +21,7 @@ non_unique_record_id_tables = [
     "purchased_power_ferc1",
     "electric_energy_account_sources_ferc1",
     "electric_energy_dispositions_ferc1",
+    "utility_plant_summary_ferc1",
 ]
 unique_record_tables = [
     t
