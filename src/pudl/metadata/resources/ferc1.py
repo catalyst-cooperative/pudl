525b5d77652df84b100982b2b62bc10fa2c5f1b1
Zane Selvans <zane.selvans@catalyst.coop>
2022-12-14 22:20:57 -0600

    Add missing record_id fields for energy source/disposition tables.

--- src/pudl/metadata/resources/ferc1.py
+++ src/pudl/metadata/resources/ferc1.py
@@ -32,6 +32,7 @@ RESOURCE_METADATA: dict[str, dict[str, Any]] = {
                 "energy_source_type",
                 "row_type_xbrl",
                 "energy_mwh",
+                "record_id",
             ],
             "primary_key": [
                 "utility_id_ferc1",
@@ -52,6 +53,7 @@ RESOURCE_METADATA: dict[str, dict[str, Any]] = {
                 "energy_disposition_type",
                 "row_type_xbrl",
                 "energy_mwh",
+                "record_id",
             ],
             "primary_key": [
                 "utility_id_ferc1",
