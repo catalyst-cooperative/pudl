0d07ea1024cdf7c17e2a9acc0c9f007ee68b8e41
Zane Selvans <zane.selvans@catalyst.coop>
2022-12-20 10:16:14 -0600

    Fail gracefully when there's no local cache path

--- src/pudl/metadata/classes.py
+++ src/pudl/metadata/classes.py
@@ -947,9 +947,14 @@ class DataSource(Base):
 
     def add_datastore_metadata(self) -> None:
         """Get source file metadata from the datastore."""
+        pudl_settings = pudl.workspace.setup.get_defaults()
+        if pudl_settings["pudl_in"] is None:
+            local_cache_path = None
+        else:
+            local_cache_path = pudl_settings["data_dir"]
         dp_desc = Datastore(
             sandbox=False,
-            local_cache_path=pudl.workspace.setup.get_defaults()["data_dir"],
+            local_cache_path=local_cache_path,
             gcs_cache_path="gs://zenodo-cache.catalyst.coop",
         ).get_datapackage_descriptor(self.name)
         partitions = dp_desc.get_partitions()
