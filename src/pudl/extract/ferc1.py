f43f964b61524b4ead007e418e2f4d11684411f3
zschira <zasc3143@colorado.edu>
2022-12-22 19:06:33 -0500

    Add other_regulatory_liabilities_ferc1

--- src/pudl/extract/ferc1.py
+++ src/pudl/extract/ferc1.py
@@ -169,6 +169,10 @@ TABLE_NAME_MAP: dict[str, dict[str, str]] = {
         "dbf": "f1_dacs_epda",
         "xbrl": "summary_of_depreciation_and_amortization_charges_section_a_336",
     },
+    "other_regulatory_liabilities_ferc1": {
+        "dbf": "f1_othr_reg_liab",
+        "xbrl": "summary_of_depreciation_and_amortization_charges_section_a_336",
+    },
 }
 """A mapping of PUDL DB table names to their XBRL and DBF source table names."""
 
