a3623d15e84cabef77063242dc58e5c2a1a4472a
Dazhong Xia <dazhong.xia@catalyst.coop>
2023-02-01 15:30:28 -0500

    Add TF config for workload identity

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "project_id" {
+  type    = string
+  default = "catalyst-cooperative-pudl"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-east1"
+  zone    = "us-east1-c"
+}
+
+module "gh_oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id  = var.project_id
+  pool_id     = "gh-actions-pool"
+  provider_id = "gh-actions-provider"
+  sa_mapping = {
+    "tox-pytest-github-action-service-account" = {
+      sa_name   = "projects/${var.project_id}/serviceAccounts/tox-pytest-github-action@catalyst-cooperative-pudl.iam.gserviceaccount.com"
+      attribute = "*"
+    }
+  }
+}
