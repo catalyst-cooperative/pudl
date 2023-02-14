c48584edc45ac6f78c33739d71065742516d2b45
Kurt Nordback <kurt.nordback@protonmail.com>
2023-02-13 18:18:10 -0700

    Remove plant_name_eia in pudl.transform.eia923.fuel_receipts_costs()

--- src/pudl/transform/eia923.py
+++ src/pudl/transform/eia923.py
@@ -1072,7 +1072,6 @@ def fuel_receipts_costs(eia923_dfs, eia923_transformed_dfs):
     # Drop fields we're not inserting into the fuel_receipts_costs_eia923
     # table.
     cols_to_drop = [
-        "plant_name_eia",
         "plant_state",
         "operator_name",
         "operator_id",
