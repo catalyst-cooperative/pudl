aa888f2b10ab5976e1fbee2d2514eb44199cc736
Dazhong Xia <dazhong.xia@catalyst.coop>
2023-02-27 18:32:51 -0500

    Docker changes to get local_pudl_etl.sh working

--- docker/local_pudl_etl.sh
+++ docker/local_pudl_etl.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
 # This script runs the entire ETL and validation tests in a docker container.
 # It is mostly used for local debugging of our docker deployment and the gcp_pudl_etl.sh script.
-function authenticate_gcp() {
-    export GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/service_account_key.json
-    # Set the default gcloud project id so the zenodo-cache bucket
-    # knows what project to bill for egress
-    gcloud config set project ${GCP_BILLING_PROJECT:?err}
-}
+
+set -eux
 
 function bridge_settings() {
     export PUDL_CACHE="${CONTAINER_PUDL_IN}/data"
@@ -14,28 +10,19 @@ function bridge_settings() {
 }
 
 function run_pudl_etl() {
-    authenticate_gcp \
-    && pudl_setup \
+    pudl_setup \
         --pudl_in $CONTAINER_PUDL_IN \
         --pudl_out $CONTAINER_PUDL_OUT \
     && bridge_settings \
     && ferc_to_sqlite \
-        --clobber \
         --loglevel DEBUG \
-        --gcs-cache-path gs://zenodo-cache.catalyst.coop \
         $PUDL_SETTINGS_YML \
     && pudl_etl \
-        --clobber \
         --loglevel DEBUG \
-        --gcs-cache-path gs://zenodo-cache.catalyst.coop \
         $PUDL_SETTINGS_YML \
     && epacems_to_parquet \
-        --partition \
-        --clobber \
         --loglevel DEBUG \
-        --gcs-cache-path gs://zenodo-cache.catalyst.coop \
     && pytest \
-        --gcs-cache-path gs://zenodo-cache.catalyst.coop \
         --etl-settings $PUDL_SETTINGS_YML \
         --live-dbs test
 }
