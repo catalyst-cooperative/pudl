fb94981bd73da8ae8cdc3a8e889309cacc6edf9d
zschira <zach.schira@catalyst.coop>
2023-03-08 10:53:11 -0500

    Use epacems partition option properly

--- src/pudl/etl/epacems_assets.py
+++ src/pudl/etl/epacems_assets.py
@@ -1,4 +1,6 @@
 """EPA CEMS Hourly Emissions assets."""
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 from pathlib import Path
 
 import pandas as pd
@@ -9,8 +11,35 @@ from dagster import Field, asset
 import pudl
 from pudl.helpers import EnvVar
 from pudl.metadata.classes import Resource
+from pudl.workspace.datastore import Datastore
 
 logger = pudl.logging_helpers.get_logger(__name__)
+
+
+def _etl_one_year_epacems(
+    year: int,
+    states: list[str],
+    out_dir: str,
+    ds: Datastore,
+    epacamd_eia: pd.DataFrame,
+    plants_entity_eia: pd.DataFrame,
+) -> None:
+    """Process one year of EPA CEMS and output year-state paritioned Parquet files."""
+    schema = Resource.from_id("hourly_emissions_epacems").to_pyarrow()
+
+    for state in states:
+        with pq.ParquetWriter(
+            where=Path(out_dir) / f"epacems-{year}-{state}.parquet",
+            schema=schema,
+            compression="snappy",
+            version="2.6",
+        ) as pqwriter:
+            logger.info(f"Processing EPA CEMS hourly data for {year}-{state}")
+            df = pudl.extract.epacems.extract(year=year, state=state, ds=ds)
+            df = pudl.transform.epacems.transform(df, epacamd_eia, plants_entity_eia)
+            pqwriter.write_table(
+                pa.Table.from_pandas(df, schema=schema, preserve_index=False)
+            )
 
 
 @asset(
@@ -43,34 +72,44 @@ def hourly_emissions_epacems(
     ds = context.resources.datastore
     epacems_settings = context.resources.dataset_settings.epacems
 
-    schema = Resource.from_id("hourly_emissions_epacems").to_pyarrow()
-    partitioned_path = (
-        Path(context.op_config["pudl_output_path"]) / "hourly_emissions_epacems"
-    )
-    partitioned_path.mkdir(exist_ok=True)
-    monolithic_path = (
-        Path(context.op_config["pudl_output_path"]) / "hourly_emissions_epacems.parquet"
-    )
+    if epacems_settings.partition:
+        epacems_path = (
+            Path(context.op_config["pudl_output_path"]) / "hourly_emissions_epacems"
+        )
+        epacems_path.mkdir(exist_ok=True)
+        do_one_year = partial(
+            _etl_one_year_epacems,
+            states=epacems_settings.states,
+            out_dir=epacems_path,
+            ds=ds,
+            epacamd_eia=epacamd_eia,
+            plants_entity_eia=plants_entity_eia,
+        )
+        with ProcessPoolExecutor() as executor:
+            # Convert results of map() to list to force execution
+            _ = list(executor.map(do_one_year, epacems_settings.years))
 
-    with pq.ParquetWriter(
-        where=monolithic_path, schema=schema, compression="snappy", version="2.6"
-    ) as monolithic_writer:
-        for part in epacems_settings.partitions:
-            year = part["year"]
-            state = part["state"]
-            logger.info(f"Processing EPA CEMS hourly data for {year}-{state}")
-            df = pudl.extract.epacems.extract(year=year, state=state, ds=ds)
-            df = pudl.transform.epacems.transform(df, epacamd_eia, plants_entity_eia)
-            table = pa.Table.from_pandas(df, schema=schema, preserve_index=False)
+    else:
+        schema = Resource.from_id("hourly_emissions_epacems").to_pyarrow()
+        epacems_path = (
+            Path(context.op_config["pudl_output_path"])
+            / "hourly_emissions_epacems.parquet"
+        )
 
-            # Write to one monolithic parquet file
-            monolithic_writer.write_table(table)
-
-            # Write to a directory of partitioned parquet files
-            with pq.ParquetWriter(
-                where=partitioned_path / f"epacems-{year}-{state}.parquet",
-                schema=schema,
-                compression="snappy",
-                version="2.6",
-            ) as partitioned_writer:
-                partitioned_writer.write_table(table)
+        with pq.ParquetWriter(
+            where=str(epacems_path),
+            schema=schema,
+            compression="snappy",
+            version="2.6",
+        ) as pqwriter:
+            for part in epacems_settings.partitions:
+                year = part["year"]
+                state = part["state"]
+                logger.info(f"Processing EPA CEMS hourly data for {year}-{state}")
+                df = pudl.extract.epacems.extract(year=year, state=state, ds=ds)
+                df = pudl.transform.epacems.transform(
+                    df, epacamd_eia, plants_entity_eia
+                )
+                pqwriter.write_table(
+                    pa.Table.from_pandas(df, schema=schema, preserve_index=False)
+                )
