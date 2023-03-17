0845d85c63885593d5adcaac5ab5a9a921666c2c
Zane Selvans <zane.selvans@catalyst.coop>
2023-03-17 17:29:01 -0600

    Add type hints to analysis assets.

--- src/pudl/etl/analysis_assets.py
+++ src/pudl/etl/analysis_assets.py
@@ -16,6 +16,7 @@ different modules. Eventually I imagine these would just be the novel derived va
 probably in pretty skinny tables, which get joined / aggregated with other data in the
 denormalized tables.
 """
+import pandas as pd
 from dagster import asset
 
 import pudl
@@ -24,7 +25,7 @@ logger = pudl.logging_helpers.get_logger(__name__)
 
 
 @asset(io_manager_key="pudl_sqlite_io_manager", compute_kind="Python")
-def utility_analysis(utils_eia860):
+def utility_analysis(utils_eia860: pd.DataFrame) -> pd.DataFrame:
     """Example of how to create an analysis table that depends on an output view.
 
     This final dataframe will be written to the database (without a schema).
