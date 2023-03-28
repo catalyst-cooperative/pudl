5e20ca3e7adcd6106d70c32fbbdcc614b8b7e33c
Zane Selvans <zane.selvans@catalyst.coop>
2023-03-28 12:31:47 -0600

    Remove some bandwidth intensive download options from our chonky datasette.

--- devtools/datasette/publish.sh
+++ devtools/datasette/publish.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-PUDL_OUT=`grep "^pudl_out" $HOME/.pudl.yml | sed -e "s/^pudl_out: //"`
-SQLITE_DIR="$PUDL_OUTPUT"
-
 # make metadata.yml
 datasette_metadata_to_yml -o "metadata.yml"
+
+EXTRA_OPTIONS="--setting sql_time_limit_ms 5000"
+EXTRA_OPTIONS="$EXTRA_OPTIONS --setting allow_download off"
+EXTRA_OPTIONS="$EXTRA_OPTIONS --setting allow_csv_stream off"
 
 datasette publish cloudrun \
     --service catalyst-datasette \
@@ -13,11 +14,11 @@ datasette publish cloudrun \
     --install datasette-vega \
     --install datasette-block-robots \
     --metadata metadata.yml \
-    --extra-options="--setting sql_time_limit_ms 5000" \
-    $SQLITE_DIR/pudl.sqlite \
-    $SQLITE_DIR/ferc1.sqlite \
-    $SQLITE_DIR/ferc1_xbrl.sqlite \
-    $SQLITE_DIR/ferc2_xbrl.sqlite \
-    $SQLITE_DIR/ferc6_xbrl.sqlite \
-    $SQLITE_DIR/ferc60_xbrl.sqlite \
-    $SQLITE_DIR/ferc714_xbrl.sqlite
+    --extra-options=$EXTRA_OPTIONS \
+    $PUDL_OUTPUT/pudl.sqlite \
+    $PUDL_OUTPUT/ferc1.sqlite \
+    $PUDL_OUTPUT/ferc1_xbrl.sqlite \
+    $PUDL_OUTPUT/ferc2_xbrl.sqlite \
+    $PUDL_OUTPUT/ferc6_xbrl.sqlite \
+    $PUDL_OUTPUT/ferc60_xbrl.sqlite \
+    $PUDL_OUTPUT/ferc714_xbrl.sqlite
