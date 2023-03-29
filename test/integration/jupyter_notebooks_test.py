64370b0b022aab810e7f978d88c9c500413b61f5
Zane Selvans <zane.selvans@catalyst.coop>
2023-03-29 00:35:05 -0600

    Add devtools notebook execution to integration tests.

---
+++ test/integration/jupyter_notebooks_test.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import nbformat
+import pytest
+from nbconvert.preprocessors import ExecutePreprocessor
+from sqlalchemy.engine import Engine
+
+
+@pytest.mark.parametrize(
+    "notebook",
+    [
+        "devtools/inspect-assets.ipynb",
+        "devtools/debug-eia-etl.ipynb",
+        "devtools/debug-ferc1-etl.ipynb",
+        "devtools/debug-harvesting.ipynb",
+    ],
+)
+def test_notebook_exec(notebook: str, pudl_engine: Engine, test_dir: Path):
+    nb_path = test_dir.parent / notebook
+    with nb_path.open() as f:
+        nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        _ = ep.preprocess(nb, resources={"Application": {"log_level": 5}})
