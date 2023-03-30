d80e7c614a9cde4d762b1b48b5a4c3d0ff50645d
zschira <zach.schira@catalyst.coop>
2023-03-30 16:53:27 -0400

    Create dynamic graph for epacems asset

--- src/pudl/etl/epacems_assets.py
+++ src/pudl/etl/epacems_assets.py
@@ -3,18 +3,27 @@ from pathlib import Path
 
 import pandas as pd
 import pyarrow as pa
-import pyarrow.parquet as pq
-from dagster import Field, MultiPartitionsDefinition, StaticPartitionsDefinition, asset
+from dagster import DynamicOut, DynamicOutput, Field, graph_asset, op
 
 import pudl
 from pudl.helpers import EnvVar
 from pudl.metadata.classes import Resource
-from pudl.settings import EpaCemsSettings
 
 logger = pudl.logging_helpers.get_logger(__name__)
 
 
-@asset(
+@op(
+    out=DynamicOut(),
+    required_resource_keys={"dataset_settings"},
+)
+def get_year(context):
+    """Return set of requested years."""
+    epacems_settings = context.resources.dataset_settings.epacems
+    for year in epacems_settings.years:
+        yield DynamicOutput(year, mapping_key=str(year))
+
+
+@op(
     required_resource_keys={"datastore", "dataset_settings", "pq_writer"},
     config_schema={
         "pudl_output_path": Field(
@@ -24,23 +33,38 @@ logger = pudl.logging_helpers.get_logger(__name__)
             description="Path of directory to store the database in.",
             default_value=None,
         ),
-        "partition": Field(
-            bool,
-            description="Flag indicating whether the partitioned EPACEMS output should be created",
-            default_value=False,
-        ),
     },
-    partitions_def=MultiPartitionsDefinition(
-        {
-            "years": StaticPartitionsDefinition(
-                [str(year) for year in EpaCemsSettings().years]
-            ),
-            "states": StaticPartitionsDefinition(EpaCemsSettings().states),
-        },
-    ),
 )
+def process_single_year(
+    context,
+    year,
+    epacamd_eia: pd.DataFrame,
+    plants_entity_eia: pd.DataFrame,
+):
+    """Process a single year of epacems data."""
+    ds = context.resources.datastore
+    epacems_settings = context.resources.dataset_settings.epacems
+
+    schema = Resource.from_id("hourly_emissions_epacems").to_pyarrow()
+    partitioned_path = (
+        Path(context.op_config["pudl_output_path"]) / "hourly_emissions_epacems"
+    )
+    partitioned_path.mkdir(exist_ok=True)
+    monolithic_writer = context.resources.pq_writer
+
+    for state in epacems_settings.states:
+        logger.info(f"Processing EPA CEMS hourly data for {year}-{state}")
+        df = pudl.extract.epacems.extract(year=year, state=state, ds=ds)
+        df = pudl.transform.epacems.transform(df, epacamd_eia, plants_entity_eia)
+        table = pa.Table.from_pandas(df, schema=schema, preserve_index=False)
+
+        # Write to one monolithic parquet file
+        monolithic_writer.write_table(table)
+
+
+@graph_asset
 def hourly_emissions_epacems(
-    context, epacamd_eia: pd.DataFrame, plants_entity_eia: pd.DataFrame
+    epacamd_eia: pd.DataFrame, plants_entity_eia: pd.DataFrame
 ) -> None:
     """Extract, transform and load CSVs for EPA CEMS.
 
@@ -54,32 +78,11 @@ def hourly_emissions_epacems(
                      ORISPL code with EIA.
         plants_entity_eia: The EIA Plant entities used for aligning timezones.
     """
-    ds = context.resources.datastore
-
-    schema = Resource.from_id("hourly_emissions_epacems").to_pyarrow()
-    partitioned_path = (
-        Path(context.op_config["pudl_output_path"]) / "hourly_emissions_epacems"
+    years = get_year()
+    return years.map(
+        lambda year: process_single_year(
+            year,
+            epacamd_eia,
+            plants_entity_eia,
+        )
     )
-    partitioned_path.mkdir(exist_ok=True)
-    monolithic_writer = context.resources.pq_writer
-
-    partitions = context.partition_key.keys_by_dimension.split("|")
-    year = int(partitions[0])
-    state = partitions[1]
-    logger.info(f"Processing EPA CEMS hourly data for {year}-{state}")
-    df = pudl.extract.epacems.extract(year=year, state=state, ds=ds)
-    df = pudl.transform.epacems.transform(df, epacamd_eia, plants_entity_eia)
-    table = pa.Table.from_pandas(df, schema=schema, preserve_index=False)
-
-    # Write to one monolithic parquet file
-    monolithic_writer.write_table(table)
-
-    if context.op_config["partition"]:
-        # Write to a directory of partitioned parquet files
-        with pq.ParquetWriter(
-            where=partitioned_path / f"epacems-{year}-{state}.parquet",
-            schema=schema,
-            compression="snappy",
-            version="2.6",
-        ) as partitioned_writer:
-            partitioned_writer.write_table(table)
