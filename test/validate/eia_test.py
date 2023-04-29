17d15554a66be497f3f8c905c0500ca6fe61b1c6
Zane Selvans <zane.selvans@catalyst.coop>
2023-04-29 12:40:40 -0600

    Pull primary keys from Resource instead of Package.

--- test/validate/eia_test.py
+++ test/validate/eia_test.py
@@ -5,7 +5,7 @@ from test.conftest import skip_table_if_null_freq_table
 import pytest
 
 from pudl import validate as pv
-from pudl.metadata.classes import Package
+from pudl.metadata.classes import Resource
 from pudl.output.pudltabl import PudlTabl
 
 logger = logging.getLogger(__name__)
@@ -98,86 +98,27 @@ def test_minmax_rows(
 @pytest.mark.parametrize(
     "df_name,unique_subset",
     [
-        (
-            "bf_eia923",
-            (
-                Package.from_resource_ids()
-                .get_resource("boiler_fuel_eia923")
-                .schema.primary_key
-            ),
-        ),
+        ("bf_eia923", Resource.from_id("boiler_fuel_eia923").schema.primary_key),
         (
             "bga_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("boiler_generator_assn_eia860")
-                .schema.primary_key
-            ),
+            Resource.from_id("boiler_generator_assn_eia860").schema.primary_key,
         ),
-        (
-            "boil_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("boilers_eia860")
-                .schema.primary_key
-            ),
-        ),
-        (
-            "gen_eia923",
-            (
-                Package.from_resource_ids()
-                .get_resource("generation_eia923")
-                .schema.primary_key
-            ),
-        ),
-        (
-            "gens_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("generators_eia860")
-                .schema.primary_key
-            ),
-        ),
+        ("boil_eia860", Resource.from_id("boilers_eia860").schema.primary_key),
+        ("gen_eia923", Resource.from_id("generation_eia923").schema.primary_key),
+        ("gens_eia860", Resource.from_id("generators_eia860").schema.primary_key),
         (
             "gf_eia923",
-            (
-                Package.from_resource_ids()
-                .get_resource("denorm_generation_fuel_combined_eia923")
-                .schema.primary_key
-            ),
+            Resource.from_id(
+                "denorm_generation_fuel_combined_eia923"
+            ).schema.primary_key,
         ),
-        (
-            "own_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("ownership_eia860")
-                .schema.primary_key
-            ),
-        ),
-        (
-            "plants_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("plants_eia860")
-                .schema.primary_key
-            ),
-        ),
+        ("own_eia860", Resource.from_id("ownership_eia860").schema.primary_key),
+        ("plants_eia860", Resource.from_id("plants_eia860").schema.primary_key),
         (
             "pu_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("denorm_plants_utilities_eia")
-                .schema.primary_key
-            ),
+            Resource.from_id("denorm_plants_utilities_eia").schema.primary_key,
         ),
-        (
-            "utils_eia860",
-            (
-                Package.from_resource_ids()
-                .get_resource("utilities_eia860")
-                .schema.primary_key
-            ),
-        ),
+        ("utils_eia860", Resource.from_id("utilities_eia860").schema.primary_key),
     ],
 )
 def test_unique_rows_eia(pudl_out_eia, live_dbs, unique_subset, df_name):
