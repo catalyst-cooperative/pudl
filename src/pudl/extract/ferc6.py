37888fdac24fbdc699ca70840b85aca2b4639fdf
Jan Rous <rousik@gmail.com>
2023-05-23 14:53:07 -0600

    FERC Form 6 integration
    1. added dbf metadata files for the archive layouts
    2. added pk/fk constraint over f6_s0_respondent_id.respondent_id

---
+++ src/pudl/extract/ferc6.py
@@ -0,0 +1,26 @@
+"""Extract FERC Form 6 data from DBF archives."""
+
+import sqlalchemy as sa
+
+from pudl.extract.dbf import FercDbfExtractor
+from pudl.extract.ferc import add_key_constraints
+from pudl.settings import FercToSqliteSettings, GenericDatasetSettings
+
+
+class Ferc6DbfExtractor(FercDbfExtractor):
+    """Extracts FERC Form 2 data from the legacy DBF archives."""
+
+    DATASET = "ferc6"
+    DATABASE_NAME = "ferc6.sqlite"
+
+    def get_settings(
+        self, global_settings: FercToSqliteSettings
+    ) -> GenericDatasetSettings:
+        """Returns settings for FERC Form 1 DBF dataset."""
+        return global_settings.ferc6_dbf_to_sqlite_settings
+
+    def finalize_schema(self, meta: sa.MetaData) -> sa.MetaData:
+        """Add primary and foreign keys for respondent_id."""
+        return add_key_constraints(
+            meta, pk_table="f6_s0_respondent_id", column="respondent_id"
+        )
