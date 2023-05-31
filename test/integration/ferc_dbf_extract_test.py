1a669b043d6f00c682f58c0bdc4deef4b6841634
Jan Rous <rousik@gmail.com>
2023-05-31 15:32:26 -0600

    Generalize extraction tests to ferc2 as well.

--- test/integration/ferc_dbf_extract_test.py
+++ test/integration/ferc_dbf_extract_test.py
@@ -1,6 +1,7 @@
 """PyTest based testing of the FERC DBF Extraction logic."""
 
 import logging
+import pytest
 
 import sqlalchemy as sa
 
@@ -16,31 +17,37 @@ def test_ferc1_dbf2sqlite(ferc1_engine_dbf):
         "f1_respondent_id" in sa.inspect(ferc1_engine_dbf).get_table_names()
     )
 
+@pytest.mark.parametrize(
+    "dataset",
+    [
+        pytest.param("ferc1", id="ferc1"),
+        pytest.param("ferc2", id="ferc2"),
+    ]
+)
+def test_ferc_schema(ferc_to_sqlite_settings, pudl_datastore_fixture, dataset):
+    """Check to make sure we aren't missing any old FERC Form N tables or fields.
 
-def test_ferc1_schema(ferc_to_sqlite_settings, pudl_datastore_fixture):
-    """Check to make sure we aren't missing any old FERC Form 1 tables or fields.
-
-    Exhaustively enumerate all historical sets of FERC Form 1 database tables and their
+    Exhaustively enumerate all historical sets of FERC Form N database tables and their
     constituent fields. Check to make sure that the current database definition, based
     on the given reference year and our compilation of the DBF filename to table name
     mapping from 2015, includes every single table and field that appears in the
     historical FERC Form 1 data.
     """
-    ferc1_dbf_settings = ferc_to_sqlite_settings.ferc1_dbf_to_sqlite_settings
-    refyear = ferc1_dbf_settings.refyear
-    dbf_reader = FercDbfReader(pudl_datastore_fixture, dataset="ferc1")
+    dbf_settings = getattr(ferc_to_sqlite_settings, f"{dataset}_dbf_to_sqlite_settings")
+    refyear = dbf_settings.refyear
+    dbf_reader = FercDbfReader(pudl_datastore_fixture, dataset=dataset)
     ref_archive = dbf_reader.get_archive(year=refyear, data_format="dbf")
 
-    logger.info(f"Checking for new, unrecognized FERC1 tables in {refyear}.")
+    logger.info(f"Checking for new, unrecognized {dataset} tables in {refyear}.")
     table_schemas = ref_archive.get_db_schema()
     for table in table_schemas:
         if table not in dbf_reader.get_table_names():
             raise AssertionError(
-                f"New FERC Form 1 table '{table}' in {refyear} "
-                f"does not exist in 2015 list of tables"
+                f"New {dataset} table '{table}' in {refyear} "
+                f"does not exist in canonical list of tables"
             )
-    for yr in ferc1_dbf_settings.years:
-        logger.info(f"Searching for lost FERC1 tables and fields in {yr}.")
+    for yr in dbf_settings.years:
+        logger.info(f"Searching for lost {dataset} tables and fields in {yr}.")
         # Some early years might need part=None to eliminate split-respondent
         # strange archives, but let's assume this is not needed here for now.
         yr_archive = dbf_reader.get_archive(year=yr, data_format="dbf")
