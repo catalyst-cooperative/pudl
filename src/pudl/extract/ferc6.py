ba19a55dfae10ae64d08c23cdf7f7019904e1964
Jan Rous <rousik@gmail.com>
2023-06-16 14:43:30 +0200

    Add repondent_id deduplication logic to FERC 6

--- src/pudl/extract/ferc6.py
+++ src/pudl/extract/ferc6.py
@@ -1,8 +1,14 @@
 """Extract FERC Form 6 data from DBF archives."""
 
+import pandas as pd
 import sqlalchemy as sa
 
-from pudl.extract.dbf import FercDbfExtractor, add_key_constraints
+from pudl.extract.dbf import (
+    FercDbfExtractor,
+    PartitionedDataFrame,
+    add_key_constraints,
+    deduplicate_by_year,
+)
 from pudl.settings import FercToSqliteSettings, GenericDatasetSettings
 
 
@@ -23,3 +29,16 @@ class Ferc6DbfExtractor(FercDbfExtractor):
         return add_key_constraints(
             meta, pk_table="f6_s0_respondent_id", column="respondent_id"
         )
+
+    def aggregate_table_frames(
+        self, table_name: str, dfs: list[PartitionedDataFrame]
+    ) -> pd.DataFrame | None:
+        """Runs the deduplication on f2_s0_respondent_id table.
+
+        Other tables are aggregated as usual, meaning that the partial frames are simply
+        concatenated.
+        """
+        if table_name == "f6_s0_respondent_id":
+            return deduplicate_by_year(dfs, "respondent_id")
+        else:
+            return super().aggregate_table_frames(table_name, dfs)
