67677675430cb5721410dfea0bee5fa4bacfb31b
Zane Selvans <zane.selvans@catalyst.coop>
2023-08-01 08:57:36 -0600

    Ignore redefition of function to be returned as Callable.

--- src/pudl/metadata/helpers.py
+++ src/pudl/metadata/helpers.py
@@ -394,7 +394,7 @@ def try_aggfunc(  # noqa: C901
 
     elif not raised and error is None:
 
-        def wrapped(x):
+        def wrapped(x):  # noqa: F811
             try:
                 return func(x)
             except AggregationError as e:
@@ -402,7 +402,7 @@ def try_aggfunc(  # noqa: C901
 
     elif not raised and error is not None:
 
-        def wrapped(x):
+        def wrapped(x):  # noqa: F811
             try:
                 return func(x)
             except AggregationError as e:
