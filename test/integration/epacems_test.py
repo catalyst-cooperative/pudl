63f63d50d5bf48f8154c17c5d1225a2f8aecd8dc
Zane Selvans <zane.selvans@catalyst.coop>
2023-08-21 18:34:42 -0600

    Remove caplog test.

--- test/integration/epacems_test.py
+++ test/integration/epacems_test.py
@@ -1,6 +1,4 @@
 """Tests for pudl/output/epacems.py loading functions."""
-import logging
-
 import dask.dataframe as dd
 import pytest
 from dagster import build_init_resource_context
@@ -49,20 +47,12 @@ def test_epacems_subset(epacems_year_and_state, epacems_parquet_path):
     assert actual.shape[0].compute() > 0  # nosec: B101  n rows
 
 
-def test_epacems_missing_partition(caplog, pudl_datastore_fixture):
+def test_epacems_missing_partition(pudl_datastore_fixture):
     """Check that missing partitions return an empty data frame.
 
     Note that this should pass for both the Fast and Full ETL because the behavior
     towards a missing file is identical."""
     df = extract(year=1996, state="UT", ds=pudl_datastore_fixture)
-    with caplog.at_level(logging.DEBUG):
-        assert len(caplog.records) == 1
-        for record in caplog.records:
-            assert record.levelname == "WARNING"
-            assert (
-                record.message
-                == "No data found for UT in 1996. Returning empty dataframe."
-            )
     epacems_res = Resource.from_id("hourly_emissions_epacems")
     expected_cols = list(epacems_res.get_field_names())
     assert df.shape[0] == 0  # Check that no rows of data are there
