66044a450510790edf1d69ff436e30994787867e
zschira <zach.schira@catalyst.coop>
2023-08-24 01:40:34 -0400

    Update to use new version of ferc-xbrl-extractor

--- src/pudl/extract/xbrl.py
+++ src/pudl/extract/xbrl.py
@@ -1,14 +1,10 @@
 """Generic extractor for all FERC XBRL data."""
-import contextlib
 import io
-import json
-from datetime import date, datetime
+from datetime import date
 from pathlib import Path
 
-import sqlalchemy as sa
 from dagster import Field, Noneable, op
-from ferc_xbrl_extractor import xbrl
-from ferc_xbrl_extractor.instance import InstanceBuilder
+from ferc_xbrl_extractor.cli import run_main
 
 import pudl
 from pudl.settings import FercGenericXbrlToSqliteSettings, XbrlFormNumber
@@ -27,71 +23,25 @@ class FercXbrlDatastore:
 
     def get_taxonomy(self, year: int, form: XbrlFormNumber) -> tuple[io.BytesIO, str]:
         """Returns the path to the taxonomy entry point within the an archive."""
+        taxonomy_dates = {2021: date(2022, 1, 1), 2022: date(2022, 1, 1)}
+        taxonomy_date = taxonomy_dates[year]
         raw_archive = self.datastore.get_unique_resource(
-            f"ferc{form.value}", year=year, data_format="xbrl"
+            f"ferc{form.value}",
+            year=taxonomy_date.year,
+            data_format="xbrl_taxonomy",
         )
 
-        # Construct path to taxonomy entry point within archive
-        taxonomy_date = date(year, 1, 1).isoformat()
-        taxonomy_entry_point = f"taxonomy/form{form.value}/{taxonomy_date}/form/form{form.value}/form-{form.value}_{taxonomy_date}.xsd"
+        taxonomy_entry_point = f"taxonomy/form{form.value}/{taxonomy_date}/form/form{form.value}/form-{form.value}_{taxonomy_date.isoformat()}.xsd"
 
         return io.BytesIO(raw_archive), taxonomy_entry_point
 
-    def get_filings(self, year: int, form: XbrlFormNumber) -> list[InstanceBuilder]:
-        """Return list of filings from archive."""
-        archive = self.datastore.get_zipfile_resource(
-            f"ferc{form.value}", year=year, data_format="xbrl"
+    def get_filings(self, year: int, form: XbrlFormNumber) -> io.BytesIO:
+        """Return the corresponding archive full of XBRL filings."""
+        return io.BytesIO(
+            self.datastore.get_unique_resource(
+                f"ferc{form.value}", year=year, data_format="xbrl"
+            )
         )
-
-        # Load RSS feed metadata
-        filings = []
-        with archive.open("rssfeed") as f:
-            metadata = json.load(f)
-
-            # Loop through all filings by a given filer in a given quarter
-            # And take the most recent one
-            for key, filing_info in metadata.items():
-                latest = datetime.min
-                for filing_id, info in filing_info.items():
-                    # Parse date from 9-tuple
-                    published = datetime.fromisoformat(info["published_parsed"])
-
-                    if published > latest:
-                        latest = published
-                        latest_filing = filing_id
-
-                # Create in memory buffers with file data to be used in conversion
-                filings.append(
-                    InstanceBuilder(
-                        io.BytesIO(archive.open(f"{latest_filing}.xbrl").read()),
-                        latest_filing,
-                    )
-                )
-
-        return filings
-
-
-def _get_sqlite_engine(form_number: int, clobber: bool) -> sa.engine.Engine:
-    """Create SQLite engine for specified form and drop tables.
-
-    Args:
-        form_number: FERC form number.
-        clobber: Flag indicating whether or not to drop tables.
-    """
-    # Read in the structure of the DB, if it exists
-    logger.info(
-        f"Dropping the old FERC Form {form_number} XBRL derived SQLite DB if it exists."
-    )
-    db_path = PudlPaths().sqlite_db(f"ferc{form_number}_xbrl")
-
-    logger.info(f"Connecting to SQLite at {db_path}...")
-    sqlite_engine = sa.create_engine(db_path)
-    logger.info(f"Connected to SQLite at {db_path}!")
-    with contextlib.suppress(sa.exc.OperationalError):
-        # So that we can wipe it out
-        pudl.helpers.drop_tables(sqlite_engine, clobber=clobber)
-
-    return sqlite_engine
 
 
 @op(
@@ -113,13 +63,13 @@ def _get_sqlite_engine(form_number: int, clobber: bool) -> sa.engine.Engine:
     required_resource_keys={"ferc_to_sqlite_settings", "datastore"},
 )
 def xbrl2sqlite(context) -> None:
-    """Clone the FERC Form 1 XBRL Databsae to SQLite."""
+    """Clone the FERC Form 1 XBRL Database to SQLite."""
     output_path = PudlPaths().output_dir
     clobber = context.op_config["clobber"]
     batch_size = context.op_config["batch_size"]
     workers = context.op_config["workers"]
     ferc_to_sqlite_settings = context.resources.ferc_to_sqlite_settings
-    datastore = datastore = context.resources.datastore
+    datastore = context.resources.datastore
     datastore = FercXbrlDatastore(datastore)
 
     # Loop through all other forms and perform conversion
@@ -135,14 +85,12 @@ def xbrl2sqlite(context) -> None:
             logger.info(f"Dataset ferc{form}_xbrl is disabled, skipping")
             continue
 
-        sqlite_engine = _get_sqlite_engine(form.value, clobber)
-
         convert_form(
             settings,
             form,
             datastore,
-            sqlite_engine,
             output_path=output_path,
+            clobber=clobber,
             batch_size=batch_size,
             workers=workers,
         )
@@ -152,8 +100,8 @@ def convert_form(
     form_settings: FercGenericXbrlToSqliteSettings,
     form: XbrlFormNumber,
     datastore: FercXbrlDatastore,
-    sqlite_engine: sa.engine.Engine,
     output_path: Path,
+    clobber: bool,
     batch_size: int | None = None,
     workers: int | None = None,
 ) -> None:
@@ -163,7 +111,6 @@ def convert_form(
         form_settings: Validated settings for converting the desired XBRL form to SQLite.
         form: FERC form number.
         datastore: Instance of a FERC XBRL datastore for retrieving data.
-        sqlite_engine: SQLAlchemy connection to mirrored database.
         pudl_settings: Dictionary containing paths and database URLs
             used by PUDL.
         batch_size: Number of XBRL filings to process in a single CPU process.
@@ -176,15 +123,22 @@ def convert_form(
     metadata_path = str(output_path / f"ferc{form.value}_xbrl_taxonomy_metadata.json")
     # Process XBRL filings for each year requested
     for year in form_settings.years:
-        raw_archive, taxonomy_entry_point = datastore.get_taxonomy(year, form)
-        xbrl.extract(
-            datastore.get_filings(year, form),
-            sqlite_engine,
-            raw_archive,
-            form.value,
-            batch_size=batch_size,
-            workers=workers,
-            datapackage_path=datapackage_path,
+        taxonomy_archive, taxonomy_entry_point = datastore.get_taxonomy(year, form)
+        filings_archive = datastore.get_filings(year, form)
+
+        run_main(
+            instance_path=filings_archive,
+            sql_path=PudlPaths()
+            .sqlite_db(f"ferc{form.value}_xbrl")
+            .removeprefix("sqlite:///"),  # Temp hacky solution
+            clobber=clobber,
+            taxonomy=taxonomy_archive,
+            entry_point=taxonomy_entry_point,
+            form_number=form.value,
             metadata_path=metadata_path,
-            archive_file_path=taxonomy_entry_point,
+            datapackage_path=datapackage_path,
+            workers=workers,
+            batch_size=batch_size,
+            loglevel="INFO",
+            logfile=None,
         )
