af3f800c778a8ca5b168cef523f85537a5b84f9e
thinky <ella.belfer@catalyst.coop>
2023-09-20 18:00:18 -0400

    Fix div/0 error for fuel table

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -365,7 +365,7 @@ SOURCES: dict[str, Any] = {
         },
         "field_namespace": "ferc1",
         "working_partitions": {
-            "years": sorted(set(range(1994, 2022))),
+            "years": sorted(set(range(1994, 2023))),
         },
         "contributors": [
             CONTRIBUTORS["catalyst-cooperative"],
