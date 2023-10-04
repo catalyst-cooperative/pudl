595dc29641b1d34078f4643959cbadc26f68af10
bendnorman <bdn29@cornell.edu>
2023-10-04 08:37:45 +0200

    Rename heat_rate_mmbtu_mwh -> heat_rate_mmbtu_mwh_by_unit

--- src/pudl/analysis/plant_parts_eia.py
+++ src/pudl/analysis/plant_parts_eia.py
@@ -281,7 +281,7 @@ SUM_COLS: list[str] = [
 
 WTAVG_DICT = {
     "fuel_cost_per_mwh": "capacity_mw",
-    "heat_rate_mmbtu_mwh": "capacity_mw",
+    "heat_rate_mmbtu_mwh_by_unit": "capacity_mw",
     "fuel_cost_per_mmbtu": "capacity_mw",
 }
 """Dict: a dictionary of columns (keys) to perform weighted averages on and the weight
--- src/pudl/metadata/fields.py
+++ src/pudl/metadata/fields.py
@@ -1159,7 +1159,7 @@ FIELD_METADATA: dict[str, dict[str, Any]] = {
         "description": "The energy contained in fuel burned, measured in million BTU.",
         "unit": "MMBtu",
     },
-    "heat_rate_mmbtu_mwh": {
+    "heat_rate_mmbtu_mwh_by_unit": {
         "type": "number",
         "description": "Fuel content per unit of electricity generated. Coming from MCOE calculation.",
         "unit": "MMBtu_MWh",
