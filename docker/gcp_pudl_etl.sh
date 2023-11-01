dcfb8de3598c7f08eda774a5dc6585218166256a
bendnorman <bdn29@cornell.edu>
2023-10-31 16:49:34 -0800

    Use max cpus for nightly builds and spin dagster-storage SQL instance up and down

--- docker/gcp_pudl_etl.sh
+++ docker/gcp_pudl_etl.sh
@@ -4,6 +4,12 @@
 
 set -x
 
+function setup_dagster_storage() {
+    # Start dagster-storage Cloud SQL instance
+    gcloud sql instances patch dagster-storage --activation-policy=ALWAYS
+    # create database
+}
+
 function send_slack_msg() {
     curl -X POST -H "Content-type: application/json" -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/chat.postMessage --data "{\"channel\": \"C03FHB9N0PQ\", \"text\": \"$1\"}"
 }
@@ -21,6 +27,7 @@ function authenticate_gcp() {
 function run_pudl_etl() {
     send_slack_msg ":large_yellow_circle: Deployment started for $ACTION_SHA-$GITHUB_REF :floppy_disk:"
     authenticate_gcp \
+    && setup_dagster_storage \
     && alembic upgrade head \
     && pudl_setup \
     && ferc_to_sqlite \
@@ -30,7 +37,6 @@ function run_pudl_etl() {
         $PUDL_SETTINGS_YML \
     && pudl_etl \
         --loglevel DEBUG \
-        --max-concurrent 6 \
         --gcs-cache-path gs://internal-zenodo-cache.catalyst.coop \
         $PUDL_SETTINGS_YML \
     && pytest \
@@ -44,6 +50,10 @@ function shutdown_vm() {
     gsutil -m cp -r $PUDL_OUTPUT "gs://nightly-build-outputs.catalyst.coop/$ACTION_SHA-$GITHUB_REF"
 
     upload_file_to_slack $LOGFILE "pudl_etl logs for $ACTION_SHA-$GITHUB_REF:"
+
+    # Delete dagster-storage database
+    # Start dagster-storage Cloud SQL instance
+    gcloud sql instances patch dagster-storage --activation-policy=NEVER
 
     echo "Shutting down VM."
     # # Shut down the vm instance when the etl is done.
