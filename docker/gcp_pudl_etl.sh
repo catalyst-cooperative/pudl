ac31f1ec0ae1dbc5976581be6ea3cc5162df90ec
Zane Selvans <zane.selvans@catalyst.coop>
2023-12-20 13:43:55 -0600

    Clean up GCP PUDL ETL script
    - Remove ad-hoc test of nightly build tagging
    - Use "$DOUBPLE_QUOTES" around envvars to prevent globbing/split words
    - Use $BUILD_ID consistently when referring to nightly build outputs.
    - Update name of epacems directory that we remove before deployment.

--- docker/gcp_pudl_etl.sh
+++ docker/gcp_pudl_etl.sh
@@ -3,7 +3,7 @@
 # This script won't work locally because it needs adequate GCP permissions.
 
 # Set PUDL_GCS_OUTPUT *only* if it is currently unset
-: "${PUDL_GCS_OUTPUT:=gs://nightly-build-outputs.catalyst.coop/$ACTION_SHA-$GITHUB_REF}"
+: "${PUDL_GCS_OUTPUT:=gs://nightly-build-outputs.catalyst.coop/$BUILD_ID}"
 
 set -x
 
@@ -12,64 +12,64 @@ function send_slack_msg() {
 }
 
 function upload_file_to_slack() {
-    curl -F file=@$1 -F "initial_comment=$2" -F channels=C03FHB9N0PQ -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/files.upload
+    curl -F "file=@$1" -F "initial_comment=$2" -F channels=C03FHB9N0PQ -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/files.upload
 }
 
 function authenticate_gcp() {
     # Set the default gcloud project id so the zenodo-cache bucket
     # knows what project to bill for egress
-    gcloud config set project $GCP_BILLING_PROJECT
+    gcloud config set project "$GCP_BILLING_PROJECT"
 }
 
 function run_pudl_etl() {
-    send_slack_msg ":large_yellow_circle: Deployment started for $ACTION_SHA-$GITHUB_REF :floppy_disk:"
+    send_slack_msg ":large_yellow_circle: Deployment started for $BUILD_ID :floppy_disk:"
     authenticate_gcp && \
     alembic upgrade head && \
     ferc_to_sqlite \
         --loglevel DEBUG \
         --gcs-cache-path gs://internal-zenodo-cache.catalyst.coop \
         --workers 8 \
-        $PUDL_SETTINGS_YML \
+        "$PUDL_SETTINGS_YML" \
     && pudl_etl \
         --loglevel DEBUG \
         --gcs-cache-path gs://internal-zenodo-cache.catalyst.coop \
-        $PUDL_SETTINGS_YML \
+        "$PUDL_SETTINGS_YML" \
     && pytest \
         -n auto \
         --gcs-cache-path gs://internal-zenodo-cache.catalyst.coop \
-        --etl-settings $PUDL_SETTINGS_YML \
+        --etl-settings "$PUDL_SETTINGS_YML" \
         --live-dbs test/integration test/unit \
     && pytest \
         -n auto \
         --gcs-cache-path gs://internal-zenodo-cache.catalyst.coop \
-        --etl-settings $PUDL_SETTINGS_YML \
+        --etl-settings "$PUDL_SETTINGS_YML" \
         --live-dbs test/validate \
-    && touch ${PUDL_OUTPUT}/success
+    && touch "$PUDL_OUTPUT/success"
 }
 
 function shutdown_vm() {
-    upload_file_to_slack $LOGFILE "pudl_etl logs for $ACTION_SHA-$GITHUB_REF:"
+    upload_file_to_slack "$LOGFILE" "pudl_etl logs for $BUILD_ID:"
     # Shut down the vm instance when the etl is done.
     echo "Shutting down VM."
     ACCESS_TOKEN=$(curl \
         "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
         -H "Metadata-Flavor: Google" | jq -r '.access_token')
-    curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer ${ACCESS_TOKEN}" https://compute.googleapis.com/compute/v1/projects/catalyst-cooperative-pudl/zones/$GCE_INSTANCE_ZONE/instances/$GCE_INSTANCE/stop
+    curl -X POST -H "Content-Length: 0" -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://compute.googleapis.com/compute/v1/projects/catalyst-cooperative-pudl/zones/$GCE_INSTANCE_ZONE/instances/$GCE_INSTANCE/stop"
 }
 
 function copy_outputs_to_gcs() {
     echo "Copying outputs to GCP bucket $PUDL_GCS_OUTPUT"
-    gsutil -m cp -r $PUDL_OUTPUT ${PUDL_GCS_OUTPUT}
-    rm ${PUDL_OUTPUT}/success
+    gsutil -m cp -r "$PUDL_OUTPUT" "$PUDL_GCS_OUTPUT"
+    rm "$PUDL_OUTPUT/success"
 }
 
 function copy_outputs_to_distribution_bucket() {
     # Only attempt to update outputs if we have a real value of GITHUB_REF
     if [ -n "$GITHUB_REF" ]; then
         echo "Removing old $GITHUB_REF outputs from GCP distributon bucket."
-        gsutil -m -u $GCP_BILLING_PROJECT rm -r "gs://pudl.catalyst.coop/$GITHUB_REF"
+        gsutil -m -u "$GCP_BILLING_PROJECT" rm -r "gs://pudl.catalyst.coop/$GITHUB_REF"
         echo "Copying outputs to GCP distribution bucket"
-        gsutil -m -u $GCP_BILLING_PROJECT cp -r "$PUDL_OUTPUT/*" "gs://pudl.catalyst.coop/$GITHUB_REF"
+        gsutil -m -u "$GCP_BILLING_PROJECT" cp -r "$PUDL_OUTPUT/*" "gs://pudl.catalyst.coop/$GITHUB_REF"
 
         echo "Removing old $GITHUB_REF outputs from AWS distributon bucket."
         aws s3 rm "s3://pudl.catalyst.coop/$GITHUB_REF" --recursive
@@ -80,41 +80,28 @@ function copy_outputs_to_distribution_bucket() {
 
 function zenodo_data_release() {
     echo "Creating a new PUDL data release on Zenodo."
-    ~/devtools/zenodo/zenodo_data_release.py --publish --env sandbox --source-dir $PUDL_OUTPUT
+    ~/devtools/zenodo/zenodo_data_release.py --publish --env sandbox --source-dir "$PUDL_OUTPUT"
 }
 
 function notify_slack() {
     # Notify pudl-builds slack channel of deployment status
-    if [ $1 = "success" ]; then
+    if [ "$1" = "success" ]; then
         message=":large_green_circle: :sunglasses: :unicorn_face: :rainbow: The deployment succeeded!! :partygritty: :database_parrot: :blob-dance: :large_green_circle:\n\n "
         message+="<https://github.com/catalyst-cooperative/pudl/compare/main...${GITHUB_REF}|Make a PR for \`${GITHUB_REF}\` into \`main\`!>\n\n"
-    elif [ $1 = "failure" ]; then
+    elif [ "$1" = "failure" ]; then
         message=":large_red_square: Oh bummer the deployment failed ::fiiiiine: :sob: :cry_spin:\n\n "
     else
         echo "Invalid deployment status"
         exit 1
     fi
-    message+="See https://console.cloud.google.com/storage/browser/nightly-build-outputs.catalyst.coop/$ACTION_SHA-$GITHUB_REF for logs and outputs."
+    message+="See https://console.cloud.google.com/storage/browser/nightly-build-outputs.catalyst.coop/$BUILD_ID for logs and outputs."
 
     send_slack_msg "$message"
 }
 
-if [ "$GITHUB_ACTION_TRIGGER" = "workflow_dispatch" ]; then
-    echo "Deployed via workflow_dispatch, testing git authentication!"
-    # Remove the read-only authentication header
-    git config --unset http.https://github.com/.extraheader
-    git config user.email "pudl@catalyst.coop"
-    git config user.name "pudlbot"
-    git remote set-url origin "https://pudlbot:$PUDL_BOT_PAT@github.com/catalyst-cooperative/pudl.git"
-    git config -l
-    git tag -a -m "Nightly test tag" nightly-tag-test
-    git push origin nightly-tag-test
-    shutdown_vm
-fi
-
 # # Run ETL. Copy outputs to GCS and shutdown VM if ETL succeeds or fails
 # 2>&1 redirects stderr to stdout.
-run_pudl_etl 2>&1 | tee $LOGFILE
+run_pudl_etl 2>&1 | tee "$LOGFILE"
 
 ETL_SUCCESS=${PIPESTATUS[0]}
 
@@ -122,42 +109,44 @@ copy_outputs_to_gcs
 
 # if pipeline is successful, distribute + publish datasette
 if [[ $ETL_SUCCESS == 0 ]]; then
-    if [ $GITHUB_ACTION_TRIGGER = "schedule" ]; then
-        # Update the nightly branch to point at newly successful nightly build tag
+    if [ "$GITHUB_ACTION_TRIGGER" = "schedule" ]; then
+        # Remove read-only authentication header added by git checkout
+        git config --unset http.https://github.com/.extraheader
         git config user.email "pudl@catalyst.coop"
         git config user.name "pudlbot"
-        git remote set-url origin https://pudlbot:$PUDL_BOT_PAT@github.com/catalyst-cooperative/pudl.git
+        git remote set-url origin "https://pudlbot:$PUDL_BOT_PAT@github.com/catalyst-cooperative/pudl.git"
+        # Update the nightly branch to point at newly successful nightly build tag
         git checkout nightly
-        git merge --ff-only $NIGHTLY_TAG
+        git merge --ff-only "$NIGHTLY_TAG"
         git push
     fi
     # Deploy the updated data to datasette
-    if [ $GITHUB_REF = "dev" ]; then
-        python ~/devtools/datasette/publish.py 2>&1 | tee -a $LOGFILE
+    if [ "$GITHUB_REF" = "dev" ]; then
+        python ~/devtools/datasette/publish.py 2>&1 | tee -a "$LOGFILE"
         ETL_SUCCESS=${PIPESTATUS[0]}
     fi
 
     # Compress the SQLite DBs for easier distribution
     # Remove redundant multi-file EPA CEMS outputs prior to distribution
-    gzip --verbose $PUDL_OUTPUT/*.sqlite && \
-    rm -rf $PUDL_OUTPUT/hourly_emissions_epacems/ && \
-    rm -f $PUDL_OUTPUT/metadata.yml
+    gzip --verbose "$PUDL_OUTPUT"/*.sqlite && \
+    rm -rf "$PUDL_OUTPUT/core_epacems__hourly_emissions/" && \
+    rm -f "$PUDL_OUTPUT/metadata.yml"
     ETL_SUCCESS=${PIPESTATUS[0]}
 
     # Dump outputs to s3 bucket if branch is dev or build was triggered by a tag
     # TODO: this behavior should be controlled by on/off switch here and this logic
     # should be moved to the triggering github action. Having it here feels
     # fragmented.
-    if [ $GITHUB_ACTION_TRIGGER = "push" ] || [ $GITHUB_REF = "dev" ]; then
+    if [ "$GITHUB_ACTION_TRIGGER" = "push" ] || [ "$GITHUB_REF" = "dev" ]; then
         copy_outputs_to_distribution_bucket
         ETL_SUCCESS=${PIPESTATUS[0]}
-        zenodo_data_release 2>&1 | tee -a $LOGFILE
+        zenodo_data_release 2>&1 | tee -a "$LOGFILE"
         ETL_SUCCESS=${PIPESTATUS[0]}
     fi
 fi
 
 # This way we also save the logs from latter steps in the script
-gsutil cp $LOGFILE ${PUDL_GCS_OUTPUT}
+gsutil cp "$LOGFILE" "$PUDL_GCS_OUTPUT"
 
 # Notify slack about entire pipeline's success or failure;
 # PIPESTATUS[0] either refers to the failed ETL run or the last distribution
