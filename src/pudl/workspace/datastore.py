4fb33e2e30ee832484a2fc56f849ae41e278b82e
e-belfer <ella.belfer@catalyst.coop>
2023-12-22 10:37:37 -0500

    WIP: add list partitions to _matches

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -104,6 +104,14 @@ class DatapackageDescriptor:
                     f"Resource filter values should be all lowercase: {k}={v}"
                 )
         parts = res.get("parts", {})
+
+        # If partitions are list, match whole list if it contains desired element
+        if set(map(type, parts.values())) == {list}:
+            return all(
+                any(part.lower() == str(v).lower() for part in parts.get(k))
+                for k, v in filters.items()
+            )
+        # Otherwise return matches to int/str partitions
         return all(
             str(parts.get(k)).lower() == str(v).lower() for k, v in filters.items()
         )
@@ -177,7 +185,7 @@ class ZenodoDoiSettings(BaseSettings):
     eia923: ZenodoDoi = "10.5281/zenodo.10067550"
     eia_bulk_elec: ZenodoDoi = "10.5281/zenodo.7067367"
     epacamd_eia: ZenodoDoi = "10.5281/zenodo.7900974"
-    epacems: ZenodoDoi = "10.5281/zenodo.10306114"
+    epacems: ZenodoDoi = "10.5281/zenodo.10420843"
     ferc1: ZenodoDoi = "10.5281/zenodo.8326634"
     ferc2: ZenodoDoi = "10.5281/zenodo.8326697"
     ferc6: ZenodoDoi = "10.5281/zenodo.8326696"
