d26b4aa991f439f3ce0b1f11fb3f6d34c9ba91ed
e-belfer <ella.belfer@catalyst.coop>
2023-12-26 12:10:40 -0500

    Update to non-broken CEMS archive

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -187,7 +187,7 @@ class ZenodoDoiSettings(BaseSettings):
     eia923: ZenodoDoi = "10.5281/zenodo.10067550"
     eia_bulk_elec: ZenodoDoi = "10.5281/zenodo.7067367"
     epacamd_eia: ZenodoDoi = "10.5281/zenodo.7900974"
-    epacems: ZenodoDoi = "10.5281/zenodo.10420843"
+    epacems: ZenodoDoi = "10.5281/zenodo.10425497"
     ferc1: ZenodoDoi = "10.5281/zenodo.8326634"
     ferc2: ZenodoDoi = "10.5281/zenodo.8326697"
     ferc6: ZenodoDoi = "10.5281/zenodo.8326696"
