edbb9367f804e043452e979b8b87bf5e8db66d13
Katie Lamb <katherine.lamb@catalyst.coop>
2024-01-10 14:05:33 -0800

    fix typo

--- src/pudl/analysis/record_linkage/__init__.py
+++ src/pudl/analysis/record_linkage/__init__.py
@@ -1,4 +1,4 @@
-"""This module impolements models for various forms of record linkage."""
+"""This module implements models for various forms of record linkage."""
 from . import (
     classify_plants_ferc1,
     eia_ferc1_record_linkage,
