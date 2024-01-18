7c384a647b49f1aa3199328263f048b7fcdf341e
e-belfer <ella.belfer@catalyst.coop>
2024-01-18 15:49:03 -0500

    Map Part L

--- src/pudl/extract/phmsagas.py
+++ src/pudl/extract/phmsagas.py
@@ -56,6 +56,7 @@ raw_table_names = (
     "raw_phmsagas__yearly_miles_of_gathering_pipe_by_nps",
     "raw_phmsagas__yearly_miles_of_transmission_pipe_by_nps",
     "raw_phmsagas__yearly_inspections_and_assessments",
+    "raw_phmsagas__yearly_miles_of_pipe_by_class_location",
 )
 
 phmsagas_raw_dfs = excel.raw_df_factory(Extractor, name="phmsagas")
