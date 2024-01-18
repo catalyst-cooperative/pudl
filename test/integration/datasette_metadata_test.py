1bd6ee43f94b9575cffa61f4cb9349a9e36c0904
Jan Rous <rousik@gmail.com>
2024-01-17 22:39:35 -0700

    some unit test fixes
    1. move datasette yml into temp directory
    2. hardcode which dataset ids should be processed to only
    require ferc1_dbf and ferc1_xbrl.
    
    Note that there's some confusion as to how temp files are used. It
    seems that this directory was used for all sorts of stuff, but currently
    is only used for inputs/ouputs and is otherwise not accessible.

--- test/integration/datasette_metadata_test.py
+++ test/integration/datasette_metadata_test.py
@@ -2,6 +2,8 @@
 
 import json
 import logging
+import tempfile
+from pathlib import Path
 
 import datasette.utils
 import yaml
@@ -18,41 +20,56 @@ def test_datasette_metadata_to_yml(ferc1_engine_xbrl):
     Requires the ferc1_engine_xbrl because we construct Datasette metadata from the
     datapackage.json files which annotate the XBRL derived FERC SQLite DBs.
     """
-    metadata_yml = PudlPaths().output_dir / "metadata.yml"
-    logger.info(f"Writing Datasette Metadata to {metadata_yml}")
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        metadata_yml = Path(tmp_dir) / "metadata.yml"
+        logger.info(f"Writing Datasette Metadata to {metadata_yml}")
 
-    dm = DatasetteMetadata.from_data_source_ids(PudlPaths().output_dir)
-    with metadata_yml.open("w") as f:
-        f.write(dm.to_yaml())
+        dm = DatasetteMetadata.from_data_source_ids(
+            output_path=PudlPaths().output_dir,
+            data_source_ids=[
+                "pudl",
+                "eia860",
+                "eia860m",
+                "eia861",
+                "eia923",
+                "ferc1",
+            ],
+            xbrl_ids=["ferc1_xbrl"],
+        )
+        with metadata_yml.open("w") as f:
+            f.write(dm.to_yaml())
 
-    logger.info("Parsing generated metadata using datasette utils.")
-    metadata_json = json.dumps(yaml.safe_load(metadata_yml.open()))
-    parsed_metadata = datasette.utils.parse_metadata(metadata_json)
-    assert sorted(set(parsed_metadata["databases"])) == sorted(
-        {
-            "ferc1_dbf",
-            "ferc1_xbrl",
-            "ferc2_dbf",
-            "ferc2_xbrl",
-            "ferc60_dbf",
-            "ferc60_xbrl",
-            "ferc6_dbf",
-            "ferc6_xbrl",
-            "ferc714_xbrl",
-            "pudl",
-        }
-    )
-    assert parsed_metadata["license"] == "CC-BY-4.0"
-    assert (
-        parsed_metadata["databases"]["pudl"]["source_url"]
-        == "https://github.com/catalyst-cooperative/pudl"
-    )
-    assert (
-        parsed_metadata["databases"]["pudl"]["tables"]["core_eia__entity_plants"][
-            "label_column"
-        ]
-        == "plant_name_eia"
-    )
-    for tbl_name in parsed_metadata["databases"]["pudl"]["tables"]:
-        if parsed_metadata["databases"]["pudl"]["tables"][tbl_name]["columns"] is None:
-            raise AssertionError(f"pudl.{tbl_name}.columns is None")
+        logger.info("Parsing generated metadata using datasette utils.")
+        metadata_json = json.dumps(yaml.safe_load(metadata_yml.open()))
+        parsed_metadata = datasette.utils.parse_metadata(metadata_json)
+        assert sorted(set(parsed_metadata["databases"])) == sorted(
+            {
+                "ferc1_dbf",
+                "ferc1_xbrl",
+                "ferc2_dbf",
+                "ferc2_xbrl",
+                "ferc60_dbf",
+                "ferc60_xbrl",
+                "ferc6_dbf",
+                "ferc6_xbrl",
+                "ferc714_xbrl",
+                "pudl",
+            }
+        )
+        assert parsed_metadata["license"] == "CC-BY-4.0"
+        assert (
+            parsed_metadata["databases"]["pudl"]["source_url"]
+            == "https://github.com/catalyst-cooperative/pudl"
+        )
+        assert (
+            parsed_metadata["databases"]["pudl"]["tables"]["core_eia__entity_plants"][
+                "label_column"
+            ]
+            == "plant_name_eia"
+        )
+        for tbl_name in parsed_metadata["databases"]["pudl"]["tables"]:
+            if (
+                parsed_metadata["databases"]["pudl"]["tables"][tbl_name]["columns"]
+                is None
+            ):
+                raise AssertionError(f"pudl.{tbl_name}.columns is None")
