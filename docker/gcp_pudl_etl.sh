8e42da233da6db81e1a53928b3f4d9deb583ed5a
Dazhong Xia <dazhong.xia@catalyst.coop>
2024-01-24 13:14:46 -0500

    Remove testing short-circuits

--- docker/gcp_pudl_etl.sh
+++ docker/gcp_pudl_etl.sh
@@ -24,8 +24,6 @@ function authenticate_gcp() {
 function run_pudl_etl() {
     echo "Running PUDL ETL"
     send_slack_msg ":large_yellow_circle: Deployment started for $BUILD_ID :floppy_disk:"
-    echo "Short circuiting run_pudl_etl" >> $LOGFILE
-    return 1
     authenticate_gcp && \
     alembic upgrade head && \
     ferc_to_sqlite \
@@ -61,8 +59,6 @@ function shutdown_vm() {
 }
 
 function save_outputs_to_gcs() {
-    echo "Short circuiting save_outputs_to_gcs" >> $LOGFILE
-    return 1
     echo "Copying outputs to GCP bucket $PUDL_GCS_OUTPUT" && \
     gsutil -m cp -r "$PUDL_OUTPUT" "$PUDL_GCS_OUTPUT" && \
     rm "$PUDL_OUTPUT/success"
@@ -72,8 +68,6 @@ function copy_outputs_to_distribution_bucket() {
     # Only attempt to update outputs if we have a real value of BUILD_REF
     # This avoids accidentally blowing away the whole bucket if it's not set.
     echo "Copying outputs to distribution buckets"
-    echo "Short circuiting copy_outputs_to_distribution_bucket" >> $LOGFILE
-    return 1
     if [[ -n "$BUILD_REF" ]]; then
         if [[ "$GITHUB_ACTION_TRIGGER" == "schedule" ]]; then
             # If running nightly builds, copy outputs to the "nightly" bucket path
@@ -107,8 +101,6 @@ function copy_outputs_to_distribution_bucket() {
 }
 
 function zenodo_data_release() {
-    echo "Short circuiting zenodo_data_release" >> $LOGFILE
-    return 1
     echo "Creating a new PUDL data release on Zenodo." && \
     ~/pudl/devtools/zenodo/zenodo_data_release.py --publish --env "$1" --source-dir "$PUDL_OUTPUT"
 }
@@ -132,8 +124,6 @@ function notify_slack() {
 function update_nightly_branch() {
     # When building the image, GHA adds an HTTP basic auth header in git
     # config, which overrides the auth we set below. So we unset it.
-    echo "Short circuiting update_nightly_branch" >> $LOGFILE
-    return 1
     git config --unset http.https://github.com/.extraheader && \
     git config user.email "pudl@catalyst.coop" && \
     git config user.name "pudlbot" && \
@@ -149,8 +139,6 @@ function update_nightly_branch() {
 
 function clean_up_outputs_for_distribution() {
     # Compress the SQLite DBs for easier distribution
-    echo "Short circuiting clean_up_outputs_for_distribution" >> $LOGFILE
-    return 1
     gzip --verbose "$PUDL_OUTPUT"/*.sqlite && \
     # Remove redundant multi-file EPA CEMS outputs prior to distribution
     rm -rf "$PUDL_OUTPUT/core_epacems__hourly_emissions/" && \
