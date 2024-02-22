89aa4b4836c0b1ceaee251d3e54c23ef9568b46f
Zach Schira <zach.schira@catalyst.coop>
2024-02-22 18:34:38 -0500

    Experiment tracking (#3289)
    * Add experiment tracking to ferc-ferc model
    
    * Update ferc-ferc integration test
    
    * Update ferc-ferc config format
    
    * Log dataframe embedding config
    
    * Improve dataframe embedder mlflow logging
    
    * Add experiment tracking to ferc-eia model
    
    * Add mlflow to dependencies
    
    * Refactor experiment tracking to allow enabling tracking with dagster config
    
    * Add tests for ExperimentTracker
    
    * Test ExperimentTracker when tracking is disabled
    
    * Update gitignore
    
    * Move ml model utils to be more generic
    
    * Expand docstrings in ml_tools.py
    
    * Improve long line in docstring
    
    * Add more detail to experiment tracking docstring
    
    * conda lock files
    
    * fix bad function name
    
    * Make subpackage for ml_tools
    
    * Fix ml model configuration/integration with dagster
    
    * Fix ferc_to_ferc test
    
    * Log more ferc_to_ferc metrics
    
    * Add better docstrings for ml_tools modules
    
    * Fix docs build failure
    
    * Change default path to experiments DB to fix docs
    
    * Fix missing arg
    
    ---------
    
    Co-authored-by: Katie Lamb <katherine.lamb@catalyst.coop>

--- src/pudl/analysis/record_linkage/eia_ferc1_record_linkage.py
+++ src/pudl/analysis/record_linkage/eia_ferc1_record_linkage.py
@@ -31,15 +31,17 @@ plant-parts.
 import importlib.resources
 from typing import Literal
 
+import mlflow
 import numpy as np
 import pandas as pd
-from dagster import Out, graph_asset, op
+from dagster import Out, graph, op
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import GridSearchCV, train_test_split
 
 import pudl
 import pudl.helpers
+from pudl.analysis.ml_tools import experiment_tracking, models
 from pudl.analysis.plant_parts_eia import match_to_single_plant_part
 from pudl.analysis.record_linkage import embed_dataframe
 from pudl.metadata.classes import DataSource, Resource
@@ -263,12 +265,13 @@ def get_best_matches_with_overwrites(match_df, inputs):
 
 
 @op
-def run_matching_model(features_train, features_all, y_df):
+def run_matching_model(features_train, features_all, y_df, experiment_tracker):
     """Run model to match EIA to FERC records."""
     return run_model(
         features_train=features_train,
         features_all=features_all,
         y_df=y_df,
+        experiment_tracker=experiment_tracker,
     )
 
 
@@ -292,8 +295,13 @@ def get_match_full_records(best_match_df, inputs):
     ).enforce_schema(connected_df)
 
 
-@graph_asset
-def out_pudl__yearly_assn_eia_ferc1_plant_parts(
+@models.pudl_model(
+    "out_pudl__yearly_assn_eia_ferc1_plant_parts",
+    config_from_yaml=False,
+)
+@graph
+def ferc_to_eia(
+    experiment_tracker: experiment_tracking.ExperimentTracker,
     out_ferc1__yearly_all_plants: pd.DataFrame,
     out_ferc1__yearly_steam_plants_fuel_by_plant_sched402: pd.DataFrame,
     out_eia__yearly_plant_parts: pd.DataFrame,
@@ -312,13 +320,14 @@ def out_pudl__yearly_assn_eia_ferc1_plant_parts(
         out_eia__yearly_plant_parts,
     )
     all_pairs_df, train_pairs_df = get_pairs_dfs(inputs)
-    features_all = pair_vectorizers(all_pairs_df)
-    features_train = pair_vectorizers(train_pairs_df)
+    features_all = pair_vectorizers(all_pairs_df, experiment_tracker)
+    features_train = pair_vectorizers(train_pairs_df, experiment_tracker)
     y_df = get_y_label_df(train_pairs_df, inputs)
     match_df = run_matching_model(
         features_train=features_train,
         features_all=features_all,
         y_df=y_df,
+        experiment_tracker=experiment_tracker,
     )
     # choose one EIA match for each FERC record
     best_match_df = get_best_matches_with_overwrites(match_df, inputs)
@@ -528,7 +537,10 @@ class InputManager:
 
 
 def run_model(
-    features_train: pd.DataFrame, features_all: pd.DataFrame, y_df: pd.DataFrame
+    features_train: pd.DataFrame,
+    features_all: pd.DataFrame,
+    y_df: pd.DataFrame,
+    experiment_tracker: experiment_tracking.ExperimentTracker,
 ) -> pd.DataFrame:
     """Train Logistic Regression model using GridSearch cross validation.
 
@@ -574,6 +586,10 @@ def run_model(
     lrc = LogisticRegression()
     clf = GridSearchCV(estimator=lrc, param_grid=param_grid, verbose=True, n_jobs=-1)
     clf.fit(X=X_train, y=y_train)
+
+    # Log best parameters
+    experiment_tracker.execute_logging(lambda: mlflow.log_params(clf.best_params_))
+
     y_pred = clf.predict(X_test)
     precision, recall, f_score, _ = precision_recall_fscore_support(
         y_test, y_pred, average="binary"
@@ -586,6 +602,18 @@ def run_model(
         f"    Precision: {precision:.02}\n"
         f"    Recall:    {recall:.02}\n"
     )
+    # Log model metrics
+    experiment_tracker.execute_logging(
+        lambda: mlflow.log_metrics(
+            {
+                "accuracy": accuracy,
+                "f_score": f_score,
+                "precision": precision,
+                "recall": recall,
+            }
+        )
+    )
+
     preds = clf.predict(features_all.matrix)
     probs = clf.predict_proba(features_all.matrix)
     final_df = pd.DataFrame(
