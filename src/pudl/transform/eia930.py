9633c5f2154b757cb9f36c2652d2ee4aefc31296
Zane Selvans <zane.selvans@catalyst.coop>
2024-04-20 01:46:24 -0600

    Move EIA-930 BA net gen renaming into extraction metadata.

--- src/pudl/transform/eia930.py
+++ src/pudl/transform/eia930.py
@@ -25,63 +25,28 @@ def core_eia930__hourly_balancing_authority_net_generation(
         # eia_region_code,
         # report_hour_local
     ]
-    # We don't want to retain totals, which are redundant
-    total_netgen_cols = [
-        "net_generation_imputed_mw",
-        "net_generation_adjusted_mw",
-        "net_generation_mw",
-    ]
+    # Select only the columns relevant to the BA net generation table
     netgen_cols = list(raw_eia930__balance.filter(like="net_generation"))
-    # Remove total net gen values from the list of net gen cols
-    netgen_cols = [x for x in netgen_cols if x not in total_netgen_cols]
-    netgen = raw_eia930__balance[qual_cols + netgen_cols]
-
-    def _simplify_columns(col: str) -> str:
-        return (
-            col.removeprefix("net_generation_")
-            .removesuffix("_mw")
-            .replace("all_petroleum_products", "oil")
-            .replace("hydropower_and_pumped_storage", "hydro")
-            .replace("natural_gas", "gas")
-            .replace("other_fuel_sources", "other")
-        )
+    interchange_cols = list(raw_eia930__balance.filter(like="interchange"))
+    netgen = raw_eia930__balance[qual_cols + netgen_cols + interchange_cols]
 
     # Rename columns so that they contain only the energy source and the level of
     # processing, so it's easy to construct a multi-index to unstack below
     # Doing these manipulations while the values are in the column names rather than
     # after they've been turned into a categorical column with millions of entries
     # is much faster.
-    netgen_renamed = (
-        netgen.rename(_simplify_columns, axis="columns")
-        .rename(
-            columns={
-                "coal": "coal_raw",
-                "gas": "gas_raw",
-                "hydro": "hydro_raw",
-                "oil": "oil_raw",
-                "other": "other_raw",
-                "nuclear": "nuclear_raw",
-                "solar": "solar_raw",
-                "unknown_fuel_sources": "unknown_raw",
-                "wind": "wind_raw",
-            },
-        )
-        .set_index(
-            [
-                "report_datetime_local",
-                "report_datetime_utc",
-                "balancing_authority_code_eia",
-            ]
-        )
-    )
+    netgen_renamed = netgen.rename(
+        lambda col: col.removeprefix("net_generation_").removesuffix("_mw"),
+        axis="columns",
+    ).set_index(qual_cols)
 
     # Prepare a multi-index for the columns so that we can stack cleanly
     netgen_renamed.columns = pd.MultiIndex.from_tuples(
         [x.split("_") for x in netgen_renamed.columns], names=["energy_source", None]
     )
-    return (
+    netgen_stacked = (
         netgen_renamed.stack(level=0, future_stack=True)
-        .rename(columns=lambda x: x + "_net_generation_mw")
+        .rename(columns=lambda x: f"net_generation_{x}_mw")
         .reset_index()
         .astype({"energy_source": "string"})
         .astype(
@@ -91,6 +56,13 @@ def core_eia930__hourly_balancing_authority_net_generation(
             }
         )
     )
+    # TODO[zaneselvans] 2024-04-20: Verify that sum of net generation from all fuels
+    # adds up to the total And then drop the total rows. Note that currently there are
+    # some big differences between the calculated total and the reported total.
+
+    # return netgen_stacked[netgen_stacked["energy_source"] != "total"
+
+    return netgen_stacked
 
 
 # core_eia930__hourly_subregion_demand
