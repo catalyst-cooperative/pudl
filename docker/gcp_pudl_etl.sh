34ebaaf1813e628e73b91b94aa17cf29778881d6
Zane Selvans <zane.selvans@catalyst.coop>
2024-05-01 09:35:21 -0600

    Fix quoting in hourly parquet deployment command

--- docker/gcp_pudl_etl.sh
+++ docker/gcp_pudl_etl.sh
@@ -210,7 +210,7 @@ function clean_up_outputs_for_distribution() {
     # Compress the SQLite DBs for easier distribution
     gzip --verbose "$PUDL_OUTPUT"/*.sqlite && \
     # Grab hourly tables which are only written to Parquet for distribution
-    cp "$PUDL_OUTPUT/parquet/*__hourly_*.parquet" "$PUDL_OUTPUT" && \
+    cp "$PUDL_OUTPUT"/parquet/*__hourly_*.parquet "$PUDL_OUTPUT" && \
     # Remove all other parquet output, which we are not yet distributing.
     rm -rf "$PUDL_OUTPUT/parquet" && \
     rm -f "$PUDL_OUTPUT/metadata.yml"
