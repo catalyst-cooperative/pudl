221ce77c25f72489ab75ca7d7b8b761fa8bca8ed
Zane Selvans <zane.selvans@catalyst.coop>
2024-07-29 21:59:59 -0600

    Merge alembic migrations from this branch and main

---
+++ migrations/versions/33d66c56823a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 33d66c56823a
+Revises: 52aa9f44b6a7, 7a16ce1fe774
+Create Date: 2024-07-29 20:33:23.329220
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33d66c56823a'
+down_revision = ('52aa9f44b6a7', '7a16ce1fe774')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
