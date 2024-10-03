3c8cab60db15a033f65f63f289f33600908ba342
Austen Sharpe <austensharpe@gmail.com>
2024-10-03 16:02:07 -0600

    Add blank transform module for vceregen

---
+++ src/pudl/transform/vceregen.py
@@ -0,0 +1,5 @@
+"""Transformations of the Vibrant Clean Energy renewable generation profiles.
+
+Wind and solar profiles are extracted separately, but concatenated into a single table
+in this module, as they have exactly the same structure.
+"""
