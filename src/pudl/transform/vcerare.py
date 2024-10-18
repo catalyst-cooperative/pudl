de63b12f87c96d4adebefe585725f799a8459eee
Austen Sharpe <austensharpe@gmail.com>
2024-10-18 00:30:35 -0400

    Update docstring for transform module

--- src/pudl/transform/vcerare.py
+++ src/pudl/transform/vcerare.py
@@ -1,4 +1,4 @@
-"""Transformations of the Vibrant Clean Energy renewable generation profiles.
+"""Transformations of the Vibrant Clean Energy Resource Adequacy Renewable Energy (RARE) Power Dataset.
 
 Wind and solar profiles are extracted separately, but concatenated into a single table
 in this module, as they have exactly the same structure.
