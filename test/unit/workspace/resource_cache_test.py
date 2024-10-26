90ed32ce77d6fdd51dec0ef99f85e043402e552f
Gaurav Gurjar <ggurjar333@gmail.com>
2024-10-26 23:14:53 +0400

    Refactor unit tests for better test independence

--- test/unit/workspace/resource_cache_test.py
+++ test/unit/workspace/resource_cache_test.py
@@ -2,9 +2,9 @@
 
 import shutil
 import tempfile
-import unittest
 from pathlib import Path
 
+import pytest
 import requests.exceptions as requests_exceptions
 from google.api_core.exceptions import BadRequest
 from google.cloud.storage.retry import _should_retry
@@ -13,7 +13,7 @@ from pudl.workspace import resource_cache
 from pudl.workspace.resource_cache import PudlResourceKey, extend_gcp_retry_predicate
 
 
-class TestGoogleCloudStorageCache(unittest.TestCase):
+class TestGoogleCloudStorageCache:
     """Unit tests for the GoogleCloudStorageCache class."""
 
     def test_bad_request_predicate(self):
@@ -21,77 +21,77 @@ class TestGoogleCloudStorageCache(unittest.TestCase):
         bad_request_predicate = extend_gcp_retry_predicate(_should_retry, BadRequest)
 
         # Check default exceptions.
-        self.assertFalse(_should_retry(BadRequest(message="Bad request!")))
-        self.assertTrue(_should_retry(requests_exceptions.Timeout()))
+        assert not _should_retry(BadRequest(message="Bad request!"))
+        assert _should_retry(requests_exceptions.Timeout())
 
-        # Check extended predicate handles default exceptionss and BadRequest.
-        self.assertTrue(bad_request_predicate(requests_exceptions.Timeout()))
-        self.assertTrue(bad_request_predicate(BadRequest(message="Bad request!")))
+        # Check extended predicate handles default exceptions and BadRequest.
+        assert bad_request_predicate(requests_exceptions.Timeout())
+        assert bad_request_predicate(BadRequest(message="Bad request!"))
 
 
-class TestLocalFileCache(unittest.TestCase):
+class TestLocalFileCache:
     """Unit tests for the LocalFileCache class."""
 
-    def setUp(self):
+    def setup_method(self):
         """Prepares temporary directory for storing cache contents."""
         self.test_dir = tempfile.mkdtemp()
         self.cache = resource_cache.LocalFileCache(Path(self.test_dir))
 
-    def tearDown(self):
+    def teardown_method(self):
         """Deletes content of the temporary directories."""
         shutil.rmtree(self.test_dir)
 
     def test_add_single_resource(self):
         """Adding resource has expected effect on later get() and contains() calls."""
         res = PudlResourceKey("ds", "doi", "file.txt")
-        self.assertFalse(self.cache.contains(res))
+        assert not self.cache.contains(res)
         self.cache.add(res, b"blah")
-        self.assertTrue(self.cache.contains(res))
-        self.assertEqual(b"blah", self.cache.get(res))
+        assert self.cache.contains(res)
+        assert self.cache.get(res) == b"blah"
 
     def test_that_two_cache_objects_share_storage(self):
         """Two LocalFileCache instances with the same path share the object storage."""
         second_cache = resource_cache.LocalFileCache(Path(self.test_dir))
         res = PudlResourceKey("dataset", "doi", "file.txt")
-        self.assertFalse(self.cache.contains(res))
-        self.assertFalse(second_cache.contains(res))
+        assert not self.cache.contains(res)
+        assert not second_cache.contains(res)
         self.cache.add(res, b"testContents")
-        self.assertTrue(self.cache.contains(res))
-        self.assertTrue(second_cache.contains(res))
-        self.assertEqual(b"testContents", second_cache.get(res))
+        assert self.cache.contains(res)
+        assert second_cache.contains(res)
+        assert second_cache.get(res) == b"testContents"
 
     def test_deletion(self):
         """Deleting resources has expected effect on later get() / contains() calls."""
         res = PudlResourceKey("a", "b", "c")
-        self.assertFalse(self.cache.contains(res))
+        assert not self.cache.contains(res)
         self.cache.add(res, b"sampleContents")
-        self.assertTrue(self.cache.contains(res))
+        assert self.cache.contains(res)
         self.cache.delete(res)
-        self.assertFalse(self.cache.contains(res))
+        assert not self.cache.contains(res)
 
     def test_read_only_add_and_delete_do_nothing(self):
         """Test that in read_only mode, add() and delete() calls are ignored."""
         res = PudlResourceKey("a", "b", "c")
         ro_cache = resource_cache.LocalFileCache(Path(self.test_dir), read_only=True)
-        self.assertTrue(ro_cache.is_read_only())
+        assert ro_cache.is_read_only()
 
         ro_cache.add(res, b"sample")
-        self.assertFalse(ro_cache.contains(res))
+        assert not ro_cache.contains(res)
 
         # Use read-write cache to insert resource
         self.cache.add(res, b"sample")
-        self.assertFalse(self.cache.is_read_only())
-        self.assertTrue(ro_cache.contains(res))
+        assert not self.cache.is_read_only()
+        assert ro_cache.contains(res)
 
         # Deleting via ro cache should not happen
         ro_cache.delete(res)
-        self.assertTrue(ro_cache.contains(res))
+        assert ro_cache.contains(res)
 
 
-class TestLayeredCache(unittest.TestCase):
+class TestLayeredCache:
     """Unit tests for LayeredCache class."""
 
-    def setUp(self):
+    def setup_method(self):
         """Constructs two LocalFileCache layers pointed at temporary directories."""
         self.layered_cache = resource_cache.LayeredCache()
         self.test_dir_1 = tempfile.mkdtemp()
