361bbe34f41d582bd501cfc380643004e5868c5b
Gaurav Gurjar <ggurjar333@gmail.com>
2024-11-20 23:35:21 +0400

    Refactor tests in csv_test.py to use pytest and simplify mock handling

--- test/unit/extract/csv_test.py
+++ test/unit/extract/csv_test.py
@@ -1,3 +1,7 @@
+"""Pytest for pudl.extract.csv module."""
+
+from unittest.mock import MagicMock
+
 import pandas as pd
 import pytest
 
@@ -12,16 +16,16 @@ CSV_FILENAME = f"{DATASET}_{PARTITION_SELECTION}.csv"
 
 
 class FakeExtractor(CsvExtractor):
-    def __init__(self, mocker):
+    def __init__(self):
         # TODO: Make these tests independent of the eia176 implementation
         self.METADATA = GenericMetadata("eia176")
-        super().__init__(ds=mocker.MagicMock())
+        super().__init__(ds=MagicMock())
 
 
 @pytest.fixture
-def extractor(mocker):
-    # Create an instance of the CsvExtractor class with mocker
-    return FakeExtractor(mocker)
+def extractor():
+    # Create an instance of the CsvExtractor class
+    return FakeExtractor()
 
 
 def test_source_filename_valid_partition(extractor):
@@ -43,12 +47,17 @@ def test_source_filename_multiple_selections(extractor):
 
 def test_load_source(mocker, extractor):
     mock_pd = mocker.patch("pudl.extract.csv.pd")
+    mock_ds = mocker.patch.object(extractor, "ds")
+    mock_zipfile = mocker.MagicMock()
+    mock_file = mocker.MagicMock()
+
+    mock_ds.get_zipfile_resource.return_value.__enter__.return_value = mock_zipfile
+    mock_zipfile.open.return_value.__enter__.return_value = mock_file
+
     assert extractor.load_source(PAGE, **PARTITION) == mock_pd.read_csv.return_value
-    extractor.ds.get_zipfile_resource.assert_called_once_with(DATASET, **PARTITION)
-    zipfile = extractor.ds.get_zipfile_resource.return_value.__enter__.return_value
-    zipfile.open.assert_called_once_with(CSV_FILENAME)
-    file = zipfile.open.return_value.__enter__.return_value
-    mock_pd.read_csv.assert_called_once_with(file)
+    mock_ds.get_zipfile_resource.assert_called_once_with(DATASET, **PARTITION)
+    mock_zipfile.open.assert_called_once_with(CSV_FILENAME)
+    mock_pd.read_csv.assert_called_once_with(mock_file)
 
 
 def test_extract(mocker, extractor):
@@ -60,25 +69,21 @@ def test_extract(mocker, extractor):
 
     # TODO: Once FakeExtractor is independent of eia176, mock out populating _column_map for PARTITION_SELECTION;
     #  Also include negative tests, i.e., for partition selections not in the _column_map
-    mocker.patch.object(CsvExtractor, "load_source", return_value=df)
-    # Testing the rename
-    mocker.patch.object(
-        GenericMetadata,
-        "get_column_map",
-        return_value={"company_rename": company_field},
-    )
-    # Transposing the df here to get the orientation we expect get_page_cols to return
-    mocker.patch.object(
-        CsvExtractor,
-        "get_page_cols",
-        return_value=df.T.index,
-    )
-    res = extractor.extract(**PARTITION)
-    assert len(res) == 1  # Assert only one page extracted
-    assert list(res.keys()) == [PAGE]  # Assert it is named correctly
-    assert (
-        res[PAGE][company_field][0] == company_data
-    )  # Assert that column correctly renamed and data is there.
+    with (
+        mocker.patch.object(CsvExtractor, "load_source", return_value=df),
+        mocker.patch.object(
+            GenericMetadata,
+            "get_column_map",
+            return_value={"company_rename": company_field},
+        ),
+        mocker.patch.object(CsvExtractor, "get_page_cols", return_value=df.T.index),
+    ):
+        res = extractor.extract(**PARTITION)
+        assert len(res) == 1  # Assert only one page extracted
+        assert list(res.keys()) == [PAGE]  # Assert it is named correctly
+        assert (
+            res[PAGE][company_field][0] == company_data
+        )  # Assert the data is extracted correctly
 
 
 def test_validate_exact_columns(mocker, extractor):
