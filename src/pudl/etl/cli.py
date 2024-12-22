ba58b944bb002ad424a3b7be1ec9755a374191cd
Dazhong Xia <dazhong.xia@catalyst.coop>
2024-12-21 23:24:17 -0500

    Make asset checks run in integration test (#3990)
    * WIP: Use same dagster Definitions for test and prod.
    
    * only check row nums for years that are in the dataset_settings
    
    * make the warning only warn once
    
    * Since some asset checks use job name to switch what checks they run, allow test to specify job name.
    
    ---------
    
    Co-authored-by: Christina Gosnell <cgosnell@catalyst.coop>

--- src/pudl/etl/cli.py
+++ src/pudl/etl/cli.py
@@ -8,14 +8,13 @@ import click
 import fsspec
 from dagster import (
     DagsterInstance,
-    Definitions,
     JobDefinition,
     build_reconstructable_job,
-    define_asset_job,
     execute_job,
 )
 
 import pudl
+from pudl.etl import defs
 from pudl.helpers import get_dagster_execution_config
 from pudl.settings import EpaCemsSettings, EtlSettings
 from pudl.workspace.setup import PudlPaths
@@ -37,24 +36,12 @@ def pudl_etl_job_factory(
         The job definition to be executed.
     """
 
-    def get_pudl_etl_job():
+    def get_pudl_etl_job(job_name: str | None = None):
         """Create an pudl_etl_job wrapped by to be wrapped by reconstructable."""
         pudl.logging_helpers.configure_root_logger(logfile=logfile, loglevel=loglevel)
-        jobs = [define_asset_job("etl_job")]
-        if not process_epacems:
-            jobs = [
-                define_asset_job(
-                    "etl_job",
-                    selection=pudl.etl.create_non_cems_selection(
-                        pudl.etl.default_assets
-                    ),
-                )
-            ]
-        return Definitions(
-            assets=pudl.etl.default_assets,
-            resources=pudl.etl.default_resources,
-            jobs=jobs,
-        ).get_job_def("etl_job")
+        if job_name is None:
+            job_name = "etl_full_no_cems" if not process_epacems else "etl_full"
+        return defs.get_job_def(job_name)
 
     return get_pudl_etl_job
 
