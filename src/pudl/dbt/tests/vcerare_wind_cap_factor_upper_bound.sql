415a113c708dd84cd269f82c27fcd0c1454e5461
zschira <zach.schira@catalyst.coop>
2025-01-10 12:47:40 -0500

    Setup multiple dbt profiles

--- src/pudl/dbt/tests/vcerare_wind_cap_factor_upper_bound.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM {{ source('pudl_nightly', 'out_vcerare__hourly_available_capacity_factor') }}
-WHERE
-    capacity_factor_offshore_wind > 1.02 OR
-    capacity_factor_onshore_wind > 1.02
