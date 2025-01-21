6335e940e73f0fa43bf72220bd93177d40af9673
zschira <zach.schira@catalyst.coop>
2025-01-21 12:47:52 -0500

    Reorganize dbt into multiple schema.yml files

--- src/pudl/dbt/models/ferc1/ferc1_fbp_cost_per_mmbtu.sql
+++ src/pudl/dbt/models/ferc1/ferc1_fbp_cost_per_mmbtu.sql
@@ -3,4 +3,4 @@ select
 {% for fuel_type in ["gas", "oil", "coal"] %}
     {{ fuel_type }}_fraction_cost * fuel_cost / ({{ fuel_type }}_fraction_mmbtu * fuel_mmbtu) as {{ fuel_type }}_cost_per_mmbtu,
 {% endfor %}
-from {{ source('pudl', 'out_ferc1__yearly_steam_plants_fuel_by_plant_sched402') }}
+from {{ source('ferc1', 'out_ferc1__yearly_steam_plants_fuel_by_plant_sched402') }}
