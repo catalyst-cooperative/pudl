f96f304ee2b17f378093dc9c56076c8e1d0f9144
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-03-05 14:12:09 -0500

    Add bespoke test_bga_eia860 tests for tables core_eia860__assn_boiler_generator, _out_eia__yearly_generators

---
+++ dbt/tests/validate_core_eia860__assn_boiler_generator__associations.sql
@@ -0,0 +1,45 @@
+with GensSimple as (
+    select
+        report_date,
+        plant_id_eia,
+        generator_id,
+        fuel_type_code_pudl
+    from {{ source('pudl', '_out_eia__yearly_generators') }}
+), BgaGens as (
+    select distinct
+        report_date,
+        plant_id_eia,
+        unit_id_pudl,
+        generator_id
+    from {{ source('pudl', 'core_eia860__assn_boiler_generator') }}
+), UnitsSimple as (
+    select distinct
+        report_date,
+        plant_id_eia,
+        fuel_type_code_pudl,
+        unit_id_pudl
+    from GensSimple join BgaGens using (report_date, plant_id_eia, generator_id)
+), UnitsFuelCount as (
+    select
+        report_date,
+        plant_id_eia,
+        unit_id_pudl,
+        count(1) as fuel_type_count
+    from UnitsSimple group by all
+), DifferingPrimaryFuels as (
+    select
+        case when fuel_type_count > 1 then 1
+        else 0
+        end as multi_fuel_units,
+        1 as all_units
+    from UnitsFuelCount
+), DifferingPrimaryFuelRate as (
+    select
+        sum(multi_fuel_units) as num_multi_fuel_units,
+        sum(all_units) as all_units
+    from DifferingPrimaryFuels
+)
+select * from DifferingPrimaryFuelRate
+where (num_multi_fuel_units / all_units) >= 0.01 -- existing test has no failure condition
+                                                 -- and just logs the current rate
+                                                 -- (0.0087 for etl-fast)
