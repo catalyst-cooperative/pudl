4c8c6777b1afe798f3e46ad4cb8b88c3735f7207
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-03-06 14:54:28 -0500

    Add max outliers and improve argument names for group_mean_continuity_check.
    Also documentation.

--- dbt/macros/group_mean_continuity_check.sql
+++ dbt/macros/group_mean_continuity_check.sql
@@ -1,23 +1,24 @@
-{% test group_mean_continuity_check(model, column_name, group_column, max_pct_change) %}
+{% test group_mean_continuity_check(model, column_name, ordered_group_column, max_fr_change, n_outliers_allowed=0) %}
 
 with GroupMean as (
     select
-    {{ group_column }},
+    {{ ordered_group_column }},
     avg({{ column_name }}) as mean
     from {{ model }}
-    group by {{ group_column }}
+    group by {{ ordered_group_column }}
 ),
 Continuity as (
     select
-    {{ group_column }},
+    {{ ordered_group_column }},
     mean as newer_value,
-    LAG(mean) OVER (order by {{ group_column }}) as older_value
+    LAG(mean) OVER (order by {{ ordered_group_column }}) as older_value
     from GroupMean
 )
 select
-{{ group_column }},
-abs(newer_value - older_value)/older_value as pct_change
+{{ ordered_group_column }},
+abs(newer_value - older_value)/older_value as fr_change
 from Continuity
-where newer_value is not null and older_value is not null and pct_change >= {{ max_pct_change }}
+where newer_value is not null and older_value is not null and fr_change >= {{ max_fr_change }}
+limit 1e6 offset {{ n_outliers_allowed }}
 
 {% endtest %}
