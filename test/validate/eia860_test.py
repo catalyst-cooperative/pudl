8a811d7e5a44839b0f4dcc41a0c521da1727f9f7
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-03-25 15:10:23 -0400

    Remove old implementation of test_own_eia860

--- test/validate/eia860_test.py
+++ test/validate/eia860_test.py
@@ -4,9 +4,6 @@ import logging
 
 import pandas as pd
 import pytest
-from scipy import stats
-
-from pudl import helpers
 
 logger = logging.getLogger(__name__)
 
@@ -52,49 +49,6 @@ def test_bga_eia860(pudl_out_eia, live_dbs):
         f"{multi_fuel_unit_fraction:.0%} of generation units contain "
         f"generators with differing primary fuels."
     )
-
-
-def test_own_eia860(pudl_out_eia, live_dbs):
-    """Sanity checks for EIA 860 generator ownership data."""
-    if not live_dbs:
-        pytest.skip("Data validation only works with a live PUDL DB.")
-    if pudl_out_eia.freq is not None:
-        pytest.skip()
-
-    own_out = pudl_out_eia.own_eia860()
-
-    if (own_out.fraction_owned > 1.0).any():
-        raise AssertionError(
-            "Generators with ownership fractions > 1.0 found. Bad data?"
-        )
-
-    if (own_out.fraction_owned < 0.0).any():
-        raise AssertionError(
-            "Generators with ownership fractions < 0.0 found. Bad data?"
-        )
-
-    # Verify that the reported ownership fractions add up to something very
-    # close to 1.0 (i.e. that the full ownership of each generator is
-    # specified by the EIA860 data)
-    own_gb = own_out.groupby(["report_date", "plant_id_eia", "generator_id"])
-    own_sum = own_gb["fraction_owned"].agg(helpers.sum_na).reset_index()
-    logger.info(
-        f"{len(own_sum[own_sum.fraction_owned.isnull()])} generator-years have no ownership data."
-    )
-
-    own_sum = own_sum.dropna()
-    pct_missing = stats.percentileofscore(own_sum.fraction_owned, 0.98)
-    logger.info(
-        f"{len(own_sum[own_sum.fraction_owned < 0.98])} ({pct_missing}%) "
-        f"generator-years have incomplete ownership data."
-    )
-    if not max(own_sum["fraction_owned"] < 1.02):
-        raise ValueError("Plants with more than 100% ownership found...")
-    # There might be a few generators with incomplete ownership but virtually
-    # everything should be pretty fully described. If not, let us know. The
-    # 0.5 threshold means 0.5% -- i.e. less than 1 in 200 is partial.
-    if pct_missing >= 0.5:
-        raise ValueError(f"{pct_missing}% of generators lack complete ownership data.")
 
 
 @pytest.mark.xfail(reason="There are 730 generators that report multiple operators")
