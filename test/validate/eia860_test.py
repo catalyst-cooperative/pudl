7deaa08f485ca25a6d2aad751f7ad2214e7b6a8e
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-03-25 15:13:30 -0400

    Remove old implementation of test_generator_id_consistency

--- test/validate/eia860_test.py
+++ test/validate/eia860_test.py
@@ -8,44 +8,6 @@ import pytest
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.xfail(reason="There are 40 known inconsistent generator IDs.")
-def test_generator_id_consistency(pudl_out_eia, live_dbs):
-    """Check if there are any plants that report inconsistent generator IDs.
-
-    There are some instances in which a plant will report generator IDs differently in
-    different years, such that the IDs differ only in terms of the case of letters, or
-    non-alphanumeric characters. This test identifies them. We haven't fixed them yet.
-    """
-    if not live_dbs:
-        pytest.skip("Data validation only works with a live PUDL DB.")
-    if pudl_out_eia.freq is not None:
-        pytest.skip()
-
-    # All unique plant+generator ID combinations:
-    gen_ids = pudl_out_eia.gens_eia860()[
-        ["plant_id_eia", "generator_id"]
-    ].drop_duplicates()
-    # A simple generator_id w/o non-alphanumeric characters or lower-case letters:
-    gen_ids["simple_id"] = gen_ids.generator_id.str.upper().str.replace(
-        r"[^a-zA-Z0-9]", "", regex=True
-    )
-
-    # Identify the set of simple IDs which map to multiple original generator IDs,
-    # meaning that within a single plant, there are generators whose original IDs
-    # only differ by non-alphanumeric characters, or by the case of the letters:
-    multiple_ids = (
-        gen_ids.groupby(["plant_id_eia", "simple_id"])["generator_id"].nunique().gt(1)
-    )
-
-    # Select only those generator IDs that have multiple values:
-    problem_generators = (
-        gen_ids.set_index(["plant_id_eia", "simple_id"]).loc[multiple_ids].sort_index()
-    )
-    if not problem_generators.empty:
-        errstr = f"Found {len(problem_generators)} ambiguous generator IDs."
-        raise ValueError(errstr)
-
-
 def test_nuclear_units_are_generators(pudl_out_eia, live_dbs):
     """Validate that all nuclear Unit IDs correspond to generator IDs."""
     if not live_dbs:
