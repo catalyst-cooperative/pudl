41e900d881b64db64bf3d162ae2d140e965aeb9d
Zane Selvans <zane.selvans@catalyst.coop>
2025-04-02 15:04:57 -0600

    merge alembic heads for SEC 10-k & EIA-930 schema changes

---
+++ migrations/versions/073115e6da74_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 073115e6da74
+Revises: 58ff77a4079c, bc6035553794
+Create Date: 2025-04-02 15:03:13.383041
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '073115e6da74'
+down_revision = ('58ff77a4079c', 'bc6035553794')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
