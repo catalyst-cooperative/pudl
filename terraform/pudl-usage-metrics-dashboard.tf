64ea2f31b66b34ee6020179d857fb2b800fbd2f4
Zane Selvans <zane.selvans@catalyst.coop>
2025-04-04 12:20:35 -0600

    Remove ZEROLab from PUDL Sustainers. (#4164)
    * Remove ZEROLab from PUDL Sustainers.
    
    * fix: pudl usage metrics dashboard rename was never pushed :(
    
    * fix: remove SAs we added for folks to access parquet beta outputs
    
    ---------
    
    Co-authored-by: Dazhong Xia <dazhong.xia@catalyst.coop>

--- terraform/pudl-usage-metrics-dashboard.tf
+++ terraform/pudl-usage-metrics-dashboard.tf
@@ -1,15 +1,15 @@
 // secrets
 locals {
-  metrics_dashboard_secret_versions = {
-    metrics_dashboard_password    = 1
-    metrics_dashboard_db_name     = 1
-    metrics_dashboard_db_username = 1
-    metrics_dashboard_db_password = 1
+  pudl_usage_metrics_dashboard_secret_versions = {
+    pudl_usage_metrics_dashboard_password    = 1
+    pudl_usage_metrics_dashboard_db_name     = 1
+    pudl_usage_metrics_dashboard_db_username = 1
+    pudl_usage_metrics_dashboard_db_password = 1
   }
 }
 
-resource "google_secret_manager_secret" "metrics_dashboard_secrets" {
-  for_each  = local.metrics_dashboard_secret_versions
+resource "google_secret_manager_secret" "pudl_usage_metrics_dashboard_secrets" {
+  for_each  = local.pudl_usage_metrics_dashboard_secret_versions
   secret_id = each.key
   replication {
     auto {}
@@ -17,78 +17,78 @@ resource "google_secret_manager_secret" "metrics_dashboard_secrets" {
 }
 
 // Deployment: SA and artifact registry
-resource "google_service_account" "metrics_dashboard_deploy_gha" {
-  account_id   = "metrics-dashboard-deploy-gha"
+resource "google_service_account" "pudl_usage_metrics_dashboard_deploy_gha" {
+  account_id   = "pudl-usage-metrics-dash-deploy"
   display_name = "SA for metrics dashboard deployment"
 }
 
-resource "google_project_iam_member" "metrics_dashboard_artifact_registry" {
+resource "google_project_iam_member" "pudl_usage_metrics_dashboard_artifact_registry" {
   project = var.project_id
   role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${google_service_account.metrics_dashboard_deploy_gha.email}"
+  member  = "serviceAccount:${google_service_account.pudl_usage_metrics_dashboard_deploy_gha.email}"
 }
 
-resource "google_project_iam_member" "metrics_dashboard_cloud_run_deploy" {
+resource "google_project_iam_member" "pudl_usage_metrics_dashboard_cloud_run_deploy" {
   project = var.project_id
   role    = "roles/run.developer"
-  member  = "serviceAccount:${google_service_account.metrics_dashboard_deploy_gha.email}"
+  member  = "serviceAccount:${google_service_account.pudl_usage_metrics_dashboard_deploy_gha.email}"
 }
 
-resource "google_project_iam_member" "metrics_dashboard_deploy_use_cloud_run" {
+resource "google_project_iam_member" "pudl_usage_metrics_dashboard_deploy_use_cloud_run" {
   project = var.project_id
   role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.metrics_dashboard_deploy_gha.email}"
+  member  = "serviceAccount:${google_service_account.pudl_usage_metrics_dashboard_deploy_gha.email}"
 }
 
-resource "google_artifact_registry_repository" "metrics_dashboard" {
+resource "google_artifact_registry_repository" "pudl_usage_metrics_dashboard" {
   location      = "us-east1"
-  repository_id = "metrics-dashboard"
+  repository_id = "pudl-usage-metrics-dashboard"
   description   = "Docker repository for metrics dashboard"
   format        = "DOCKER"
 }
 
 
 // the actual cloud run SA
-resource "google_service_account" "metrics_dashboard_cloud_run" {
-  account_id   = "metrics-dash"
-  display_name = "Metrics Dashboard Service Account"
+resource "google_service_account" "pudl_usage_metrics_dashboard_cloud_run" {
+  account_id   = "pudl-usage-metrics-dashboard"
+  display_name = "PUDL Usage Metrics Dashboard Service Account"
 }
 
-resource "google_secret_manager_secret_iam_member" "metrics_dashboard_secret_accessor" {
-  for_each  = google_secret_manager_secret.metrics_dashboard_secrets
+resource "google_secret_manager_secret_iam_member" "pudl_usage_metrics_dashboard_secret_accessor" {
+  for_each  = google_secret_manager_secret.pudl_usage_metrics_dashboard_secrets
   secret_id = each.value.secret_id
   role      = "roles/secretmanager.secretAccessor"
-  member    = google_service_account.metrics_dashboard_cloud_run.member
+  member    = google_service_account.pudl_usage_metrics_dashboard_cloud_run.member
 }
 
-resource "google_project_iam_member" "metrics_dashboard_cloud_sql" {
+resource "google_project_iam_member" "pudl_usage_metrics_dashboard_cloud_sql" {
   project = var.project_id
   role    = "roles/cloudsql.client"
-  member  = google_service_account.metrics_dashboard_cloud_run.member
+  member  = google_service_account.pudl_usage_metrics_dashboard_cloud_run.member
 }
 
 // read-only SQL user
 
-data "google_secret_manager_secret_version" "metrics_dashboard_db_password" {
-  secret  = "metrics_dashboard_db_password"
+data "google_secret_manager_secret_version" "pudl_usage_metrics_dashboard_db_password" {
+  secret  = "pudl_usage_metrics_dashboard_db_password"
   version = "1"
 }
 
-data "google_secret_manager_secret_version" "metrics_dashboard_db_username" {
-  secret  = "metrics_dashboard_db_username"
+data "google_secret_manager_secret_version" "pudl_usage_metrics_dashboard_db_username" {
+  secret  = "pudl_usage_metrics_dashboard_db_username"
   version = "1"
 }
 
-resource "google_sql_user" "metrics_dashboard_readonly" {
-  name     = data.google_secret_manager_secret_version.metrics_dashboard_db_username.secret_data
-  password = data.google_secret_manager_secret_version.metrics_dashboard_db_password.secret_data
+resource "google_sql_user" "pudl_usage_metrics_dashboard_readonly" {
+  name     = data.google_secret_manager_secret_version.pudl_usage_metrics_dashboard_db_username.secret_data
+  password = data.google_secret_manager_secret_version.pudl_usage_metrics_dashboard_db_password.secret_data
   instance = "pudl-usage-metrics-db"
 }
 
 
 // cloud run service
-resource "google_cloud_run_v2_service" "metrics_dashboard" {
-  name                = "metrics-dashboard"
+resource "google_cloud_run_v2_service" "pudl_usage_metrics_dashboard" {
+  name                = "pudl-usage-metrics-dashboard"
   location            = "us-east1"
   deletion_protection = false
 
@@ -102,7 +102,7 @@ resource "google_cloud_run_v2_service" "metrics_dashboard" {
     }
 
 
-    service_account = google_service_account.metrics_dashboard_cloud_run.email
+    service_account = google_service_account.pudl_usage_metrics_dashboard_cloud_run.email
     volumes {
       name = "cloudsql"
       cloud_sql_instance {
@@ -111,7 +111,7 @@ resource "google_cloud_run_v2_service" "metrics_dashboard" {
     }
 
     containers {
-      image = "us-east1-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.metrics_dashboard.name}/metrics-dashboard:latest"
+      image = "us-east1-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.pudl_usage_metrics_dashboard.name}/pudl-usage-metrics-dashboard:latest"
 
       resources {
         limits = {
@@ -137,7 +137,7 @@ resource "google_cloud_run_v2_service" "metrics_dashboard" {
       }
 
       dynamic "env" {
-        for_each = local.metrics_dashboard_secret_versions
+        for_each = local.pudl_usage_metrics_dashboard_secret_versions
         content {
           name = upper(env.key)
           value_source {
@@ -153,9 +153,9 @@ resource "google_cloud_run_v2_service" "metrics_dashboard" {
 }
 
 
-resource "google_cloud_run_v2_service_iam_member" "metrics_dashboard_internet" {
-  location = google_cloud_run_v2_service.metrics_dashboard.location
-  name     = google_cloud_run_v2_service.metrics_dashboard.name
+resource "google_cloud_run_v2_service_iam_member" "pudl_usage_metrics_dashboard_internet" {
+  location = google_cloud_run_v2_service.pudl_usage_metrics_dashboard.location
+  name     = google_cloud_run_v2_service.pudl_usage_metrics_dashboard.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
