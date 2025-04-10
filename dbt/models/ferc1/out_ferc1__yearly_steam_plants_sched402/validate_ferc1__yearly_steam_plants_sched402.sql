00f758e83cd26061451b0b50cc2199c90b0c301f
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-04-10 15:35:10 -0400

    Migrate vs_bounds tests for table out_ferc1__yearly_steam_plants_sched402.
    Demoted 5 checks to warnings due to significant quantile shifts in etl-fast, and added tracking issues for further investigation.
    
    Lightly-modified 2 quantiles due to minor shifts in etl-fast, and annotated for attention to investigate if they get worse.

--- dbt/models/ferc1/out_ferc1__yearly_steam_plants_sched402/validate_ferc1__yearly_steam_plants_sched402.sql
+++ dbt/models/ferc1/out_ferc1__yearly_steam_plants_sched402/validate_ferc1__yearly_steam_plants_sched402.sql
@@ -1,4 +1,8 @@
 select
+capacity_factor,
+report_year,
+plant_capability_mw,
+capacity_mw,
 water_limited_capacity_mw / capacity_mw as water_limited_ratio,
 not_water_limited_capacity_mw / capacity_mw as not_water_limited_ratio,
 peak_demand_mw / capacity_mw as peak_demand_ratio,
