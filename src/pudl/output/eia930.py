d3e14067e0bf9fb6171aa6d33c10fd503bfa81f0
zschira <zach.schira@catalyst.coop>
2025-04-21 17:15:04 -0400

    Test new 930 imputation setup

--- src/pudl/output/eia930.py
+++ src/pudl/output/eia930.py
@@ -74,6 +74,58 @@ def _years_from_context(context) -> list[int]:
     ]
 
 
+@asset
+def _out_eia930__combined_demand(
+    _out_eia930__hourly_operations: pd.DataFrame,
+    _out_eia930__hourly_subregion_demand: pd.DataFrame,
+) -> pd.DataFrame:
+    """Combine subregion and BA demand into a single DataFrame to perform imputation."""
+    common_cols = ["datetime_utc", "demand_reported_mwh", "timezone", "generic_id"]
+    return pd.concat(
+        [
+            _out_eia930__hourly_subregion_demand.rename(
+                columns={"combined_subregion_ba_id": "generic_id"}
+            )[common_cols],
+            _out_eia930__hourly_operations.rename(
+                columns={"balancing_authority_code_eia": "generic_id"}
+            )[common_cols],
+        ]
+    )
+
+
+imputed_subregion_demand_assets = impute_timeseries_asset_factory(
+    input_asset_name="_out_eia930__combined_demand",
+    output_asset_name="_out_eia930__combined_imputed_demand",
+    years_from_context=_years_from_context,
+    value_col="demand_reported_mwh",
+    imputed_value_col="demand_imputed_pudl_mwh",
+    id_col="generic_id",
+    settings=ImputeTimeseriesSettings(),
+    output_io_manager_key="io_manager",
+)
+
+
+@asset(io_manager_key="parquet_io_manager")
+def out_eia930__hourly_subregion_demand(
+    _out_eia930__hourly_subregion_demand: pd.DataFrame,
+    _out_eia930__combined_imputed_demand: pd.DataFrame,
+) -> pd.DataFrame:
+    """Merge imputed subregion demand back on subregion table."""
+    return _out_eia930__hourly_subregion_demand.merge(
+        _out_eia930__combined_imputed_demand[
+            [
+                "datetime_utc",
+                "generic_id",
+                "demand_imputed_pudl_mwh",
+                "demand_imputed_pudl_mwh_imputation_code",
+            ]
+        ],
+        left_on=["datetime_utc", "combined_subregion_ba_id"],
+        right_on=["datetime_utc", "generic_id"],
+    )
+
+
+"""
 imputed_subregion_demand_assets = impute_timeseries_asset_factory(
     input_asset_name="_out_eia930__hourly_subregion_demand",
     output_asset_name="out_eia930__hourly_subregion_demand",
@@ -93,3 +145,4 @@ imputed_ba_demand_assets = impute_timeseries_asset_factory(
     imputed_value_col="demand_imputed_pudl_mwh",
     id_col="balancing_authority_code_eia",
 )
+"""
