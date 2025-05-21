bc986e47a284f66f6221d6fd292497758bc26972
Zane Selvans <zane.selvans@catalyst.coop>
2025-05-21 08:28:50 -0600

    Post PUDL v2025.5.0 release cleanup. (#4289)

--- docker/gcp_pudl_etl.sh
+++ docker/gcp_pudl_etl.sh
@@ -197,7 +197,7 @@ function clean_up_outputs_for_distribution() {
     # Remove any remaiining files and directories we don't want to distribute
     rm -rf "$PUDL_OUTPUT/parquet" && \
     rm -f "$PUDL_OUTPUT/metadata.yml" && \
-    rm -f "$PUDL_OUTPUT/pudl_duckdb_tests.duckdb"
+    rm -f "$PUDL_OUTPUT/pudl_dbt_tests.duckdb"
 }
 
 ########################################################################################
