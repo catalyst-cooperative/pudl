db2b682965c63e81b61b8b27c427a9045b688287
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-05-21 10:35:25 -0400

    fix: use assets not checks

--- test/unit/dbt_asset_checks_test.py
+++ test/unit/dbt_asset_checks_test.py
@@ -1,8 +1,8 @@
-from dagster import load_asset_checks_from_modules
+from dagster import load_assets_from_modules
 
 from pudl.etl import dbt_asset_checks
 
 
 def test_asset_checks_exist():
-    loaded_checks = load_asset_checks_from_modules([dbt_asset_checks])
+    loaded_checks = load_assets_from_modules([dbt_asset_checks])[0].check_specs
     assert loaded_checks != []
