e30be55174ade65b3bb1e643ea145069b8d633e8
Zane Selvans <zane.selvans@catalyst.coop>
2025-07-03 15:34:36 -0600

    Clean up / simplify not all null test a little.

--- dbt/tests/data_tests/generic_tests/expect_columns_not_all_null.sql
+++ dbt/tests/data_tests/generic_tests/expect_columns_not_all_null.sql
@@ -8,7 +8,7 @@
 {% set get_columns_query %}
     SELECT column_name
     FROM (DESCRIBE {{ model }})
-    WHERE column_name NOT IN ({{ "'" + exclude_columns | join("', '") + "'" if exclude_columns else "''" }})
+    WHERE column_name NOT IN ('{{ exclude_columns | join("', '") }}')
 {% endset %}
 
 {% if execute %}
@@ -18,7 +18,6 @@
     {% set column_names = [] %}
 {% endif %}
 
-WITH column_null_checks AS (
 {% set checks = [] %}
 {% for column_name in column_names %}
     {% if column_name in conditional_columns %}
@@ -51,6 +50,7 @@ WITH column_null_checks AS (
     {% do checks.append(check) %}
 {% endfor %}
 
+WITH column_null_checks AS (
 {% if checks %}
     {{ checks | join('\nUNION ALL\n') }}
 {% else %}
