2d68ed95c7326cfb18c13682a5f606ba23202194
Zane Selvans <zane.selvans@catalyst.coop>
2025-07-04 11:36:35 -0600

    Apply expect_column_fraction_with_condition test more widely

--- dbt/tests/data_tests/generic_tests/expect_column_fraction_with_condition.sql
+++ dbt/tests/data_tests/generic_tests/expect_column_fraction_with_condition.sql
@@ -1,22 +1,22 @@
 {% test expect_column_fraction_with_condition(
     model,
     column_name,
-    row_condition,
+    numerator_row_condition,
     min_fraction,
     max_fraction
 ) %}
 
 WITH fraction_check AS (
     SELECT
-        SUM(CASE WHEN {{ row_condition }} THEN {{ column_name }} ELSE 0 END) * 1.0 / SUM({{ column_name }}) AS actual_fraction,
-        SUM(CASE WHEN {{ row_condition }} THEN {{ column_name }} ELSE 0 END) AS condition_sum,
+        SUM(CASE WHEN {{ numerator_row_condition }} THEN {{ column_name }} ELSE 0 END) * 1.0 / SUM({{ column_name }}) AS actual_fraction,
+        SUM(CASE WHEN {{ numerator_row_condition }} THEN {{ column_name }} ELSE 0 END) AS condition_sum,
         SUM({{ column_name }}) AS total_sum,
     FROM {{ model }}
 )
 
 SELECT
     'Test failed: actual_fraction of {{ column_name }} where ' ||
-    '{{ row_condition | replace("'", "''")}}' ||
+    '{{ numerator_row_condition | replace("'", "''")}}' ||
     ' is outside of acceptable range.'
     AS failure_message,
     ROUND(actual_fraction, 4) as actual_fraction,
