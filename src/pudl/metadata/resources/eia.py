a22351339271c69872fe15c087b796cb419777ac
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-07-21 14:22:46 -0400

    Migrate core_eia__codes_particulate_compliance_strategies description.

--- src/pudl/metadata/resources/eia.py
+++ src/pudl/metadata/resources/eia.py
@@ -331,7 +331,9 @@ RESOURCE_METADATA: dict[str, dict[str, Any]] = {
         "field_namespace": "eia",
     },
     "core_eia__codes_particulate_compliance_strategies": {
-        "description": "A coding table describing different compliance strategies used to control particulate matter in the EIA-860.",
+        "description": {
+            "additional_summary_text": "compliance strategies used to control particulate matter in the EIA-860.",
+        },
         "schema": {
             "fields": ["code", "label", "description"],
             "primary_key": ["code"],
