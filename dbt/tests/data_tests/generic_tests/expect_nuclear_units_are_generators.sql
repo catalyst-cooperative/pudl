a0567d8cb35b0ec37d4fdb7d5205c1e0af58d7f7
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-07-25 13:59:19 -0400

    Convert singular test validate__out_eia__yearly_generators__nuclear_units to generic test expect_nuclear_units_are_generators.

--- dbt/tests/data_tests/generic_tests/expect_nuclear_units_are_generators.sql
+++ dbt/tests/data_tests/generic_tests/expect_nuclear_units_are_generators.sql
@@ -1,24 +1,27 @@
--- Validate that all nuclear Unit IDs correspond to generator IDs.
+{% test expect_nuclear_units_are_generators(model, generator_model) %}
+
 with NukeGens as (
     -- Get all generators with nuclear fuel
     select distinct
         plant_id_eia,
         generator_id,
         'generator' as generator
-    from {{ source('pudl', 'out_eia__yearly_generators') }}
+    from {{ generator_model }}
     where energy_source_code_1 = 'NUC'
 ), NukeGenerationFuel as (
     -- Get generator IDs for nuclear units that have them
     select
         plant_id_eia,
         nuclear_unit_id as generator_id
-    from {{ source('pudl', 'core_eia923__monthly_generation_fuel_nuclear') }}
+    from {{ model }}
     where nuclear_unit_id != 'UNK'
 ), NukeMatches as (
-    -- Want generators from NikeGenerationFuel to be a subset of the ones
+    -- Want generators from NukeGenerationFuel to be a subset of the ones
     -- from NukeGens, so do a left join and look for nulls on the right
     select *
     from NukeGenerationFuel left join NukeGens using (plant_id_eia, generator_id)
 ) select *
 from NukeMatches
 where generator is null
+
+{% endtest %}
