0a5358fb6789eee12260c251ec3dc966f630b28a
Ella Belfer <e.belfer@berkeley.edu>
2025-08-04 18:27:12 -0400

    Update dois, extract new partition, update row counts

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -317,7 +317,7 @@ SOURCES: dict[str, Any] = {
         "working_partitions": {
             "half_years": [
                 f"{year}half{half}" for year in range(2015, 2026) for half in [1, 2]
-            ][1:-1]  # Begins in H2 of 2015 and currently ends in H1 of 2025
+            ][1:]  # Begins in H2 of 2015 and currently ends in H2 of 2025
         },
         "contributors": [
             CONTRIBUTORS["catalyst-cooperative"],
