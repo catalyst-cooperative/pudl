9ad9e94f9394f4571c463e22d35ca8d782097ba2
Austen Sharpe <austensharpe@gmail.com>
2025-08-08 15:24:30 -0600

    Merge migration heads

---
+++ migrations/versions/06ca81ec4de6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 06ca81ec4de6
+Revises: 341e3c844e85, 9466571f3daa
+Create Date: 2025-08-08 15:24:07.775602
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '06ca81ec4de6'
+down_revision = ('341e3c844e85', '9466571f3daa')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
