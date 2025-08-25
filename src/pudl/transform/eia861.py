52c55bc739ba5fc002de88a6337a606cd9605dc8
Austen Sharpe <austensharpe@gmail.com>
2025-08-25 12:47:41 -0400

    Update _combine_88888_values so it doesn't make NA values blank

--- src/pudl/transform/eia861.py
+++ src/pudl/transform/eia861.py
@@ -1054,7 +1054,8 @@ def _combine_88888_values(df: pd.DataFrame, idx_cols: list[str]) -> pd.DataFrame
         ):
             # Special case: if there are NA values in the index columns we can't
             # combine two dfs with the same index without creating two rows.
-            # This messes up the results, so we drop the NA values from the index
+            # This messes up the results
+            # , so we drop the NA values from the index
             # when combining the newly aggregated groups. This does not affect the
             # actual primary key, it's just a slightly hacky way to combine the rows correctly.
             idx_has_na = group[idx_cols].isna().any()
@@ -1063,9 +1064,11 @@ def _combine_88888_values(df: pd.DataFrame, idx_cols: list[str]) -> pd.DataFrame
             # Now we can safely sum the numeric columns, recombine with the non-numeric
             # columns, and return the result.
             group = group.set_index(mod_idx_cols)
-            non_num_group = group[non_num_cols].iloc[[0]]
+            non_num_group = group[non_num_cols + na_cols].iloc[[0]]
             num_group = (
-                group.drop(columns=non_num_cols)
+                group.drop(
+                    columns=non_num_cols + na_cols
+                )  # STATE is not part of non_num cols so it is getting blanked instead of NA-ed
                 .groupby(mod_idx_cols, dropna=False)
                 .sum()
             )
