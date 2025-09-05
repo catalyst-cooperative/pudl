19c99a9954daa9a44f9e109e6da391447d5a5abb
Austen Sharpe <austensharpe@gmail.com>
2025-09-05 15:50:12 -0600

    Update zenodo archive

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -197,7 +197,7 @@ class ZenodoDoiSettings(BaseSettings):
     eia923: ZenodoDoi = "10.5281/zenodo.16563837"
     eia930: ZenodoDoi = "10.5281/zenodo.16676166"
     eiawater: ZenodoDoi = "10.5281/zenodo.10806016"
-    eiaaeo: ZenodoDoi = "10.5281/zenodo.10838488"
+    eiaaeo: ZenodoDoi = "10.5281/zenodo.15622378"
     eiaapi: ZenodoDoi = "10.5281/zenodo.16676182"
     epacamd_eia: ZenodoDoi = "10.5281/zenodo.14834878"
     epacems: ZenodoDoi = "10.5281/zenodo.16792669"
