698cd14caaf06a1be526c3a1b9114af08babd8e3
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-11-04 14:56:09 -0500

    feat: add small script to compare local/nightly Parquet outputs

---
+++ devtools/check_against_nightly.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+"""Check a local parquet file or parquet files against the nightly build.
+
+Usage:
+
+    ./check_against_nightly.py <local_targets>
+
+Example usage:
+
+    ./check_against_nightly.py core_eia930__*.parquet
+"""
+
+from pathlib import Path
+
+import click
+import polars as pl
+from polars.testing import assert_frame_equal
+
+
+@click.command(help=__doc__)
+@click.argument("local-targets", nargs=-1, type=click.Path())
+def cli(local_targets: tuple[Path]):
+    """Compare local Parquet files against nightly."""
+    local_paths = [Path(target) for target in local_targets]
+    click.echo(f"Comparing {', '.join(p.stem for p in local_paths)} against nightly...")
+    for local_path in local_paths:
+        click.echo(f"Comparing {local_path.stem}...", nl=False)
+        local_lf = pl.scan_parquet(local_path)
+        nightly_base = "https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/nightly"
+        remote_lf = pl.scan_parquet(f"{nightly_base}/{local_path.name}")
+        try:
+            assert_frame_equal(local_lf, remote_lf)
+        except AssertionError as e:
+            click.echo(f"MISMATCH\n{e}")
+        else:
+            click.echo("OK")
+
+
+if __name__ == "__main__":
+    cli()
