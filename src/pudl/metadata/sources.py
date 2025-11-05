086f0141fbb5cb20208b885890e85170b23a15e9
Austen Sharpe <austensharpe@gmail.com>
2025-11-05 15:21:01 -0700

    Update CEMS working partition

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -456,7 +456,7 @@ SOURCES: dict[str, Any] = {
         "working_partitions": {
             "year_quarters": [
                 str(q).lower()
-                for q in pd.period_range(start="1995q1", end="2025q1", freq="Q")
+                for q in pd.period_range(start="1995q1", end="2025q3", freq="Q")
             ]
         },
         "contributors": [
