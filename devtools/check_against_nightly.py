a5e80fd34541639750352d0d4794e0ccf6fd998f
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-11-06 15:05:05 -0500

    feat: allow custom reference dir instead of hard-coding s3

--- devtools/check_against_nightly.py
+++ devtools/check_against_nightly.py
@@ -8,7 +8,17 @@ Usage:
 
 Example usage:
 
+Check against S3 nightly:
+
     ./check_against_nightly.py core_eia930__*.parquet
+
+Check against local copy:
+
+    ./check_against_nightly.py --reference-dir $PUDL_OUTPUT/../nightly/parquet core_eia930__*.parquet
+
+Check against GCS build outputs (requires gcloud application default credentials):
+
+    ./check_against_nightly.py --reference-dir gs://builds.catalyst.coop/2025-10-11-0605-a531a9a7f-main/parquet core_eia930__*.parquet
 """
 
 from pathlib import Path
@@ -23,21 +33,30 @@ from polars.testing import assert_frame_equal
 @click.option(
     "--pdb", is_flag=True, help="Drop into a breakpoint if any dataframes don't match."
 )
-def cli(local_targets: tuple[Path], pdb: bool = False):
+@click.option(
+    "--reference-dir",
+    type=str,
+    default="https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/nightly",
+    help="The directory containing the parquet files to check against. Defaults to S3 nightly.",
+)
+def cli(local_targets: tuple[Path], pdb: bool, reference_dir):
     """Compare local Parquet files against nightly."""
     local_paths = [Path(target) for target in local_targets]
-    click.echo(f"Comparing {', '.join(p.stem for p in local_paths)} against nightly...")
+    click.echo(
+        f"Comparing {', '.join(p.stem for p in local_paths)} against those in {reference_dir}..."
+    )
     for local_path in local_paths:
         click.echo(f"Comparing {local_path.stem}...", nl=False)
-        local_lf = pl.scan_parquet(local_path)
-        nightly_base = "https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/nightly"
-        remote_lf = pl.scan_parquet(f"{nightly_base}/{local_path.name}")
+        local = pl.scan_parquet(local_path)
+        reference = pl.scan_parquet(f"{reference_dir}/{local_path.name}")
         try:
-            assert_frame_equal(local_lf, remote_lf)
+            assert_frame_equal(local, reference)
         except AssertionError as e:
             if pdb:
                 breakpoint()
-            click.echo(f"MISMATCH\n{e}")
+            click.echo(
+                f"MISMATCH\n{e}".replace("left", "local").replace("right", "reference")
+            )
         else:
             click.echo("OK")
 
