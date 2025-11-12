8c347e6d81216a204ea58a442c43c071c39b4f1c
Mead Barrel <lai.tash@gmail.com>
2025-11-12 16:16:12 -0300

    Create core_eia176__yearly_gas_disposition_by_consumer (#4728)
    * make draft pull request
    
    * Add asset for core_eia176__yearly_gas_disposition_by_consumer
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    * Change "customer_type" to "consumer_type"
    
    * Add metadata/resources/eia176.py and switch back to using
    "customer_type"
    
    * Update src/pudl/metadata/resources/eia176.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * Update core_eia176__yearly_gas_disposition_by_consumer
    
     - Drop all rows with all NA values
     - Drop rows with non-standard operating states
     - Add `revenue_class` category
     - Rename `volume` to `volume_mfc`
     - Add field metadata
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    * Remove unnecessary line
    
    * Change operator_utility_id_eia back to operator_id_eia
    
    * Migration: add core_eia176__yearly_gas_disposition_by_consumer
    
    * Add row counts for core_eia176__yearly_gas_disposition_by_consumer
    
    * Update schema for core_eia176__yearly_gas_disposition_by_consumer
    
    * Rename volume_mfc to volume_mcf and add some descriptions to fields
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    * Address CI issues
    
    * Update src/pudl/metadata/fields.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * Update src/pudl/metadata/fields.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * Update src/pudl/metadata/fields.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    * Update src/pudl/metadata/fields.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * Update src/pudl/metadata/fields.py
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * Update dbt/models/eia176/core_eia176__yearly_gas_disposition_by_consumer/schema.yml
    
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    For more information, see https://pre-commit.ci
    
    * Remove old expression_is_true check
    
    * Merge migrations
    
    * Remove unnecessary type conversions
    
    * Improve memory consumption
    
    * Update release notes
    
    * Remove unused assignment
    
    * Address make ci changes
    
    * Update migrations
    
    * Update migrations
    
    * Change field descriptions to not use enums
    
    * Update field descriptions to remove enums, clarify eia176 vs eia861 field types
    
    * add updated migration
    
    * Update volume totals match requirement to handle fast ETL
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: E. Belfer <37471869+e-belfer@users.noreply.github.com>
    Co-authored-by: Ella Belfer <e.belfer@berkeley.edu>

---
+++ src/pudl/metadata/resources/eia176.py
@@ -0,0 +1,35 @@
+"""Definitions of data tables primarily coming from EIA-860."""
+
+from typing import Any
+
+RESOURCE_METADATA: dict[str, dict[str, Any]] = {
+    "core_eia176__yearly_gas_disposition_by_consumer": {
+        "description": {
+            "additional_summary_text": (
+                "a company's natural gas deliveries to end-use consumers within the report state."
+            ),
+            "additional_source_text": "(Part 6, Lines 10.0-11.6)",
+        },
+        "schema": {
+            "fields": [
+                "report_year",
+                "operator_id_eia",
+                "operating_state",
+                "customer_class",
+                "revenue_class",
+                "consumers",
+                "revenue",
+                "volume_mcf",
+            ],
+            "primary_key": [
+                "report_year",
+                "operator_id_eia",
+                "customer_class",
+                "revenue_class",
+            ],
+        },
+        "field_namespace": "eia",
+        "sources": ["eia176"],
+        "etl_group": "eia176",
+    }
+}
