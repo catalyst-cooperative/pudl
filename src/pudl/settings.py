a0af25023a8500ca950454754bac3ff8c289606c
Zane Selvans <zane.selvans@catalyst.coop>
2025-12-14 21:10:45 -0600

    Add most recent FERC EQR quarter and merge dependencies from main

--- src/pudl/settings.py
+++ src/pudl/settings.py
@@ -30,7 +30,7 @@ ferceqr_year_quarters: StaticPartitionsDefinition = StaticPartitionsDefinition(
         f"{year}q{quarter}"
         for year in range(2013, 2026)
         for quarter in range(1, 5)
-        if not ((year == 2013 and quarter < 3) or (year == 2025 and quarter > 2))
+        if not ((year == 2013 and quarter < 3) or (year == 2025 and quarter > 3))
     ]
 )
 
