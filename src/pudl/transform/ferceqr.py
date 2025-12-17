922ead6126ad8791804f66ca8a9cd8495f285831
zschira <zach.schira@catalyst.coop>
2025-12-17 15:18:03 -0500

    Try to make eqr transforms more readable

--- src/pudl/transform/ferceqr.py
+++ src/pudl/transform/ferceqr.py
@@ -12,12 +12,13 @@ from pudl.metadata.classes import Resource
 from pudl.settings import ferceqr_year_quarters
 
 
-def apply_dtypes_to_duckdb_table(
+def transform_eqr_table(
     table_name: str,
     table_data: duckdb.DuckDBPyRelation,
-    **col_expressions: dict[str, duckdb.Expression],
-):
-    """Generate a select statement containing transforms to all columns and cast to correct dtypes.
+    year_quarter: str,
+    col_expressions: dict[str, duckdb.Expression],
+) -> ParquetData:
+    """Generate a SQL query to transform an EQR table, then write outputs to parquet.
 
     EQR transforms are implemented using the duckdb expression api, which allows us
     to dynamically build SQL statements to operate on each column. By collecting all
@@ -28,19 +29,25 @@ def apply_dtypes_to_duckdb_table(
         table_name: Name of table, which should have corresponding table level metadata.
         col_expressions: Map column names to pre-generated expressions. Each of these
             expressions will have a ``cast`` added on to it to apply correct dtypes.
-            For columns defined in the table schema, but
+            Columns defined in the table schema, but not in col_expressions will
+            also be cast to appropriate dtype.
     """
     dtypes = Resource.from_id(table_name).to_duckdb_dtypes()
-    return table_data.select(
-        *[
-            expression.cast(dtypes[col]).alias(col)
-            for col, expression in col_expressions.items()
-        ],
-        *[
-            duckdb.ColumnExpression(col).cast(dtype).alias(col)
-            for col, dtype in dtypes.items()
-            if col not in col_expressions
-        ],
+    return persist_table_as_parquet(
+        table_data=table_data.select(
+            *[
+                expression.cast(dtypes[col]).alias(col)
+                for col, expression in col_expressions.items()
+            ],
+            *[
+                duckdb.ColumnExpression(col).cast(dtype).alias(col)
+                for col, dtype in dtypes.items()
+                if col not in col_expressions
+            ],
+        ),
+        table_name=table_name,
+        partitions={"year_quarter": year_quarter},
+        use_output_dir=True,
     )
 
 
@@ -69,11 +76,12 @@ def core_ferceqr__identity(
         table,
         _,
     ):
-        return persist_table_as_parquet(
-            apply_dtypes_to_duckdb_table(
-                table_name="core_ferceqr__identity",
-                table_data=table,
-                transactions_reported_to_index_price_publishers=(
+        return transform_eqr_table(
+            table_name="core_ferceqr__identity",
+            table_data=table,
+            year_quarter=year_quarter,
+            col_expressions={
+                "transactions_reported_to_index_price_publishers": (
                     duckdb.CaseExpression(
                         condition=duckdb.SQLExpression(
                             "UPPER(transactions_reported_to_index_price_publishers)"
@@ -88,10 +96,7 @@ def core_ferceqr__identity(
                         value=False,
                     )
                 ),
-            ),
-            table_name="core_ferceqr__identity",
-            partitions={"year_quarter": year_quarter},
-            use_output_dir=True,
+            },
         )
 
 
@@ -104,33 +109,33 @@ def core_ferceqr__transactions(context, raw_ferceqr__transactions: ParquetData):
     with duckdb_relation_from_parquet(
         raw_ferceqr__transactions, use_all_partitions=True
     ) as (table, _):
-        return persist_table_as_parquet(
-            apply_dtypes_to_duckdb_table(
-                table_name="core_ferceqr__transactions",
-                table_data=table,
-                transaction_begin_date=duckdb.SQLExpression(
+        return transform_eqr_table(
+            table_name="core_ferceqr__transactions",
+            table_data=table,
+            year_quarter=year_quarter,
+            col_expressions={
+                "transaction_begin_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(transaction_begin_date, '%Y%m%d%H%M')"
                 ),
-                transaction_end_date=duckdb.SQLExpression(
+                "transaction_end_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(transaction_end_date, '%Y%m%d%H%M')"
                 ),
-                trade_date=duckdb.SQLExpression("TRY_STRPTIME(trade_date, '%Y%m%d')"),
-                contract_service_agreement_id=duckdb.ColumnExpression(
+                "trade_date": duckdb.SQLExpression(
+                    "TRY_STRPTIME(trade_date, '%Y%m%d')"
+                ),
+                "contract_service_agreement_id": duckdb.ColumnExpression(
                     "contract_service_agreement"
                 ),
-                exchange_brokerage_service=_na_to_null("exchange_brokerage_service"),
-                type_of_rate=_na_to_null("type_of_rate"),
-                timezone=_na_to_null("time_zone"),
-                class_name=_na_to_null("class_name"),
-                term_name=_na_to_null("term_name"),
-                increment_name=_na_to_null("increment_name"),
-                increment_peaking_name=_na_to_null("increment_peaking_name"),
-                product_name=_na_to_null("product_name"),
-                rate_units=_na_to_null("rate_units"),
-            ),
-            table_name="core_ferceqr__transactions",
-            partitions={"year_quarter": year_quarter},
-            use_output_dir=True,
+                "exchange_brokerage_service": _na_to_null("exchange_brokerage_service"),
+                "type_of_rate": _na_to_null("type_of_rate"),
+                "timezone": _na_to_null("time_zone"),
+                "class_name": _na_to_null("class_name"),
+                "term_name": _na_to_null("term_name"),
+                "increment_name": _na_to_null("increment_name"),
+                "increment_peaking_name": _na_to_null("increment_peaking_name"),
+                "product_name": _na_to_null("product_name"),
+                "rate_units": _na_to_null("rate_units"),
+            },
         )
 
 
@@ -141,35 +146,38 @@ def core_ferceqr__contracts(context, raw_ferceqr__contracts: ParquetData):
     with duckdb_relation_from_parquet(
         raw_ferceqr__contracts, use_all_partitions=True
     ) as (table, _):
-        return persist_table_as_parquet(
-            apply_dtypes_to_duckdb_table(
-                table_name="core_ferceqr__contracts",
-                table_data=table,
-                contract_execution_date=duckdb.SQLExpression(
+        return transform_eqr_table(
+            table_name="core_ferceqr__contracts",
+            table_data=table,
+            year_quarter=year_quarter,
+            col_expressions={
+                "contract_execution_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(contract_execution_date, '%Y%m%d')"
                 ),
-                commencement_date_of_contract_term=duckdb.SQLExpression(
+                "commencement_date_of_contract_term": duckdb.SQLExpression(
                     "TRY_STRPTIME(commencement_date_of_contract_term, '%Y%m%d')"
                 ),
-                contract_termination_date=duckdb.SQLExpression(
+                "contract_termination_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(contract_termination_date, '%Y%m%d')"
                 ),
-                actual_termination_date=duckdb.SQLExpression(
+                "actual_termination_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(actual_termination_date, '%Y%m%d')"
                 ),
-                class_name=_na_to_null("class_name"),
-                term_name=_na_to_null("term_name"),
-                increment_name=_na_to_null("increment_name"),
-                increment_peaking_name=_na_to_null("increment_peaking_name"),
-                product_type_name=_na_to_null("product_type_name"),
-                product_name=_na_to_null("product_name"),
-                rate_units=_na_to_null("rate_units"),
-                units=_na_to_null("units"),
-                begin_date=duckdb.SQLExpression(
+                "class_name": _na_to_null("class_name"),
+                "term_name": _na_to_null("term_name"),
+                "increment_name": _na_to_null("increment_name"),
+                "increment_peaking_name": _na_to_null("increment_peaking_name"),
+                "product_type_name": _na_to_null("product_type_name"),
+                "product_name": _na_to_null("product_name"),
+                "rate_units": _na_to_null("rate_units"),
+                "units": _na_to_null("units"),
+                "begin_date": duckdb.SQLExpression(
                     "TRY_STRPTIME(begin_date, '%Y%m%d%H%M')"
                 ),
-                end_date=duckdb.SQLExpression("TRY_STRPTIME(end_date, '%Y%m%d%H%M')"),
-                contract_affiliate=(
+                "end_date": duckdb.SQLExpression(
+                    "TRY_STRPTIME(end_date, '%Y%m%d%H%M')"
+                ),
+                "contract_affiliate": (
                     duckdb.CaseExpression(
                         condition=duckdb.SQLExpression("UPPER(contract_affiliate)")
                         == duckdb.ConstantExpression("Y"),
@@ -180,10 +188,7 @@ def core_ferceqr__contracts(context, raw_ferceqr__contracts: ParquetData):
                         value=False,
                     )
                 ),
-            ),
-            table_name="core_ferceqr__contracts",
-            partitions={"year_quarter": year_quarter},
-            use_output_dir=True,
+            },
         )
 
 
@@ -194,16 +199,16 @@ def core_ferceqr__index_pub(context, raw_ferceqr__index_pub: ParquetData):
     with duckdb_relation_from_parquet(
         raw_ferceqr__index_pub, use_all_partitions=True
     ) as (table, _):
-        return persist_table_as_parquet(
-            apply_dtypes_to_duckdb_table(
-                table_name="core_ferceqr__index_pub",
-                table_data=table,
-                index_price_publisher_name=_na_to_null(
+        return transform_eqr_table(
+            table_name="core_ferceqr__index_pub",
+            table_data=table,
+            year_quarter=year_quarter,
+            col_expressions={
+                "index_price_publisher_name": _na_to_null(
                     "Index_Price_Publishers_To_Which_Sales_Transactions_Have_Been_Reported"
                 ),
-                transactions_reported=duckdb.ColumnExpression("Transactions_Reported"),
-            ),
-            table_name="core_ferceqr__index_pub",
-            partitions={"year_quarter": year_quarter},
-            use_output_dir=True,
+                "transactions_reported": duckdb.ColumnExpression(
+                    "Transactions_Reported"
+                ),
+            },
         )
