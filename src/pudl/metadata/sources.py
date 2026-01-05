d626878c31401206efaae147e694b00ff2e0bdd1
Christina Gosnell <cgosnell@catalyst.coop>
2026-01-05 15:06:38 -0500

    draft eia860m 2025-nov integration

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -200,7 +200,7 @@ SOURCES: dict[str, Any] = {
         "working_partitions": {
             "year_months": [
                 str(q).lower()
-                for q in pd.period_range(start="2015-07", end="2025-10", freq="M")
+                for q in pd.period_range(start="2015-07", end="2025-11", freq="M")
             ],
         },
         "keywords": sorted(
