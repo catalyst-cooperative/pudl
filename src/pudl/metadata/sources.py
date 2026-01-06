bb32387725f8a8f8c6c817ddf27a2a1706cea086
Austen Sharpe <49878195+aesharpe@users.noreply.github.com>
2026-01-06 11:09:53 -0700

    Add metadata for USDA RUS forms 7 & 12 (#4905)
    * Add metadata for USDA forms 7 & 12
    
    * Add annual to usda metadata description
    
    * Add blank USDA Form 7 and 12 PDFs to data source docs.
    
    * Add older blank USDA Form 7 and 12 PDFs to data source docs.
    
    * Add UEP Bulletin 1717B-2 and 1717B-3 PDFs with USDA Form 7 & 12 Instructions.
    
    * Updates to usda metadata - change it to rus and add other small fixes
    
    * Rename data_source file from usda to rus
    
    * make coops utils for consistency
    
    * Update Matt Grimley contributor metadata
    
    * Add orcid id to Matt Grimley metadata
    
    ---------
    
    Co-authored-by: Zane Selvans <zane.selvans@catalyst.coop>

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -884,6 +884,81 @@ SOURCES: dict[str, Any] = {
         "license_raw": LICENSES["us-govt"],
         "license_pudl": LICENSES["cc-by-4.0"],
     },
+    "rus7": {
+        "title": "USDA RUS Form 7 -- Financial and Operating Report: Electric Distribution",
+        "path": "https://www.rd.usda.gov/resources/forms/rus-forms",
+        "description": (
+            "The United States Department of Agriculture (USDA) Financial and "
+            "Operating Report for Electric Distribution (formerly known as Form 7) "
+            "is an annual report submitted by rural electric utilities "
+            "that receive funding from the USDA Rural Utilities Service (RUS) for "
+            "distribution services. This data was obtained through multiple Freedom "
+            "of Information Access (FOIA) requests."
+        ),
+        "source_file_dict": {
+            "respondents": "Rural electric utilities borrowing from USDA RUS.",
+            "source_format": "Comma Separated Value (.csv)",
+        },
+        "working_partitions": {
+            "years": sorted(set(range(2006, 2022))),
+        },
+        "contributors": [
+            CONTRIBUTORS["catalyst-cooperative"],
+            CONTRIBUTORS["matthew-grimley"],
+        ],
+        "keywords": sorted(
+            set(
+                [
+                    "form 7",
+                    "distribution",
+                ]
+                + KEYWORDS["rus"]
+                + KEYWORDS["us_govt"]
+                + KEYWORDS["finance"]
+            )
+        ),
+        "license_raw": LICENSES["us-govt"],
+        "license_pudl": LICENSES["cc-by-4.0"],
+    },
+    "rus12": {
+        "title": "USDA RUS Form 12 -- Financial and Operating Report: Electric Power Supply",
+        "path": "https://www.rd.usda.gov/resources/forms/rus-forms",
+        "description": (
+            "The United States Department of Agriculture (USDA) Financial and "
+            "Operating Report for Electric Power Supply (formerly known as Form 12) "
+            "is an annual report submitted by rural electric utilities "
+            "that receive funding from the USDA Rural Utilities Service (RUS) for "
+            "power supply (generation). This data was obtained through multiple Freedom "
+            "of Information Access (FOIA) requests."
+        ),
+        "source_file_dict": {
+            "respondents": "Rural electric utilities borrowing from USDA RUS.",
+            "source_format": "Comma Separated Value (.csv)",
+        },
+        "working_partitions": {
+            "years": sorted(set(range(2006, 2022))),
+        },
+        "contributors": [
+            CONTRIBUTORS["catalyst-cooperative"],
+            CONTRIBUTORS["matthew-grimley"],
+        ],
+        "keywords": sorted(
+            set(
+                [
+                    "form 12",
+                    "power supply",
+                ]
+                + KEYWORDS["rus"]
+                + KEYWORDS["us_govt"]
+                + KEYWORDS["finance"]
+                + KEYWORDS["electricity"]
+                + KEYWORDS["plants"]
+                + KEYWORDS["fuels"]
+            )
+        ),
+        "license_raw": LICENSES["us-govt"],
+        "license_pudl": LICENSES["cc-by-4.0"],
+    },
     "vcerare": {
         "title": "Vibrant Clean Energy Resource Adequacy Renewable Energy (RARE) Power Dataset",
         "path": "https://vibrantcleanenergy.com/products/datasets/",
