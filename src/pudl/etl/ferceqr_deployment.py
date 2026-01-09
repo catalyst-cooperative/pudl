492b49137a04bda74b57b7bcbbdd1a4564b11b11
zschira <zach.schira@catalyst.coop>
2026-01-09 12:41:11 -0500

    Improve eqr deployment log handling and fail more gracefully

--- src/pudl/etl/ferceqr_deployment.py
+++ src/pudl/etl/ferceqr_deployment.py
@@ -12,6 +12,8 @@ of the build, and triggering shutdown of the batch job.
 """
 
 import os
+import traceback
+from collections.abc import Callable
 from pathlib import Path
 from typing import Literal
 
@@ -63,12 +65,47 @@ def _notify_slack_deployments_channel(
     )
 
 
+def _get_etl_status_csv_path() -> Path:
+    return PudlPaths().output_dir / "ferceqr_etl_status.csv"
+
+
+def _get_logfile_pointer() -> str:
+    """Return pointer to logs to send in slack message."""
+    return (
+        f"<https://storage.cloud.google.com/builds.catalyst.coop/ferceqr_logs/{os.environ['BUILD_ID']}.log|*Download ferceqr logs to your computer*>\n\n"
+        f"<https://console.cloud.google.com/batch/jobsDetail/regions/us-west1/jobs/run-ferceqr-etl-{os.environ['BUILD_ID']}/logs?project=catalyst-cooperative-pudl|*Query ferceqr logs online*>\n\n"
+    )
+
+
 def _write_status_file(status: Literal["SUCCESS", "FAILURE"]):
     """Notify build script that job is complete by creating a status file."""
     (PudlPaths().output_dir / status).touch()
 
 
-@dg.asset
+def deployment_status_asset(
+    handler: Callable,
+) -> dg.AssetsDefinition:
+    """Create a wrapper for deployment handler assets.
+
+    This is useful to gracefully handle errors if the deployment status assets fail
+    for any reason. When these assets fail, sometimes the logs don't show up in the
+    batch job appropriately, and the status file never gets created, so the job keeps
+    running until it eventually times out.
+    """
+
+    @dg.asset(name=handler.__name__)
+    def _status_handler_asset():
+        try:
+            handler()
+        except Exception:
+            logger.error("FERCEQR deployment handler failed!")
+            logger.error(traceback.format_exc())
+            _write_status_file("FAILURE")
+
+    return _status_handler_asset
+
+
+@deployment_status_asset
 def deploy_ferceqr():
     """Publish EQR outputs to cloud storage."""
     # Get output locations for S3 and GCS
@@ -101,27 +138,30 @@ def deploy_ferceqr():
     # Send slack notification about successful build
     logger.info("Notifying slack about successful build.")
     _notify_slack_deployments_channel(
-        ":large_green_circle: :sunglasses: :unicorn_face: :rainbow: ferceqr deployment succeeded!!"
-        " :partygritty: :database_parrot: :blob-dance: :large_green_circle:\n\n"
-        f"Parquet files published to: {', '.join(map(str, distribution_paths))}\n"
-        f"Logfile can be found at: {os.environ['GCS_LOGS_BUCKET']}"
+        (
+            ":large_green_circle: :sunglasses: :unicorn_face: :rainbow: ferceqr deployment succeeded!!"
+            " :partygritty: :database_parrot: :blob-dance: :large_green_circle:\n\n"
+            f"Parquet files published to: {', '.join(str(p[0]) for p in distribution_paths)}\n"
+            f"Logfile can be found at: {os.environ['GCS_LOGS_BUCKET']}\n\n"
+        )
+        + _get_logfile_pointer()
     )
     _write_status_file("SUCCESS")
 
 
-@dg.asset
+@deployment_status_asset
 def handle_ferceqr_deployment_failure():
     """Send notification if EQR deployment failed."""
-    logger.info("Build failed, notifying slack.")
+    logger.error("Build failed, notifying slack.")
     _notify_slack_deployments_channel(
-        message=":x: ferceqr deployment failed! See step status here:",
+        message=(
+            ":x: ferceqr deployment failed!\n\n"
+            + _get_logfile_pointer()
+            + "See individual step status in attached file: "
+        ),
         attached_file_path=str(_get_etl_status_csv_path()),
     )
     _write_status_file("FAILURE")
-
-
-def _get_etl_status_csv_path() -> Path:
-    return PudlPaths().output_dir / "ferceqr_etl_status.csv"
 
 
 @dg.sensor(
