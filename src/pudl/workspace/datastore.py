bedf2f9a094818d91c8b1b951ee68ddfb820ef02
Christina Gosnell <cgosnell@catalyst.coop>
2026-01-14 14:42:02 -0500

    add back in 2006 and use prod archive

--- src/pudl/workspace/datastore.py
+++ src/pudl/workspace/datastore.py
@@ -210,7 +210,7 @@ class ZenodoDoiSettings(BaseSettings):
     gridpathratoolkit: ZenodoDoi = "10.5281/zenodo.10892394"
     nrelatb: ZenodoDoi = "10.5281/zenodo.12658647"
     phmsagas: ZenodoDoi = "10.5281/zenodo.17076661"
-    rus7: ZenodoDoi = "10.5072/zenodo.423923"
+    rus7: ZenodoDoi = "10.5281/zenodo.18248546"
     sec10k: ZenodoDoi = "10.5281/zenodo.15161694"
     vcerare: ZenodoDoi = "10.5281/zenodo.15166129"
 
