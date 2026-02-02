4c34b9137ef06ebf777036f75c7c3ee285595c50
Ella Belfer <e.belfer@berkeley.edu>
2026-02-02 17:49:16 -0500

    Save to devtools, update matches, add overrides, add unit tests

--- src/pudl/scripts/auto_match_utilities.py
+++ src/pudl/scripts/auto_match_utilities.py
@@ -1,6 +1,7 @@
 """A CLI tool for matching FERC and EIA utilities."""
 
 import importlib
+from pathlib import Path
 from typing import Literal
 
 import click
@@ -41,11 +42,17 @@ def clean_utility_name(col):
     return col
 
 
-def match_utility_names(eia_df: pd.DataFrame, ferc_df: pd.DataFrame):
+def match_utility_names(
+    eia_df: pd.DataFrame, ferc_df: pd.DataFrame, false_matches: pd.DataFrame
+):
     """Match FERC and EIA utilities based on their utility names.
 
     We note how many of these records are already matched to one another, and
     ignore these matches.
+
+    We also ignore matches contained in
+    src/pudl/package_data/glue/utility_id_pudl_false_matches.csv. These are matches that
+    we have hand-labelled as incorrect.
     """
     # Try an exact match on the cleaned name
     matches = ferc_df.merge(
@@ -69,26 +76,48 @@ def match_utility_names(eia_df: pd.DataFrame, ferc_df: pd.DataFrame):
         | (matches.utility_id_pudl_ferc1 is None)
         | (matches.utility_id_pudl_eia is None)
     ]
+
+    matches = pd.merge(
+        matches,
+        false_matches,
+        on=["utility_id_ferc1", "utility_id_eia"],
+        suffixes=["", "_ignore"],
+        how="outer",
+        indicator=True,
+    )
+    # Drop the matches we wish to ignore
+    logger.info(f"Ignoring {len(matches.loc[matches._merge == 'both'])} matches.")
+    matches = matches.loc[matches._merge == "left_only"].drop(
+        columns=["_merge"] + [col for col in matches.columns if col.endswith("_ignore")]
+    )
     return matches
 
 
-def get_existing_overrides():
+def get_existing_glue_df():
     """Read in the existing handmade glue spreadsheet."""
     return pd.read_csv(
         importlib.resources.files("pudl.package_data.glue") / "utility_id_pudl.csv"
     )
 
 
+def get_false_matches():
+    """Read in the existing handmade false matches spreadsheet."""
+    return pd.read_csv(
+        importlib.resources.files("pudl.package_data.glue")
+        / "utility_id_pudl_false_matches.csv"
+    )
+
+
 def drop_records_with_matches(
     entity: Literal["ferc1", "eia"],
-    matches: pd.DataFrame,
-    overrides: pd.DataFrame,
-    override_matches: pd.DataFrame,
-    override_unmatched: pd.DataFrame,
+    matches_new: pd.DataFrame,
+    existing_glue_df: pd.DataFrame,
+    matches_existing: pd.DataFrame,
+    unmatched_existing: pd.DataFrame,
 ):
     """Drop records in the original dataframe where matches have been found.
 
-    This takes the original overrides dataframe, and drops records which were
+    This takes the original existing_glue_df dataframe, and drops records which were
     previously unmatched and have been matched using the automated matching method.
     We do this in preparation for adding the new matches to the spreadsheet.
     """
@@ -101,29 +130,31 @@ def drop_records_with_matches(
     # Get all records where:
     # 1) The utility ID of the entity is unmatched in the override CSV
     # 2) The PUDL-assigned utility ID isn't already used in any match.
-    entity_matched = matches[
-        matches[utility_column].isin(override_unmatched[utility_column])
-        & (~matches[utility_pudl_column].isin(override_matches.utility_id_pudl))
+    entity_matched = matches_new[
+        matches_new[utility_column].isin(unmatched_existing[utility_column])
+        & (~matches_new[utility_pudl_column].isin(matches_existing.utility_id_pudl))
     ]
-    drop_records = overrides[
-        overrides[utility_column].isin(entity_matched[utility_column])
+    drop_records = existing_glue_df[
+        existing_glue_df[utility_column].isin(entity_matched[utility_column])
     ]
     # Check that we aren't dropping any EIA matches
     assert drop_records[matching_utility_column].isnull().all()
     # Check that no other plants are mapped to this ID before we drop it
     pudl_ids = drop_records.utility_id_pudl
-    assert overrides[
-        (overrides.utility_id_pudl.isin(pudl_ids))
-        & (overrides[matching_utility_column].notnull())
+    assert existing_glue_df[
+        (existing_glue_df.utility_id_pudl.isin(pudl_ids))
+        & (existing_glue_df[matching_utility_column].notnull())
     ].empty
     # Drop these unmatched records so we can replace them with matches.
     logger.info(
         f"Dropping {len(drop_records)} unmatched {entity} records to replace with matches."
     )
-    return overrides.drop(drop_records.index, axis=0)
+    return existing_glue_df.drop(drop_records.index, axis=0)
 
 
-def add_new_matches_to_spreadsheet(matches: pd.DataFrame, overrides: pd.DataFrame):
+def add_new_matches_to_dataframe(
+    matches_new: pd.DataFrame, existing_glue_df: pd.DataFrame
+):
     """Add new matches to existing hand-mapped dataframe.
 
     We add new matches and assign them new PUDL utility IDs, following one of the
@@ -141,17 +172,17 @@ def add_new_matches_to_spreadsheet(matches: pd.DataFrame, overrides: pd.DataFram
     from manual review to ensure that no unexpected connections have been created.
     """
     final_updates = []
-    matches["utility_id_pudl"] = pd.NA
+    matches_new["utility_id_pudl"] = pd.NA
 
-    for _i, match in matches.iterrows():
-        if match.utility_id_ferc1 in set(overrides.utility_id_ferc1):
-            if match.utility_id_eia in set(overrides.utility_id_eia):
+    for _i, match in matches_new.iterrows():
+        if match.utility_id_ferc1 in set(existing_glue_df.utility_id_ferc1):
+            if match.utility_id_eia in set(existing_glue_df.utility_id_eia):
                 raise ValueError(
                     f"Found a complex match that connects to different FERC and EIA utilities. This will require manual debugging: {match}"
                 )
             match.utility_id_pudl = match.utility_id_pudl_ferc1
-        elif match.utility_id_eia in set(overrides.utility_id_eia):
-            if match.utility_id_ferc1 in set(overrides.utility_id_ferc1):
+        elif match.utility_id_eia in set(existing_glue_df.utility_id_eia):
+            if match.utility_id_ferc1 in set(existing_glue_df.utility_id_ferc1):
                 raise ValueError(
                     f"Found a complex match that connects to different FERC and EIA utilities. This will require manual debugging: {match}"
                 )
@@ -166,12 +197,15 @@ def add_new_matches_to_spreadsheet(matches: pd.DataFrame, overrides: pd.DataFram
     # and can be reassigned without impact.
     # Reassign the blank PUDL IDs by auto-incrementing from the current max record value.
     # First, assign a group ID for each group of cleaned utility names.
+    # We use the cleaned utility name to do this as this is the field
+    # we used to originally join these records.
+    # TODO: If we introduce different joining methods (e.g., address), revise this method.
     updates_df = updates_df.assign(
         group_id=lambda x: x.groupby("cleaned_utility_name").ngroup()
     )
     # Then use this value to fill NAs in the utility ID PUDL.
     updates_df.utility_id_pudl = updates_df.utility_id_pudl.fillna(
-        updates_df.group_id + max(overrides.utility_id_pudl)
+        updates_df.group_id + max(existing_glue_df.utility_id_pudl)
     )
     updates_df = updates_df.drop(columns=["group_id", "cleaned_utility_name"])
     # Add a note to these records to help us quickly identify which were matched automatically.s
@@ -181,13 +215,13 @@ def add_new_matches_to_spreadsheet(matches: pd.DataFrame, overrides: pd.DataFram
     logger.info(
         f"Adding {len(updates_df)} utility matches to the override spreadsheet. Remember to manually review these."
     )
-    updated_overrides = pd.concat([overrides, updates_df]).sort_values(
-        "utility_id_pudl"
+    updated_existing_glue_df = pd.concat([existing_glue_df, updates_df]).sort_values(
+        ["utility_id_pudl", "utility_id_ferc1", "utility_id_eia"]
     )
 
     # Run some sanity checks
     # Flag all records where one utility ID corresponds to more than one FERC AND more than one EIA utility.
-    ids_per_utility = updated_overrides.groupby("utility_id_pudl")[
+    ids_per_utility = updated_existing_glue_df.groupby("utility_id_pudl")[
         ["utility_id_ferc1", "utility_id_eia"]
     ].nunique()
     multi_utility_ids = ids_per_utility[
@@ -197,101 +231,25 @@ def add_new_matches_to_spreadsheet(matches: pd.DataFrame, overrides: pd.DataFram
         "The following records have more than one FERC and more than one EIA utility matched to them. Please manually review!"
     )
     logger.warning(
-        updated_overrides[updated_overrides.utility_id_pudl.isin(multi_utility_ids)]
+        updated_existing_glue_df[
+            updated_existing_glue_df.utility_id_pudl.isin(multi_utility_ids)
+        ]
     )
-    return updated_overrides
+    return updated_existing_glue_df
 
 
 def write_updated_matches(test_run: bool, dataframe: pd.DataFrame):
     """Write the updated matching spreadsheet to disk."""
     if test_run:
-        csv_path = (
-            importlib.resources.files("pudl.package_data.glue")
-            / "utility_id_pudl_test_update.csv"
-        )
+        repo_root = Path.cwd()
+        devtools_dir = Path(repo_root / "devtools" / "ferc1-eia-glue")
+        csv_path = devtools_dir / "utility_id_pudl_test_update.csv"
     else:
         csv_path = (
             importlib.resources.files("pudl.package_data.glue") / "utility_id_pudl.csv"
         )
     logger.info(f"Writing matches to {csv_path}")
     dataframe.to_csv(csv_path, index=False)
-
-
-@click.command(
-    context_settings={"help_option_names": ["-h", "--help"]},
-)
-@click.option(
-    "--test-run",
-    is_flag=True,
-    default=False,
-    help="If passed, will save the updated spreadsheet to a test file rather than overwriting the existing package data.",
-)
-def get_likely_address_matches(test_run: bool):
-    """Match EIA and FERC utilities based on utility address.
-
-    This is intended to be run after main() in order to catch additional
-    utilities with more drastic name differences between EIA and FERC.
-    """
-    # Read in the data, keeping only utility IDs, names and addresses
-    eia_df = get_parquet_table(
-        "out_eia__yearly_utilities",
-        columns=[
-            "utility_id_eia",
-            "utility_id_pudl",
-            "utility_name_eia",
-            "street_address",
-            "city",
-            "state",
-            "zip_code",
-        ],
-    ).drop_duplicates()
-
-    # Drop records without any addresses
-    eia_df = eia_df.dropna(
-        subset=["street_address", "city", "state", "zip_code"], how="all"
-    )
-
-    ferc_df = get_parquet_table("core_pudl__assn_ferc1_pudl_utilities")
-    # Clean the data
-    eia_df["cleaned_utility_name"] = clean_utility_name(eia_df["utility_name_eia"])
-    ferc_df["cleaned_utility_name"] = clean_utility_name(ferc_df["utility_name_ferc1"])
-    # Exactly match the cleaned name
-    matched_utilities = match_utility_names(eia_df=eia_df, ferc_df=ferc_df)
-
-    # Warn about duplicated utilities
-    logger.warning("The following utilities are matched to more than one FERC ID:")
-    logger.info(
-        matched_utilities[matched_utilities.utility_id_ferc1.duplicated(keep=False)]
-    )
-    logger.warning("The following utilities are matched to more than one EIA ID:")
-    logger.info(
-        matched_utilities[matched_utilities.utility_id_eia.duplicated(keep=False)]
-    )
-
-    # Get the override spreadsheet
-    overrides = get_existing_overrides()
-    ## Get matches from the CSV
-    override_matches = overrides[
-        overrides.utility_id_eia.notnull() & overrides.utility_id_ferc1.notnull()
-    ]
-    # Unmatched records
-    unmatched = overrides[
-        ~(overrides.utility_id_eia.notnull() & overrides.utility_id_ferc1.notnull())
-    ]
-
-    for entity in ["ferc1", "eia"]:
-        overrides = drop_records_with_matches(
-            entity=entity,
-            matches=matched_utilities,
-            overrides=overrides,
-            override_matches=override_matches,
-            override_unmatched=unmatched,
-        )
-
-    updated_spreadsheet = add_new_matches_to_spreadsheet(
-        matches=matched_utilities, overrides=overrides
-    )
-    write_updated_matches(test_run=test_run, dataframe=updated_spreadsheet)
 
 
 @click.command(
@@ -314,45 +272,58 @@ def main(test_run: bool):
     # Clean the data
     eia_df["cleaned_utility_name"] = clean_utility_name(eia_df["utility_name_eia"])
     ferc_df["cleaned_utility_name"] = clean_utility_name(ferc_df["utility_name_ferc1"])
+
+    # Get false matches (we ignore these even if they exactly name match)
+    false_matches = get_false_matches()
+
     # Exactly match the cleaned name
-    matched_utilities = match_utility_names(eia_df=eia_df, ferc_df=ferc_df)
+    matched_utilities = match_utility_names(
+        eia_df=eia_df, ferc_df=ferc_df, false_matches=false_matches
+    )
 
     # Warn about duplicated utilities
-    logger.warning("The following utilities are matched to more than one FERC ID:")
+    logger.info("The following utilities are matched to more than one FERC ID:")
     logger.info(
-        matched_utilities[matched_utilities.utility_id_ferc1.duplicated(keep=False)]
+        matched_utilities[
+            matched_utilities.utility_id_ferc1.duplicated(keep=False)
+        ].sort_values("utility_id_ferc1")
     )
-    logger.warning("The following utilities are matched to more than one EIA ID:")
+    logger.info("The following utilities are matched to more than one EIA ID:")
     logger.info(
-        matched_utilities[matched_utilities.utility_id_eia.duplicated(keep=False)]
+        matched_utilities[
+            matched_utilities.utility_id_eia.duplicated(keep=False)
+        ].sort_values("utility_id_eia")
     )
 
     # Get the override spreadsheet
-    overrides = get_existing_overrides()
+    existing_glue_df = get_existing_glue_df()
     ## Get matches from the CSV
-    override_matches = overrides[
-        overrides.utility_id_eia.notnull() & overrides.utility_id_ferc1.notnull()
+    matches_existing = existing_glue_df[
+        existing_glue_df.utility_id_eia.notnull()
+        & existing_glue_df.utility_id_ferc1.notnull()
     ]
     # Unmatched records
-    unmatched = overrides[
-        ~(overrides.utility_id_eia.notnull() & overrides.utility_id_ferc1.notnull())
+    unmatched = existing_glue_df[
+        ~(
+            existing_glue_df.utility_id_eia.notnull()
+            & existing_glue_df.utility_id_ferc1.notnull()
+        )
     ]
 
     for entity in ["ferc1", "eia"]:
-        overrides = drop_records_with_matches(
+        existing_glue_df = drop_records_with_matches(
             entity=entity,
-            matches=matched_utilities,
-            overrides=overrides,
-            override_matches=override_matches,
-            override_unmatched=unmatched,
+            matches_new=matched_utilities,
+            existing_glue_df=existing_glue_df,
+            matches_existing=matches_existing,
+            unmatched_existing=unmatched,
         )
 
-    updated_spreadsheet = add_new_matches_to_spreadsheet(
-        matches=matched_utilities, overrides=overrides
+    updated_spreadsheet = add_new_matches_to_dataframe(
+        matches_new=matched_utilities, existing_glue_df=existing_glue_df
     )
     write_updated_matches(test_run=test_run, dataframe=updated_spreadsheet)
 
 
 if __name__ == "__main__":
-    # main()
-    get_likely_address_matches()
+    main()
