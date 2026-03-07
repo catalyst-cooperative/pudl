80f270cc32352822a124eb9b0cb6042f9e330a53
Zane Selvans <zane.selvans@catalyst.coop>
2026-03-06 20:08:18 -0600

    Move envvar control of generated files over to agent skills PR.

--- docs/conf.py
+++ docs/conf.py
@@ -75,9 +75,8 @@ bibtex_bibfiles = [
     "further_reading.bib",
 ]
 
-# Default behavior: do not keep generated files.
-# Any value (including an empty string) opts into keeping them.
-keep_generated_files = "PUDL_DOCS_KEEP_GENERATED_FILES" in os.environ
+# Default behavior: do not keep generated files. Clean up after the build exits.
+keep_generated_files = False
 
 # Redirects to keep folks from hitting 404 errors:
 redirects = {
