2472de9b68b6fe4d506643dbd92ae1141f092475
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-04-30 15:39:50 -0400

    test: make the bad-zip error a little nicer + explain it

--- dbt/tests/data_tests/generic_tests/foreign_key.sql
+++ dbt/tests/data_tests/generic_tests/foreign_key.sql
@@ -1,18 +1,26 @@
-{% test foreign_key(model, fk_column_names, pk_table_name, pk_column_names) %}
-{% if fk_column_names | length != pk_column_names | length %}
-    {{ exceptions.raise_compiler_error(
-        "foreign_key requires same number of child and parent key columns, got "
-        ~ (fk_column_names | length)
-        ~ " and "
-        ~ (pk_column_names | length)
-    ) }}
-{% endif %}
 -- As of 2026-04 we couldn't find a library that provided composite key foreign key checks.
 --
 -- So we're rolling our own.
 --
 -- SQLite ignores FKs if any of the key values in the child row are NULL, so we copy that behavior here.
 -- We grab the *whole* child row so we can see it in the failure.
+{% test foreign_key(model, fk_column_names, pk_table_name, pk_column_names) %}
+
+-- NOTE 2026-04-30: If we don't do this check, zip() will happily let us try to match one child column to two parent columns.
+{% if fk_column_names | length != pk_column_names | length %}
+    {{ exceptions.raise_compiler_error(
+        "foreign_key requires same number of child and parent key columns. "
+        ~ "child ("
+        ~ (fk_column_names | length)
+        ~ "): ["
+        ~ (fk_column_names | join(", "))
+        ~ "]; parent ("
+        ~ (pk_column_names | length)
+        ~ "): ["
+        ~ (pk_column_names | join(", "))
+        ~ "]"
+    ) }}
+{% endif %}
 SELECT
     child_rows.*,
     'missing_parent_key' AS failure_type
