1623726812f56494ea8139f46a3a8c34a393914a
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-05-07 11:34:42 -0400

    docs: remove confusing docs about pudl.dagster - you don't need to know more than 'this is the dang dagster related namespace'

--- src/pudl/dagster/__init__.py
+++ src/pudl/dagster/__init__.py
@@ -1,11 +1,1 @@
-"""Canonical Dagster orchestration package for PUDL.
-
-This package is the main import surface for the Dagster objects that make up the PUDL
-code location. It should expose the assembled ``Definitions`` object along with the
-default assets, asset checks, jobs, resources, and sensors that other modules or tools
-need to load. Import concrete submodules directly instead of relying on package-level
-re-exports so this package can stay free of import-time wiring and side effects.
-
-For the underlying Dagster concept, see
-https://docs.dagster.io/getting-started/concepts#definitions
-"""
+"""Top-level Dagster orchestration package for PUDL."""
