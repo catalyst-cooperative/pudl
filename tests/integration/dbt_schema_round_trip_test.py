15b2a8f5bf861f331419d3a605fd8da38ac3e01b
Dazhong Xia <dazhong.xia@catalyst.coop>
2026-05-11 17:37:49 -0400

    test: round-trip test didn't test model round-trip.

--- tests/integration/dbt_schema_round_trip_test.py
+++ tests/integration/dbt_schema_round_trip_test.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import deepdiff
 import pytest
 
-from pudl.dbt_schema import merge_schema_paths
+from pudl.dbt_schema import DbtSchema, merge_schema_paths
 from pudl.metadata.classes import PUDL_PACKAGE
-from pudl.scripts.dbt_helper import DbtSchema
+from pudl.scripts.dbt_helper import insert_data_source
 
 
 def get_schemas(root_dir: Path, pattern: str):
@@ -19,15 +19,17 @@ def get_schemas(root_dir: Path, pattern: str):
 def test_merge_schema_roundtrip(resource_name):
     dbt_dir = Path(__file__).parent.parent.parent / "dbt"
     reference = DbtSchema.from_yaml(
-        next(dbt_dir.glob(f"models/**/{resource_name}/schema.yml"))
+        insert_data_source(dbt_dir / "models", resource_name) / "schema.yml"
     )
-    machine_path = next(
-        dbt_dir.glob(f"schema_inputs/**/{resource_name}/schema.machine.yml")
+    machine_path = (
+        insert_data_source(dbt_dir / "schema_inputs", resource_name)
+        / "schema.machine.yml"
+    )
+    human_path = (
+        insert_data_source(dbt_dir / "schema_inputs", resource_name)
+        / "schema.human.yml"
     )
 
-    human_path = next(
-        dbt_dir.glob(f"schema_inputs/**/{resource_name}/schema.human.yml")
-    )
     merged = merge_schema_paths(machine_path, human_path)
     try:
         assert merged == reference
@@ -36,9 +38,7 @@ def test_merge_schema_roundtrip(resource_name):
             f"{resource_name} strict diff failed, trying order-insensitive",
             stacklevel=1,
         )
-        merged_table = merged.sources[0].tables[0]
-        reference_table = reference.sources[0].tables[0]
         diff = deepdiff.DeepDiff(
-            merged_table, reference_table, ignore_order=True, report_repetition=True
+            merged, reference, ignore_order=True, report_repetition=True
         )
         assert diff == {}
