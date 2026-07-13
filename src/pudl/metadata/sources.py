f767e47efbc609497de4e455bced2ca3be128d22
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2026-07-13 16:52:22 -0400

    Add 2026half2 to recognized partitions for eia930

--- src/pudl/metadata/sources.py
+++ src/pudl/metadata/sources.py
@@ -354,7 +354,7 @@ SOURCES: dict[str, Any] = {
         "working_partitions": {
             "half_years": [
                 f"{year}half{half}" for year in range(2015, 2027) for half in [1, 2]
-            ][1:-1]  # Begins in H2 of 2015 and currently ends in H2 of 2026
+            ][1:]  # Begins in H2 of 2015 and currently ends in H2 of 2026
         },
         "contributors": [
             CONTRIBUTORS["catalyst-cooperative"],
